7742c8fe1b98c6995a79e3bbbd81f6228f93e932
Martijn van de Rijdt <mvdrijdt@planet.nl>
2018-12-23 16:37:34 +0100

    Partial solution

---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2018/day04/Guard.java
@@ -0,0 +1,48 @@
+package nl.mvdr.adventofcode.adventofcode2018.day04;
+
+import java.util.stream.IntStream;
+
+/**
+ * Information about a guard.
+ *
+ * Note that this class is mutable.
+ *
+ * @author Martijn van de Rijdt
+ */
+class Guard {
+    /** Identification of this guard. */
+    private final int id;
+    
+    /** The total number of minutes that this guard has been asleep during midnight hours. */
+    private int minutesAsleep;
+    
+    /** Map of each minute of the midnight hour, to the number of times the guard has been asleep during that minute. */
+    private int[] numberOfTimesAsleep;
+
+    /** Constructor. */
+    Guard(int id) {
+        super();
+        
+        this.id = id;
+        this.minutesAsleep = 0;
+        
+        this.numberOfTimesAsleep = new int[60];
+    }
+    
+    /**
+     * Records a nap during the midnight hour.
+     * 
+     * @param startMinute minute the guard fell asleep (inclusive boundary)
+     * @param endMinute minute the guard woke up (exclusive boundary)
+     */
+    void sleep(int startMinute, int endMinute) {
+        this.minutesAsleep += endMinute - startMinute;
+        
+        IntStream.range(startMinute, endMinute)
+                .forEach(minute -> numberOfTimesAsleep[minute]++);
+    }
+
+    int getMinutesAsleep() {
+        return minutesAsleep;
+    }
+}
--- src/main/java/nl/mvdr/adventofcode/adventofcode2018/day04/ReposeRecord.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2018/day04/ReposeRecord.java
@@ -3,7 +3,15 @@ package nl.mvdr.adventofcode.adventofcode2018.day04;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import nl.mvdr.adventofcode.PathSolver;
 
@@ -14,17 +22,59 @@ import nl.mvdr.adventofcode.PathSolver;
  * @author Martijn van de Rijdt
  */
 public class ReposeRecord implements PathSolver {
-
+    /** Regular expression for a record indicating the start of a shift. Group 1 is the guard id. */
+    private static final Pattern PATTERN_BEGIN_SHIFT = Pattern.compile("\\[\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:\\d\\d\\] Guard #(\\d+) begins shift");
+    /** Regular expression for a record indicating that a guard falls asleep. Group 1 is the minute of the midnight hour. */
+    private static final Pattern PATTERN_FALL_ASLEEP = Pattern.compile("\\[\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:(\\d\\d)\\] falls asleep");
+    /** Regular expression for a record indicating that a guard wakes up. Group 1 is the minute of the midnight hour. */
+    private static final Pattern PATTERN_WAKE_UP = Pattern.compile("\\[\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:(\\d\\d)\\] wakes up");
+    
     @Override
     public String solve(Path inputFilePath) throws IOException {
-        Files.lines(inputFilePath)
+        List<String> records = Files.lines(inputFilePath)
                 // ignore empty lines (the last line in the file)
                 .filter(Objects::nonNull)
                 .filter(line -> !line.isBlank())
                 // sort input in chronological order (which is equal to lexicographical order)
                 .sorted()
-                .forEach(System.out::println);
+                .collect(Collectors.toList());
         
+        Map<Integer, Guard> guards = new HashMap<>();
+        
+        Guard currentGuard = null;
+        Iterator<String> iterator = records.iterator();
+        while (iterator.hasNext()) {
+            String record = iterator.next();
+            Matcher matcher = PATTERN_BEGIN_SHIFT.matcher(record);
+            if (matcher.matches()) {
+                // Start of a new shift.
+                int guardId = Integer.valueOf(matcher.group(1));
+                guards.putIfAbsent(Integer.valueOf(guardId), new Guard(guardId));
+                currentGuard = guards.get(Integer.valueOf(guardId));
+            } else {
+                matcher = PATTERN_FALL_ASLEEP.matcher(record);
+                if (!matcher.matches()) {
+                    throw new IllegalStateException("Unexpected record: " + record);
+                }
+                // The current guard fell asleep.
+                int napStart = Integer.valueOf(matcher.group(1));
+                
+                // The next record must be of the current guard waking up.
+                record = iterator.next();
+                matcher = PATTERN_WAKE_UP.matcher(record);
+                if (!matcher.matches()) {
+                    throw new IllegalStateException("Unexpected record: " + record);
+                }
+                int napEnd = Integer.valueOf(matcher.group(1));
+                currentGuard.sleep(napStart, napEnd);
+            }
+        }
+        
+        Guard guard = guards.values().stream()
+                .max(Comparator.comparing(Guard::getMinutesAsleep))
+                .get();
+                
+        System.out.println(guard);
         
         // TODO implement
         return null;
