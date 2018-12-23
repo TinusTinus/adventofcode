0487e73482fb0841f5d693eab93f63fd71a7272c
Martijn van de Rijdt <mvdrijdt@planet.nl>
2018-12-23 12:34:37 +0100

    Solved day 2 part 2.

---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2018/day02/InventoryManagementSystemPart2.java
@@ -0,0 +1,64 @@
+package nl.mvdr.adventofcode.adventofcode2018.day02;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import nl.mvdr.adventofcode.PathSolver;
+
+/**
+ * Solution to the day 2 puzzle of 2018's Advent of Code:
+ * <a href="https://adventofcode.com/2018/day/2">Inventory Management
+ * System</a>.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class InventoryManagementSystemPart2 implements PathSolver {
+
+    @Override
+    public String solve(Path inputFilePath) throws IOException {
+        List<String> boxIds = Files.lines(inputFilePath)
+                // ignore empty lines (the last line in the file)
+                .filter(Objects::nonNull)
+                .filter(line -> !line.isBlank())
+                .collect(Collectors.toList());
+        
+        String result = null;
+        for (String boxId0 : boxIds) {
+            for (String boxId1 : boxIds) {
+                if (boxId0.length() == boxId1.length()) {
+                    int differingCharacters = 0;
+                    StringBuilder resultBuilder = new StringBuilder();
+                    for (int i = 0; i != boxId0.length(); i++) {
+                        if (boxId0.charAt(i) == boxId1.charAt(i)) {
+                            resultBuilder.append(boxId0.charAt(i));
+                        } else {
+                            differingCharacters++;
+                        }
+                    }
+                    if (differingCharacters == 1) {
+                        result = resultBuilder.toString();
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+
+    /**
+     * Main method.
+     * 
+     * @param args commandline arguments; these are ignored
+     */
+    public static void main(String[] args) {
+        InventoryManagementSystemPart2 instance = new InventoryManagementSystemPart2();
+
+        String result = instance.solve("input-day02-2018.txt");
+
+        System.out.println(result);
+    }
+}
---
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2018/day02/InventoryManagementSystemPart2Test.java
@@ -0,0 +1,29 @@
+package nl.mvdr.adventofcode.adventofcode2018.day02;
+
+import org.junit.jupiter.api.Test;
+
+import nl.mvdr.adventofcode.SolverTest;
+
+/**
+ * Unit test for {@link InventoryManagementSystemPart2}.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class InventoryManagementSystemPart2Test extends SolverTest<InventoryManagementSystemPart2> {
+	/** Constructor. */
+	public InventoryManagementSystemPart2Test() {
+		super(InventoryManagementSystemPart2.class);
+	}
+	
+	/** Test case based on the example from the puzzle. */
+	@Test
+	public void testExample() {
+		assertSolution("fgij", "example-day02-2018-part2.txt");
+	}
+	
+	/** Test case based on the accepted solution. */
+    @Test
+    public void testSolution() {
+        assertSolution("bvnfawcnyoeyudzrpgslimtkj", "input-day02-2018.txt");
+    }
+}
