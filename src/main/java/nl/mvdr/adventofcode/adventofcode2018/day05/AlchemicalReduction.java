44afcb8eb6908542d51c892e5ae2267bfef83a64
Martijn van de Rijdt <mvdrijdt@planet.nl>
2018-12-23 17:34:26 +0100

    Refactoring.

--- src/main/java/nl/mvdr/adventofcode/adventofcode2018/day05/AlchemicalReduction.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2018/day05/AlchemicalReduction.java
@@ -16,39 +16,11 @@ public class AlchemicalReduction implements PathSolver {
 
     @Override
     public String solve(Path inputFilePath) throws IOException {
-        String polymer = Files.lines(inputFilePath)
+        return Files.lines(inputFilePath)
                 .findFirst()
-                .get();
-        
-        return "" + reduce(polymer).length();
-    }
-    
-    private String reduce(String polymer) {
-        String result = polymer;
-        
-        boolean done = false;
-        while (!done) {
-            int i = 1;
-            while (i != result.length() && !canReact(result.charAt(i - 1), result.charAt(i))) {
-                i++;
-            }
-            
-            if (i == result.length()) {
-                done = true;
-            } else {
-                // A reaction is possible.
-                result = result.substring(0, i - 1) + result.substring(i + 1);
-            }
-        }
-        
-        return result;
-    }
-    
-    private boolean canReact(char c0, char c1) {
-        // must have the same type
-        return Character.toLowerCase(c0) == Character.toLowerCase(c1)
-                // and opposing polarities
-                && Character.isUpperCase(c0) != Character.isUpperCase(c1);
+                .map(Polymers::react)
+                .get()
+                .length() + "";
     }
 
     /**
---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2018/day05/Polymers.java
@@ -0,0 +1,48 @@
+package nl.mvdr.adventofcode.adventofcode2018.day05;
+
+/**
+ * Helper class.
+ *
+ * @author Martijn van de Rijdt
+ * 
+ */
+class Polymers {
+    /** Private class to prevent singleton instantiation. */
+    private Polymers() {
+        super();
+    }
+    
+    /**
+     * Reduces the given polymer by reacting as much as possible.
+     * 
+     * @param polymer polymer
+     * @return polymer after reaction
+     */
+    static String react(String polymer) {
+        String result = polymer;
+        
+        boolean done = false;
+        while (!done) {
+            int i = 1;
+            while (i != result.length() && !canReact(result.charAt(i - 1), result.charAt(i))) {
+                i++;
+            }
+            
+            if (i == result.length()) {
+                done = true;
+            } else {
+                // A reaction is possible.
+                result = result.substring(0, i - 1) + result.substring(i + 1);
+            }
+        }
+        
+        return result;
+    }
+    
+    private static boolean canReact(char c0, char c1) {
+        // must have the same type
+        return Character.toLowerCase(c0) == Character.toLowerCase(c1)
+                // and opposing polarities
+                && Character.isUpperCase(c0) != Character.isUpperCase(c1);
+    }
+}
