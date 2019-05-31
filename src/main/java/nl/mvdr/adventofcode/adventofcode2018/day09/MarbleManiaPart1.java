626759a5d6ad4a3d011686720204cc406ca0b0c0
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-05-31 19:42:29 +0200

    Partial solution to day 9

--- src/main/java/nl/mvdr/adventofcode/adventofcode2018/day09/MarbleManiaPart1.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2018/day09/MarbleManiaPart1.java
@@ -1,10 +1,11 @@
 package nl.mvdr.adventofcode.adventofcode2018.day09;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.regex.Matcher;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,14 +22,53 @@ public class MarbleManiaPart1 implements PathSolver {
     
     private static final Logger LOGGER = LoggerFactory.getLogger(MarbleManiaPart1.class);
     
-    private static final Pattern PATTERN = Pattern.compile("(\\d+) players; last marble is worth (\\d+) points");
-    
     @Override
     public String solve(Path inputFilePath) throws IOException {
         PuzzleInput puzzleInput = PuzzleInput.parse(inputFilePath);
         
-        // TODO
-        return null;
+        int[] scores = new int[puzzleInput.getPlayers()];
+        List<Integer> marbles = new ArrayList<>(List.of(Integer.valueOf(0)));
+        int currentMarbleIndex = 0;
+        
+        for (int marble = 1; marble != puzzleInput.getPoints(); marble++) {
+            int playerIndex = marble % puzzleInput.getPlayers();
+            if (marble % 23 == 0) {
+                scores[playerIndex] += marble;
+                currentMarbleIndex = (currentMarbleIndex - 7 + marbles.size()) % marbles.size();
+                scores[playerIndex] += marbles.remove(currentMarbleIndex).intValue();
+            } else {
+                currentMarbleIndex = (currentMarbleIndex + 2) % marbles.size() + 1;
+                marbles.add(currentMarbleIndex, Integer.valueOf(marble));
+            }
+            
+            logGameState(puzzleInput, marbles, currentMarbleIndex, playerIndex);
+            
+        }
+        
+        return "" + IntStream.of(scores).max().getAsInt();
+    }
+
+    private void logGameState(PuzzleInput puzzleInput, List<Integer> marbles, int currentMarbleIndex, int playerIndex) {
+        if (LOGGER.isDebugEnabled()) {
+
+            StringBuilder builder = new StringBuilder("[");
+            builder.append(String.format("%" + ("" + puzzleInput.getPlayers()).length() + "d", playerIndex + 1));
+            builder.append("] ");
+            for (int i = 0; i != marbles.size(); i++) {
+                if (i == currentMarbleIndex) {
+                    builder.append("(");
+                } else {
+                    builder.append(" ");
+                }
+                builder.append(String.format("%" + ("" + puzzleInput.getPoints()).length() + "d", marbles.get(i)));
+                if (i == currentMarbleIndex) {
+                    builder.append(")");
+                } else {
+                    builder.append(" ");
+                }
+            }
+            LOGGER.debug(builder.toString());
+        }
     }
     
     /**
