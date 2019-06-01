ddd95e6d46fec56a7d5d4380070aa6780ad7aa99
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-06-01 10:40:49 +0200

    Implemented power level calculation.

---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2018/day11/Cell.java
@@ -0,0 +1,63 @@
+package nl.mvdr.adventofcode.adventofcode2018.day11;
+
+/**
+ * A fuel cell.
+ *
+ * @author Martijn van de Rijdt
+ */
+class Cell {
+
+    private final int x;
+
+    private final int y;
+    
+    private final int serialNumber;
+
+    /**
+     * Constructor.
+     * 
+     * @param x x coordinate (horizontal)
+     * @param y y coordinate (vertical)
+     * @param serialNumber the grid serial number
+     */
+    Cell(int x, int y, int serialNumber) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.serialNumber = serialNumber;
+    }
+    
+    /**
+     * Computes this cell's power level.
+     * 
+     * The power level in a given fuel cell can be found through the following process:
+     * <ul>
+     * <li>Find the fuel cell's rack ID, which is its X coordinate plus 10.</li>
+     * <li>Begin with a power level of the rack ID times the Y coordinate.</li>
+     * <li>Increase the power level by the value of the grid serial number (your puzzle input).</li>
+     * <li>Set the power level to itself multiplied by the rack ID.</li>
+     * <li>Keep only the hundreds digit of the power level (so 12345 becomes 3; numbers with no hundreds digit become 0).</li>
+     * <li>Subtract 5 from the power level.</li>
+     * </ul>
+     * 
+     * @return this cell's power level
+     */
+    int powerLevel() {
+        // Find the fuel cell's rack ID, which is its X coordinate plus 10.
+        int rackId = x + 10;
+        
+        // Begin with a power level of the rack ID times the Y coordinate.
+        int result = rackId * y;
+        // Increase the power level by the value of the grid serial number (your puzzle input).
+        result = result + serialNumber;
+        // Set the power level to itself multiplied by the rack ID.
+        result = result * rackId;
+        // Keep only the hundreds digit of the power level (so 12345 becomes 3; numbers with no hundreds digit become 0).
+        result = result / 100;
+        result = result % 10;
+        // Subtract 5 from the power level.
+        result = result - 5;
+        
+        return result;
+    }
+}
---
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2018/day11/CellTest.java
@@ -0,0 +1,77 @@
+package nl.mvdr.adventofcode.adventofcode2018.day11;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test cases for {@link Cell}.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class CellTest {
+    
+    /**
+     * Performs a test case for {@link Cell#powerLevel}.
+     * 
+     * @param x cell's x coordinate
+     * @param y cell's y coordinate
+     * @param serialNumber grid's serial number
+     * @param expectedPowerLevel expected power level
+     */
+    private void testPowerLevel(int x, int y, int serialNumber, int expectedPowerLevel) {
+        Cell cell = new Cell(x, y, serialNumber);
+        
+        int powerLevel = cell.powerLevel();
+        
+        Assertions.assertEquals(expectedPowerLevel, powerLevel);
+    }
+    
+    /**
+     * Test case for {@link Cell#powerLevel()} based on the first example from the puzzle description.
+     * 
+     * "For example, to find the power level of the fuel cell at 3,5 in a grid with serial number 8:
+     * <ul>
+     * <li>The rack ID is 3 + 10 = 13.</li>
+     * <li>The power level starts at 13 * 5 = 65.</li>
+     * <li>Adding the serial number produces 65 + 8 = 73.</li>
+     * <li>Multiplying by the rack ID produces 73 * 13 = 949.</li>
+     * <li>The hundreds digit of 949 is 9.</li>
+     * <li>Subtracting 5 produces 9 - 5 = 4.</li>
+     * </ul>
+     * So, the power level of this fuel cell is 4."
+     */
+    @Test
+    public void testPowerLevelExample0() {
+        testPowerLevel(3, 5, 8, 4);
+    }
+    
+    /**
+     * Test case for {@link Cell#powerLevel()} based on an example from the puzzle description.
+     * 
+     * "Fuel cell at  122,79, grid serial number 57: power level -5."
+     */
+    @Test
+    public void testPowerLevelExample1() {
+        testPowerLevel(122, 79, 57, -5);
+    }
+    
+    /**
+     * Test case for {@link Cell#powerLevel()} based on an example from the puzzle description.
+     * 
+     * "Fuel cell at 217,196, grid serial number 39: power level 0."
+     */
+    @Test
+    public void testPowerLevelExample2() {
+        testPowerLevel(217, 196, 39, 0);
+    }
+    
+    /**
+     * Test case for {@link Cell#powerLevel()} based on an example from the puzzle description.
+     * 
+     * "Fuel cell at 101,153, grid serial number 71: power level 4."
+     */
+    @Test
+    public void testPowerLevelExample3() {
+        testPowerLevel(101, 153, 71, 4);
+    }
+}
