be494661786c51f6f08456f267fdc5d2ce756aab
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-07-15 20:16:26 +0200

    Added a skeleton for day 20.

---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2018/day20/RegularMap.java
@@ -0,0 +1,36 @@
+package nl.mvdr.adventofcode.adventofcode2018.day20;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nl.mvdr.adventofcode.PathSolver;
+
+/**
+ * Solution to the day 20 puzzle of 2018's Advent of Code:
+ * <a href="https://adventofcode.com/2018/day/20">A Regular Map</a>.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class RegularMap implements PathSolver {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(RegularMap.class);
+    
+    @Override
+    public String solve(Path inputFilePath) throws IOException {
+        return null; // TODO
+    }
+    
+    /**
+     * Main method.
+     * 
+     * @param args commandline arguments; these are ignored
+     */
+    public static void main(String[] args) {
+        RegularMap solver = new RegularMap();
+        String solution = solver.solve("input-day20-2018.txt");
+        LOGGER.info(solution);
+    }
+}
---
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2018/day20/RegularMapTest.java
@@ -0,0 +1,78 @@
+package nl.mvdr.adventofcode.adventofcode2018.day20;
+
+import org.junit.jupiter.api.Test;
+
+import nl.mvdr.adventofcode.SolverTest;
+
+/**
+ * Unit test for {@link RegularMap}.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class RegularMapTest extends SolverTest<RegularMap> {
+    /** Constructor. */
+    public RegularMapTest() {
+        super(RegularMap.class);
+    }
+
+    /**
+     * Test case based on an example from the puzzle.
+     * 
+     * In the first example ({@code ^WNE$}),
+     * the furthest room would be the north-east corner 3 doors away.
+     */
+    @Test
+    public void testExample0() {
+        assertSolution("3", "example-day20-2018-0.txt");
+    }
+    
+    /**
+     * Test case based on an example from the puzzle.
+     * 
+     * In the second example ({@code ^ENWWW(NEEE|SSE(EE|N))$}),
+     * the furthest room would be the south-east corner 10 doors away.
+     */
+    @Test
+    public void testExample1() {
+        assertSolution("10", "example-day20-2018-1.txt");
+    }
+    
+    /**
+     * Test case based on an example from the puzzle.
+     * 
+     * In the third example ({@code ^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$}),
+     * the furthest room would be the north-east corner 18 doors away.
+     */
+    @Test
+    public void testExample2() {
+        assertSolution("18", "example-day20-2018-2.txt");
+    }
+    
+    /**
+     * Test case based on an example from the puzzle.
+     * 
+     * Regex: {@code ^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$}.
+     * Furthest room requires passing 23 doors.
+     */
+    @Test
+    public void testExample3() {
+        assertSolution("23", "example-day20-2018-3.txt");
+    }
+    
+    /**
+     * Test case based on an example from the puzzle.
+     * 
+     * Regex: {@code ^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$}.
+     * Furthest room requires passing 31 doors.
+     */
+    @Test
+    public void testExample4() {
+        assertSolution("31", "example-day20-2018-4.txt");
+    }
+        
+    /** Test case based on the accepted solution to the puzzle. */
+    @Test
+    public void testSolution() {
+        assertSolution("?", "input-day20-2018.txt");
+    }
+}
