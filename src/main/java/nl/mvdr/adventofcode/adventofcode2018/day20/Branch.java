a988447e1f0e7bd086d27068e0effe57e9364348
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-07-15 23:38:18 +0200

    Refactored Branch to support any number of options.

--- src/main/java/nl/mvdr/adventofcode/adventofcode2018/day20/Branch.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2018/day20/Branch.java
@@ -1,7 +1,8 @@
 package nl.mvdr.adventofcode.adventofcode2018.day20;
 
-import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import nl.mvdr.adventofcode.adventofcode2018.point.Point;
 
@@ -11,33 +12,42 @@ import nl.mvdr.adventofcode.adventofcode2018.point.Point;
  * @author Martijn van de Rijdt
  */
 public class Branch implements RoomMapExpression {
-    /** Left-hand side. */
-    private final RoomMapExpression lhs;
-    /** Right-hand side. */
-    private final RoomMapExpression rhs;
+    
+    private final List<RoomMapExpression> options;
     
     /**
      * Constructor.
      * 
      * @param lhs left-hand side
      * @param rhs right-hand side
+     * @deprecated use {@link #Branch(List)} instead
      */
+    @Deprecated // TODO remove
     Branch(RoomMapExpression lhs, RoomMapExpression rhs) {
-        super();
-        this.lhs = lhs;
-        this.rhs = rhs;
+        this(List.of(lhs, rhs));
+        
     }
     
+    /**
+     * Constructor.
+     * 
+     * @param options options
+     */
+    Branch(List<RoomMapExpression> options) {
+        super();
+        this.options = options;
+    }
+
     @Override
     public Set<Point> apply(Set<Point> points, RoomMap map) {
-        HashSet<Point> result = new HashSet<>();
-        result.addAll(lhs.apply(points, map));
-        result.addAll(rhs.apply(points, map));
-        return result;
+        return options.stream()
+                .map(option -> option.apply(points, map))
+                .flatMap(Set::stream)
+                .collect(Collectors.toSet());
     }
     
     @Override
     public String toString() {
-        return "(" + lhs + "|" + rhs + ")";
+        return "(" + options.stream().map(Object::toString).collect(Collectors.joining("|")) + ")";
     }
 }
