28e18049d69208f7149240fad3852fbc5d6d8936
mgwhv <mgwhv@192.168.2.15>
2019-07-24 22:38:02 +0100

    Added type determination based on erosion levels

--- src/main/java/nl/mvdr/adventofcode/adventofcode2018/day22/Type.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2018/day22/Type.java
@@ -23,4 +23,25 @@ enum Type {
     int getRiskLevel() {
         return riskLevel;
     }
+    
+    /**
+     * Determines the type of a region, based on its erosion level.
+     * 
+     * @param erosionLevel erosion level
+     * @return region type
+     */
+    static Type getType(int erosionLevel) {
+        int erosionLevelModulo3 = erosionLevel % 3;
+        Type result;
+        if (erosionLevelModulo3 == 0) {
+            result = ROCKY;
+        } else if (erosionLevelModulo3 == 1) {
+            result = WET;
+        } else if (erosionLevelModulo3 == 2) {
+            result = NARROW;
+        } else {
+            throw new IllegalArgumentException("Unexpected erosion level: " + erosionLevel);
+        }
+        return result;
+    }
 }
