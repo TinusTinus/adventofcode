8bd395188b746c02dc8fdb5abe433df902b5f589
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-08-09 19:38:14 +0200

    Added an example

--- src/test/java/nl/mvdr/adventofcode/adventofcode2018/day24/ImmuneSystemSimulatorPart1Test.java
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2018/day24/ImmuneSystemSimulatorPart1Test.java
@@ -17,8 +17,14 @@ public class ImmuneSystemSimulatorPart1Test extends SolverTest<ImmuneSystemSimul
 
     /** Test case based on an example from the puzzle. */
     @Test
-    public void testExample() {
-        assertSolution("5216", "example-day24-2018.txt");
+    public void testExample0() {
+        assertSolution("5216", "example-day24-2018-0.txt");
+    }
+    
+    /** Test case based on an example from the puzzle. */
+    @Test
+    public void testExample1() {
+        assertSolution("51", "example-day24-2018-1.txt");
     }
     
     /** Test case based on the accepted solution to the puzzle. */
