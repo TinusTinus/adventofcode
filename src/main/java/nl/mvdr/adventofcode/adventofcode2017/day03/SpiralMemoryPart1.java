ca4d60c0edb6944fe604613c46ec7f922e490434
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-08-17 16:45:02 +0200

    Started on day 3

---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2017/day03/SpiralMemoryPart1.java
@@ -0,0 +1,55 @@
+package nl.mvdr.adventofcode.adventofcode2017.day03;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nl.mvdr.adventofcode.PathSolver;
+
+/**
+ * Solution to the day 3 puzzle of 2018's Advent of Code:
+ * <a href="https://adventofcode.com/2018/day/3">Spiral Memory</a>.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class SpiralMemoryPart1 implements PathSolver<Integer> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpiralMemoryPart1.class);
+    
+    /**
+     * {@inheritDoc}
+     * 
+     * @return number of steps
+     */
+    @Override
+    public Integer solve(Path inputFilePath) throws IOException {
+        String input = Files.lines(inputFilePath)
+                .findFirst()
+                .get();
+        int square = Integer.parseInt(input);
+        
+        LOGGER.debug("Input square: {}", Integer.valueOf(square));
+        
+        return null; // TODO
+    }
+    
+    /**
+     * Main method.
+     * 
+     * @param args commandline arguments; these are ignored
+     */
+    public static void main(String[] args) {
+        SpiralMemoryPart1 instance = new SpiralMemoryPart1();
+
+        String result = instance.solve("input-day03-2017.txt");
+
+        LOGGER.info(result);
+    }
+}
---
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2017/day03/SpiralMemoryPart1Test.java
@@ -0,0 +1,51 @@
+package nl.mvdr.adventofcode.adventofcode2017.day03;
+
+import org.junit.jupiter.api.Test;
+
+import nl.mvdr.adventofcode.SolverTest;
+
+/**
+ * Unit test cases for {@link SpiralMemoryPart1}.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class SpiralMemoryPart1Test extends SolverTest<SpiralMemoryPart1> {
+
+    /** Constructor. */
+    public SpiralMemoryPart1Test() {
+        super(SpiralMemoryPart1.class);
+    }
+
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample0() {
+        assertSolution("0", "example-day03-2017-0.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample1() {
+        assertSolution("3", "example-day03-2017-1.txt");
+    }
+
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample2() {
+        assertSolution("2", "example-day03-2017-2.txt");
+    }
+
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample3() {
+        assertSolution("31", "example-day03-2017-3.txt");
+    }
+
+    
+    /** Test case based on the accepted slution. */
+    @Test
+    public void testSolution() {
+        assertSolution("?", "input-day03-2017.txt");
+    }
+}
