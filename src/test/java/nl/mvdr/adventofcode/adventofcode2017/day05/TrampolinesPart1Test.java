3bf1ffc7ae2c4da805bbbde44e22586fdcd40724
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-08-17 19:07:30 +0200

    Solved AoC 2017, day 5, part 1

---
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2017/day05/TrampolinesPart1Test.java
@@ -0,0 +1,30 @@
+package nl.mvdr.adventofcode.adventofcode2017.day05;
+
+import org.junit.jupiter.api.Test;
+
+import nl.mvdr.adventofcode.SolverTest;
+
+/**
+ * Unit test cases for {@link TrampolinesPart1}.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class TrampolinesPart1Test extends SolverTest<TrampolinesPart1> {
+
+    /** Constructor. */
+    public TrampolinesPart1Test() {
+        super(TrampolinesPart1.class);
+    }
+
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample() {
+        assertSolution("5", "example-day05-2017.txt");
+    }
+    
+    /** Test case based on the accepted slution. */
+    @Test
+    public void testSolution() {
+        assertSolution("358131", "input-day05-2017.txt");
+    }
+}
