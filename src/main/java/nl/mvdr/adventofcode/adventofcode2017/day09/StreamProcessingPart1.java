8c1d9b98698792e3a233ca548c16c9c263d7b344
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-08-24 16:07:08 +0200

    Solved day 9 part 1

---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2017/day09/StreamProcessingPart1.java
@@ -0,0 +1,84 @@
+package nl.mvdr.adventofcode.adventofcode2017.day09;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nl.mvdr.adventofcode.PathSolver;
+
+/**
+ * Solution to the day 9 puzzle of 2017's Advent of Code:
+ * <a href="https://adventofcode.com/2017/day/9">Stream Processing</a>.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class StreamProcessingPart1 implements PathSolver<Integer> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(StreamProcessingPart1.class);
+    
+    /**
+     * {@inheritDoc}
+     * 
+     * @return score
+     */
+    @Override
+    public Integer solve(Path inputFilePath) throws IOException {
+        String inputText = Files.lines(inputFilePath).findFirst().get();
+        
+        LOGGER.debug("Input: {}", inputText);
+        
+        int currentGroupScore = 0;
+        int totalScore = 0;
+        boolean canceled = false;
+        boolean garbage = false;
+        
+        for (int i = 0; i != inputText.length(); i++) {
+            char c = inputText.charAt(i);
+            
+            if (canceled) {
+                LOGGER.debug(c + ": ignoring canceled character.");
+                canceled = false;
+            } else if (c == '!') {
+                LOGGER.debug("!: canceling next character.");
+                canceled = true;
+            } else if (garbage && c == '>') {
+                LOGGER.debug(">: end of garbage.");
+                garbage = false;
+            } else if (garbage) {
+                LOGGER.debug(c + ": ignoring garbage.");
+            } else if (c == '<') {
+                LOGGER.debug("<: start of garbage.");
+                garbage = true;
+            } else if (c == '{') {
+                LOGGER.debug("{: start of new group.");
+                currentGroupScore++;
+            } else if (c == '}') {
+                LOGGER.debug("}: end of group.");
+                totalScore = totalScore + currentGroupScore;
+                currentGroupScore--;
+            } else if (c == ',') {
+                LOGGER.debug(",: comma between groups.");
+            } else {
+                throw new IllegalStateException("Unexpected character " + c + " in position " + i);
+            }
+        }
+        
+        return Integer.valueOf(totalScore);
+    }
+    
+    /**
+     * Main method.
+     * 
+     * @param args commandline arguments; these are ignored
+     */
+    public static void main(String[] args) {
+        StreamProcessingPart1 instance = new StreamProcessingPart1();
+
+        String result = instance.solve("input-day09-2017.txt");
+
+        LOGGER.info(result);
+    }
+}
---
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2017/day09/StreamProcessingPart1Test.java
@@ -0,0 +1,72 @@
+package nl.mvdr.adventofcode.adventofcode2017.day09;
+
+import org.junit.jupiter.api.Test;
+
+import nl.mvdr.adventofcode.SolverTest;
+
+/**
+ * Unit test cases for {@link StreamProcessingPart1}.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class StreamProcessingPart1Test extends SolverTest<StreamProcessingPart1> {
+
+    /** Constructor. */
+    public StreamProcessingPart1Test() {
+        super(StreamProcessingPart1.class);
+    }
+
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample0() {
+        assertSolution("1", "example-day09-2017-0.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample1() {
+        assertSolution("6", "example-day09-2017-1.txt");
+    }
+
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample2() {
+        assertSolution("5", "example-day09-2017-2.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample3() {
+        assertSolution("16", "example-day09-2017-3.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample4() {
+        assertSolution("1", "example-day09-2017-4.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample5() {
+        assertSolution("9", "example-day09-2017-5.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample6() {
+        assertSolution("9", "example-day09-2017-6.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample7() {
+        assertSolution("3", "example-day09-2017-7.txt");
+    }
+    
+    /** Test case based on the accepted slution. */
+    @Test
+    public void testSolution() {
+        assertSolution("11898", "input-day09-2017.txt");
+    }
+}
