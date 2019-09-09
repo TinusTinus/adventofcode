11aad2ca4e4bc9400df79a936782d36f06d22afa
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-09-09 21:16:59 +0200

    Added criterium support.

--- src/main/java/nl/mvdr/adventofcode/adventofcode2017/day15/Generator.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2017/day15/Generator.java
@@ -11,18 +11,32 @@ class Generator {
     
     /** This generator's factor. */
     private final long factor;
+    /** This generator will only return values that are divisible by this number. */
+    private final long criterium;
     /** Previously generated value (initially: the start value). */
     private long previousValue;
-    
+
     /**
-     * Constructor.
+     * Constructor for a generator without additional criteria.
      * 
      * @param factor this generator's factor
      * @param startValue starting value for this generator (from the puzzle input)
      */
     Generator(long factor, long startValue) {
+        this(factor, 1L, startValue);
+    }
+    
+    /**
+     * Constructor.
+     * 
+     * @param factor this generator's factor
+     * @param criterium this generator will only return values that are divisible by this number
+     * @param startValue starting value for this generator (from the puzzle input)
+     */
+    Generator(long factor, long criterium, long startValue) {
         super();
         this.factor = factor;
+        this.criterium = criterium;
         this.previousValue = startValue;
     }
     
@@ -35,6 +49,9 @@ class Generator {
      */
     long nextValue() {
         long result = previousValue * factor % DIVISOR;
+        while (result % criterium != 0) {
+            result = previousValue * factor % DIVISOR;
+        }
         
         previousValue = result;
         
