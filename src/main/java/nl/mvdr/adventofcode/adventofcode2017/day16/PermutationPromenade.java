a130d16d758c6378dcb0170412a627b7470e89c8
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-09-11 22:14:47 +0200

    Reduced code duplication

---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2017/day16/PermutationPromenade.java
@@ -0,0 +1,79 @@
+package nl.mvdr.adventofcode.adventofcode2017.day16;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nl.mvdr.adventofcode.PathSolver;
+
+/**
+ * Solution to the day 16 puzzle of 2017's Advent of Code:
+ * <a href="https://adventofcode.com/2017/day/16">Permutation Promenade</a>.
+ *
+ * @author Martijn van de Rijdt
+ */
+abstract class PermutationPromenade implements PathSolver<String> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PermutationPromenade.class);
+
+    /** The number of times the dance should be performed. */
+    private final int iterations;
+    
+    /** The number of programs performing the dance. */
+    private final int numberOfPrograms;
+
+    /**
+     * Constructor.
+     * 
+     * @param iterations number of times the dance should be performed
+     */
+    PermutationPromenade(int iterations) {
+        this(iterations, 16);
+    }
+
+    /**
+     * Constructor.
+     * 
+     * @param iterations number of times the dance should be performed
+     * @param numberOfPrograms number of programs performing the dance
+     */
+    PermutationPromenade(int iterations, int numberOfPrograms) {
+        super();
+        this.iterations = iterations;
+        this.numberOfPrograms = numberOfPrograms;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @return order of the programs after completing their dance
+     */
+    @Override
+    public String solve(Path inputFilePath) throws IOException {
+        List<DanceMove> moves = DanceMove.parse(inputFilePath);
+
+        List<Program> dancers = IntStream.range(0, numberOfPrograms).map(i -> 'a' + i)
+                .mapToObj(c -> new Program((char) c)).collect(Collectors.toList());
+
+        for (int i = 0; i != iterations; i++) { 
+            if (LOGGER.isDebugEnabled() && i % 100 == 0) {
+                LOGGER.debug("{} dances completed: {}", toString(dancers));
+            }
+            
+            for (DanceMove move : moves) {
+                dancers = move.perform(dancers);
+            }
+        }
+
+        return toString(dancers);
+    }
+    
+    private String toString(List<Program> dancers) {
+        return dancers.stream().map(Object::toString).collect(Collectors.joining());
+    }
+}
--- src/main/java/nl/mvdr/adventofcode/adventofcode2017/day16/PermutationPromenadePart1.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2017/day16/PermutationPromenadePart1.java
@@ -1,15 +1,7 @@
 package nl.mvdr.adventofcode.adventofcode2017.day16;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import nl.mvdr.adventofcode.PathSolver;
 
 /**
  * Solution to the day 16 puzzle of 2017's Advent of Code:
@@ -17,12 +9,9 @@ import nl.mvdr.adventofcode.PathSolver;
  *
  * @author Martijn van de Rijdt
  */
-public class PermutationPromenadePart1 implements PathSolver<String> {
+public class PermutationPromenadePart1 extends PermutationPromenade {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PermutationPromenadePart1.class);
-    
-    /** The number of programs performing the dance. */
-    private final int numberOfPrograms;
     
     /** Constructor. */
     public PermutationPromenadePart1() {
@@ -35,31 +24,7 @@ public class PermutationPromenadePart1 implements PathSolver<String> {
      * @param numberOfPrograms number of programs performing the dance
      */
     PermutationPromenadePart1(int numberOfPrograms) {
-        super();
-        this.numberOfPrograms = numberOfPrograms;
-    }
-    
-    /**
-     * {@inheritDoc}
-     * 
-     * @return order of the programs after completing their dance
-     */
-    @Override
-    public String solve(Path inputFilePath) throws IOException {
-        List<DanceMove> moves = DanceMove.parse(inputFilePath);
-        
-        List<Program> dancers = IntStream.range(0, numberOfPrograms)
-                .map(i -> 'a' + i)
-                .mapToObj(c -> new Program((char) c))
-                .collect(Collectors.toList());
-        
-        for (DanceMove move : moves) {
-            dancers = move.perform(dancers);
-        }
-        
-        return dancers.stream()
-                .map(Object::toString)
-                .collect(Collectors.joining());
+        super(1, numberOfPrograms);
     }
     
     /**
