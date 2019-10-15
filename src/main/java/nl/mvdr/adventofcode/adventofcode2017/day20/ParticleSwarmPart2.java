acf84ed3a756dc195846c973f1a085262b625e4c
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-10-15 14:16:45 +0200

    Solved day 20 part 2

--- src/main/java/nl/mvdr/adventofcode/adventofcode2017/day20/ParticleSwarmPart2.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2017/day20/ParticleSwarmPart2.java
@@ -2,13 +2,17 @@ package nl.mvdr.adventofcode.adventofcode2017.day20;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import nl.mvdr.adventofcode.PathSolver;
+import nl.mvdr.adventofcode.point.Point3D;
 
 /**
  * Solution to the day 20 puzzle of 2017's Advent of Code:
@@ -36,12 +40,26 @@ public class ParticleSwarmPart2 implements PathSolver<Integer> {
                     .map(Particle::tick)
                     .collect(Collectors.toList());
             
-            // TODO resolve collisions
+            removeCollisions(particles);
             
             LOGGER.debug("Particles: {}", particles);
         }
         
         return Integer.valueOf(particles.size());
+    }
+    
+    private void removeCollisions(List<Particle> particles) {
+        Map<Point3D, Integer> positionCounts = new HashMap<>();
+        particles.stream()
+            .map(Particle::getPosition)
+            .forEach(position -> {
+                int newCount = positionCounts.getOrDefault(position, Integer.valueOf(0)).intValue() + 1;
+                positionCounts.put(position, Integer.valueOf(newCount));
+            });
+        positionCounts.entrySet().stream()
+                .filter(entry -> 1 < entry.getValue().intValue())
+                .map(Entry::getKey)
+                .forEach(collisionPosition -> particles.removeIf(particle -> particle.getPosition().equals(collisionPosition)));
     }
     
     /**
--- src/test/java/nl/mvdr/adventofcode/adventofcode2017/day20/ParticleSwarmPart2Test.java
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2017/day20/ParticleSwarmPart2Test.java
@@ -31,6 +31,6 @@ public class ParticleSwarmPart2Test extends SolverTest<ParticleSwarmPart2> {
     /** Test case based on the accepted solution. */
     @Test
     public void testSolution() {
-        assertSolution("?", "input-day20-2017.txt");
+        assertSolution("471", "input-day20-2017.txt");
     }
 }
