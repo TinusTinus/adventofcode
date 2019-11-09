72d95505372721474591aa58629db11c7c9b296a
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-11-09 21:58:38 +0100

    Solved day 5, part 2

---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2016/day05/ChessPart2.java
@@ -0,0 +1,83 @@
+package nl.mvdr.adventofcode.adventofcode2016.day05;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nl.mvdr.adventofcode.PathSolver;
+
+/**
+ * Solution to the day 5 puzzle of 2016's Advent of Code:
+ * <a href="https://adventofcode.com/2016/day/5">How About a Nice Game of Chess?</a>.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class ChessPart2 implements PathSolver<String> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChessPart2.class);
+    
+    /**
+     * {@inheritDoc}
+     * 
+     * @return eight-character password for the second door
+     */
+    @Override
+    public String solve(Path inputFilePath) throws IOException {
+        String doorId = Files.lines(inputFilePath).findFirst().get();
+        
+        // map containing, for each index, the computed character in that position
+        Map<Integer, Character> result = new HashMap<>();
+        
+        // consider all integers, starting from 0
+        IntStream.range(0, Integer.MAX_VALUE)
+            // append the number to the door id
+            .mapToObj(i -> doorId + i)
+            // compute md5 hash
+            .map(DigestUtils::md5Hex)
+            .peek(LOGGER::debug)
+            // only consider values where the hash starts with five zeroes
+            .filter(hash -> hash.startsWith("00000"))
+            // only consider values where the sixth character is in position 0-7
+            .filter(hash -> '0' <= hash.charAt(5))
+            .filter(hash -> hash.charAt(5) < '8')
+            // only consider the first result for each position
+            .filter(hash -> !result.containsKey(Integer.valueOf("" + hash.charAt(5))))
+            // insert into the result map
+            .peek(hash -> result.put(Integer.valueOf("" + hash.charAt(5)), Character.valueOf(hash.charAt(6))))
+            // log intermediate results for extra pride
+            .peek(hash -> LOGGER.info(toString(result)))
+            // limit the password to the first 8 resulting characters
+            .limit(8)
+            .collect(Collectors.joining());
+        
+        return toString(result);
+    }
+    
+    private static String toString(Map<Integer, Character> partialPassword) {
+        return IntStream.range(0, 8)
+                .mapToObj(i -> partialPassword.getOrDefault(Integer.valueOf(i), Character.valueOf('_')))
+                .map(Object::toString)
+                .collect(Collectors.joining());
+    }
+    
+    /**
+     * Main method.
+     * 
+     * @param args commandline arguments; these are ignored
+     */
+    public static void main(String[] args) {
+        ChessPart2 instance = new ChessPart2();
+
+        String result = instance.solve("input-day05-2016.txt");
+
+        LOGGER.info(result);
+    }
+}
---
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2016/day05/ChessPart2Test.java
@@ -0,0 +1,30 @@
+package nl.mvdr.adventofcode.adventofcode2016.day05;
+
+import org.junit.jupiter.api.Test;
+
+import nl.mvdr.adventofcode.SolverTest;
+
+/**
+ * Unit test cases for {@link ChessPart2}.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class ChessPart2Test extends SolverTest<ChessPart2> {
+
+    /** Constructor. */
+    public ChessPart2Test() {
+        super(ChessPart2.class);
+    }
+
+    /** Test case based on the example from the puzzle text. */
+    @Test
+    public void testExample() {
+        assertSolution("05ace8e3", "example-day05-2016.txt");
+    }
+
+    /** Test case based on the accepted solution. */
+    @Test
+    public void testSolution() {
+        assertSolution("863dde27", "input-day05-2016.txt");
+    }
+}
