d23ae1e7514e7686f3c93c63031438f7a5da642e
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-11-09 21:18:29 +0100

    Solved day 5 part 1

---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2016/day05/ChessPart1.java
@@ -0,0 +1,75 @@
+package nl.mvdr.adventofcode.adventofcode2016.day05;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.apache.commons.codec.binary.Hex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nl.mvdr.adventofcode.PathSolver;
+
+/**
+ * Solution to the day 5 puzzle of 2016's Advent of Code:
+ * <a href="https://adventofcode.com/2016/day/5">How About a Nice Game of Chess?</a>.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class ChessPart1 implements PathSolver<String> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChessPart1.class);
+    
+    /**
+     * {@inheritDoc}
+     * 
+     * @return eight-character password
+     */
+    @Override
+    public String solve(Path inputFilePath) throws IOException {
+        String doorId = Files.lines(inputFilePath).findFirst().get();
+        
+        MessageDigest md5;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException(e);
+        }
+        
+        // consider all integers, starting from 0
+        return IntStream.range(0, Integer.MAX_VALUE)
+            // append the number to the door id
+            .mapToObj(i -> doorId + i)
+            // compute md5 hash
+            .map(String::getBytes)
+            .map(md5::digest)
+            // encode as a hexadecimal string
+            .map(Hex::encodeHexString)
+            .peek(LOGGER::debug)
+            // only consider values where the hash starts with five zeroes
+            .filter(hash -> hash.startsWith("00000"))
+            // take the sixth character
+            .map(hash -> Character.valueOf(hash.charAt(5)))
+            .map(Object::toString)
+            // limit the password to the first 8 resulting characters
+            .limit(8)
+            .collect(Collectors.joining());
+    }
+    
+    /**
+     * Main method.
+     * 
+     * @param args commandline arguments; these are ignored
+     */
+    public static void main(String[] args) {
+        ChessPart1 instance = new ChessPart1();
+
+        String result = instance.solve("input-day05-2016.txt");
+
+        LOGGER.info(result);
+    }
+}
