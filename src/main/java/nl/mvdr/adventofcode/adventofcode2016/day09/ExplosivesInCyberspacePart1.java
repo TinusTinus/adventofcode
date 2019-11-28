c3659a7e9a2787b2a1ce6acc0460a710d871c72c
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-11-28 21:07:58 +0100

    Solved day 9 part 1

---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2016/day09/ExplosivesInCyberspacePart1.java
@@ -0,0 +1,73 @@
+package nl.mvdr.adventofcode.adventofcode2016.day09;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nl.mvdr.adventofcode.IntSolver;
+
+/**
+ * Solution to the day 9 puzzle of 2016's Advent of Code:
+ * <a href="https://adventofcode.com/2016/day/9">Explosives in Cyberspace</a>.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class ExplosivesInCyberspacePart1 implements IntSolver {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExplosivesInCyberspacePart1.class);
+    
+    /**
+     * {@inheritDoc}
+     * 
+     * @return number of lit pixels
+     */
+    @Override
+    public int solve(Stream<String> lines) {
+        String remainingText = lines.findFirst().orElseThrow();
+        int result = 0;
+        
+        while (0 < remainingText.length()) {
+            int index = remainingText.indexOf("(");
+            if (index == -1) {
+                // No more opening brackets.
+                result += remainingText.length();
+                remainingText = "";
+            } else if (index == 0) {
+                // remainingText starts with "("; this may be the start of a compressed sequence.
+                Pattern pattern = Pattern.compile("\\((\\d*)x(\\d*)\\)(.*)");
+                Matcher matcher = pattern.matcher(remainingText);
+                if (matcher.matches()) {
+                    // Start of a compressed sequence!
+                    int characters = Integer.parseInt(matcher.group(1));
+                    int repeats = Integer.parseInt(matcher.group(2));
+                    result += characters * repeats;
+                    remainingText = matcher.group(3).substring(characters);
+                } else {
+                    // Just an opening bracket, not the start of a compressed sequence.
+                    result++;
+                    remainingText = remainingText.substring(1);
+                }
+            } else {
+                result += index;
+                remainingText = remainingText.substring(index);
+            }
+        }
+        
+        return result;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        ExplosivesInCyberspacePart1 instance = new ExplosivesInCyberspacePart1();
+
+        String result = instance.solve("input-day09-2016.txt");
+
+        LOGGER.info(result);
+    }
+
+}
---
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2016/day09/ExplosivesInCyberspacePart1Test.java
@@ -0,0 +1,65 @@
+package nl.mvdr.adventofcode.adventofcode2016.day09;
+
+import org.junit.jupiter.api.Test;
+
+import nl.mvdr.adventofcode.SolverTest;
+
+/**
+ * Unit test cases for {@link ExplosivesInCyberspacePart1}.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class ExplosivesInCyberspacePart1Test extends SolverTest<ExplosivesInCyberspacePart1> {
+
+    /** Constructor. */
+    public ExplosivesInCyberspacePart1Test() {
+        super(ExplosivesInCyberspacePart1.class);
+    }
+
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample0() {
+        assertSolution("6", "example-day09-2016-0.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample1() {
+        assertSolution("7", "example-day09-2016-1.txt");
+    }
+
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample2() {
+        assertSolution("9", "example-day09-2016-2.txt");
+    }
+
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample3() {
+        assertSolution("11", "example-day09-2016-3.txt");
+    }
+
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample4() {
+        assertSolution("6", "example-day09-2016-4.txt");
+    }
+
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample5() {
+        assertSolution("18", "example-day09-2016-5.txt");
+    }
+
+    
+    /** Test case based on the accepted solution. */
+    @Test
+    public void testSolution() {
+        assertSolution("183269", "input-day09-2016.txt");
+    }
+}
