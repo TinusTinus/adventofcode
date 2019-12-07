543da65211fcfd9e3a9a84dbcfe8b0b4817e50b8
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-12-07 23:22:04 +0100

    Started on part 2

--- src/main/java/nl/mvdr/adventofcode/adventofcode2019/day07/AmplificationCircuitPart2.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2019/day07/AmplificationCircuitPart2.java
@@ -1,6 +1,5 @@
 package nl.mvdr.adventofcode.adventofcode2019.day07;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
@@ -17,9 +16,9 @@ import nl.mvdr.adventofcode.adventofcode2019.intcode.Program;
  *
  * @author Martijn van de Rijdt
  */
-public class AmplificationCircuitPart1 implements IntSolver {
+public class AmplificationCircuitPart2 implements IntSolver {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(AmplificationCircuitPart1.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(AmplificationCircuitPart2.class);
     
     /**
      * {@inheritDoc}
@@ -30,7 +29,7 @@ public class AmplificationCircuitPart1 implements IntSolver {
     public int solve(Stream<String> lines) {
         Program program = Program.parse(lines.findFirst().orElseThrow());
         
-        Set<List<Integer>> phaseSettingSequences = PhaseSettingSequences.generatePhaseSettingSequences(0, 5); 
+        Set<List<Integer>> phaseSettingSequences = PhaseSettingSequences.generatePhaseSettingSequences(5, 10); 
         
         LOGGER.debug("Phase setting sequences: {}", phaseSettingSequences);
         
@@ -48,26 +47,8 @@ public class AmplificationCircuitPart1 implements IntSolver {
      * @return thruster signal
      */
     private int computeThrusterSignal(Program initialProgram, List<Integer> phaseSettingSequence) {
-        int result = 0;
-        for (Integer phaseSetting : phaseSettingSequence) {
-            List<Integer> outputValues = new ArrayList<>();
-            
-            initialProgram.withInput(phaseSetting.intValue(), result)
-                    .withOutput(outputValues::add)
-                    .execute();
-            
-            // There should be exactly 1 output value
-            if (outputValues.size() != 1) {
-                throw new IllegalStateException("Unexpected output: " + outputValues);
-            }
-            result = outputValues.get(0).intValue();
-        }
-        
-        if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("Phase setting sequence {}: thruster signal {}", phaseSettingSequence, Integer.valueOf(result));
-        }
-        
-        return result;
+        // TODO implement
+        return 0;
     }
 
     /**
@@ -76,7 +57,7 @@ public class AmplificationCircuitPart1 implements IntSolver {
      * @param args commandline arguments; these are ignored
      */
     public static void main(String[] args) {
-        AmplificationCircuitPart1 instance = new AmplificationCircuitPart1();
+        AmplificationCircuitPart2 instance = new AmplificationCircuitPart2();
 
         String result = instance.solve("input-day07-2019.txt");
 
---
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2019/day07/AmplificationCircuitPart2Test.java
@@ -0,0 +1,36 @@
+package nl.mvdr.adventofcode.adventofcode2019.day07;
+
+import org.junit.jupiter.api.Test;
+
+import nl.mvdr.adventofcode.SolverTest;
+
+/**
+ * Unit test cases for {@link AmplificationCircuitPart2}.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class AmplificationCircuitPart2Test extends SolverTest<AmplificationCircuitPart2> {
+
+    /** Constructor. */
+    public AmplificationCircuitPart2Test() {
+        super(AmplificationCircuitPart2.class);
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample3() {
+        assertSolution("139629729", "example-day07-2019-3.txt");
+    }
+
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample4() {
+        assertSolution("18216", "example-day07-2019-4.txt");
+    }
+
+    /** Test case based on the accepted solution. */
+    @Test
+    public void testSolution() {
+        assertSolution("?", "input-day07-2019.txt"); 
+    }
+}
