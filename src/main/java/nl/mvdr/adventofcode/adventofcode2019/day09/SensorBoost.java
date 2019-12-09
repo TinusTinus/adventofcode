b7ad8f6fbaed513662bdd00917531da2759e45b1
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-12-09 20:37:24 +0100

    Refactoring

---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2019/day09/SensorBoost.java
@@ -0,0 +1,55 @@
+package nl.mvdr.adventofcode.adventofcode2019.day09;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.LongConsumer;
+import java.util.function.LongSupplier;
+import java.util.stream.Stream;
+
+import nl.mvdr.adventofcode.LongSolver;
+import nl.mvdr.adventofcode.adventofcode2019.intcode.Program;
+
+/**
+ * Solution to the day 9 puzzle of 2019's Advent of Code:
+ * <a href="https://adventofcode.com/2019/day/9">Sensor Boost</a>.
+ *
+ * @author Martijn van de Rijdt
+ */
+abstract class SensorBoost implements LongSolver {
+
+    private final long inputValue;
+    
+    /**
+     * Constructor.
+     * 
+     * @param inputValue the input value for the BOOST program
+     */
+    SensorBoost(long inputValue) {
+        super();
+        this.inputValue = inputValue;
+    }
+    
+    /**
+     * {@inheritDoc}
+     * 
+     * @return value left at position 0 after the program halts
+     */
+    @Override
+    public long solve(Stream<String> lines) {
+        String programText = lines.findFirst().orElseThrow();
+        
+        LongSupplier input = () -> inputValue;
+        
+        List<Long> outputValues = new ArrayList<>();
+        LongConsumer output = outputValue -> outputValues.add(Long.valueOf(outputValue));
+        
+        Program program = Program.parse(programText, input, output);
+        program.execute();
+
+        if (outputValues.size() != 1) {
+            throw new IllegalStateException("BOOST output: " + outputValues);
+        }
+        
+        return outputValues.get(0).longValue();
+    }
+}
--- src/main/java/nl/mvdr/adventofcode/adventofcode2019/day09/SensorBoostPart1.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2019/day09/SensorBoostPart1.java
@@ -1,16 +1,7 @@
 package nl.mvdr.adventofcode.adventofcode2019.day09;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.LongConsumer;
-import java.util.function.LongSupplier;
-import java.util.stream.Stream;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import nl.mvdr.adventofcode.LongSolver;
-import nl.mvdr.adventofcode.adventofcode2019.intcode.Program;
 
 /**
  * Solution to the day 9 puzzle of 2019's Advent of Code:
@@ -18,32 +9,13 @@ import nl.mvdr.adventofcode.adventofcode2019.intcode.Program;
  *
  * @author Martijn van de Rijdt
  */
-public class SensorBoostPart1 implements LongSolver {
+public class SensorBoostPart1 extends SensorBoost {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SensorBoostPart1.class);
     
-    /**
-     * {@inheritDoc}
-     * 
-     * @return value left at position 0 after the program halts
-     */
-    @Override
-    public long solve(Stream<String> lines) {
-        String programText = lines.findFirst().orElseThrow();
-        
-        LongSupplier input = () -> 1L;
-        
-        List<Long> outputValues = new ArrayList<>();
-        LongConsumer output = outputValue -> outputValues.add(Long.valueOf(outputValue));
-        
-        Program program = Program.parse(programText, input, output);
-        program.execute();
-
-        if (outputValues.size() != 1) {
-            throw new IllegalStateException("BOOST output: " + outputValues);
-        }
-        
-        return outputValues.get(0).longValue();
+    /** Constructor. */
+    public SensorBoostPart1() {
+        super(1L);
     }
     
     /**
