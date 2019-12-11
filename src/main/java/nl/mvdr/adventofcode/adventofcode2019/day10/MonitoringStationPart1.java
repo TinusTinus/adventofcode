6baa02e0b1756ad106afb8a77bdaf6268351a3b8
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-12-11 18:22:05 +0100

    Partial solution

--- src/main/java/nl/mvdr/adventofcode/adventofcode2019/day10/MonitoringStationPart1.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2019/day10/MonitoringStationPart1.java
@@ -1,11 +1,16 @@
 package nl.mvdr.adventofcode.adventofcode2019.day10;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import nl.mvdr.adventofcode.IntSolver;
+import nl.mvdr.adventofcode.point.Point;
 
 /**
  * Solution to the day 10 puzzle of 2019's Advent of Code:
@@ -24,7 +29,49 @@ public class MonitoringStationPart1 implements IntSolver {
      */
     @Override
     public int solve(Stream<String> lines) {
-        return 0; // TODO
+        Set<Point> asteroids = parse(lines);
+        
+        return asteroids.stream()
+                .mapToInt(asteroid -> computeVisibleAsteroids(asteroid, asteroids))
+                .max()
+                .getAsInt();
+    }
+
+    /**
+     * Parses the contents of the input into a set of asteroid locations.
+     * 
+     * @param linesStream puzzle input
+     * @return locations of the asteroids in the puzzle input
+     */
+    private Set<Point> parse(Stream<String> linesStream) {
+        Set<Point> asteroids = new HashSet<>();
+        List<String> lines = linesStream.collect(Collectors.toList());
+        for (int y = 0; y != lines.size(); y++) {
+            String line = lines.get(y);
+            for (int x = 0; x != line.length(); x++) {
+                char c = line.charAt(x);
+                if (c == '#') {
+                    asteroids.add(new Point(x, y));
+                }
+            }
+        }
+        return Set.copyOf(asteroids);
+    }
+    
+    /**
+     * Computes the number of asteroids visible from the given point.
+     * 
+     * Note that the set of asteroids includes the point itself, but that point should not be counted.
+     * 
+     * @param point point
+     * @param asteroids all asteroids
+     * @return number of visible asteroids
+     */
+    private int computeVisibleAsteroids(Point point, Set<Point> asteroids) {
+        Set<Point> otherAsteroids = new HashSet<>(asteroids);
+        otherAsteroids.remove(point);
+        
+        return 0; // TODO implement
     }
     
     /**
