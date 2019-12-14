0db0a8954c129ac7b0b857d19eb8e1ad0f595a36
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-12-14 23:22:23 +0100

    Refactoring

--- src/main/java/nl/mvdr/adventofcode/adventofcode2019/day12/Moon.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2019/day12/Moon.java
@@ -49,29 +49,25 @@ class Moon {
         
         for (Moon moon0 : system) {
             for (Moon moon1 : system) {
+                Point3D point = new Point3D(0, 0, 0);
                 if (moon0.getLocation().getX() < moon1.getLocation().getX()) {
-                    newVelocities.merge(moon0, new Point3D(1, 0, 0), Point3D::add);
-                    newVelocities.merge(moon1, new Point3D(-1, 0, 0), Point3D::add);
-                } else if (moon0.getLocation().getX() < moon1.getLocation().getX()) {
-                    newVelocities.merge(moon0, new Point3D(-1, 0, 0), Point3D::add);
-                    newVelocities.merge(moon1, new Point3D(1, 0, 0), Point3D::add);
+                    point = point.add(new Point3D(1, 0, 0));
+                } else if (moon1.getLocation().getX() < moon0.getLocation().getX()) {
+                    point = point.add(new Point3D(-1, 0, 0));
                 }
                 
                 if (moon0.getLocation().getY() < moon1.getLocation().getY()) {
-                    newVelocities.merge(moon0, new Point3D(0, 1, 0), Point3D::add);
-                    newVelocities.merge(moon1, new Point3D(0, -1, 0), Point3D::add);
-                } else if (moon0.getLocation().getY() < moon1.getLocation().getY()) {
-                    newVelocities.merge(moon0, new Point3D(0, -1, 0), Point3D::add);
-                    newVelocities.merge(moon1, new Point3D(0, 1, 0), Point3D::add);
+                    point = point.add(new Point3D(0, 1, 0));
+                } else if (moon1.getLocation().getY() < moon0.getLocation().getY()) {
+                    point = point.add(new Point3D(0, -1, 0));
                 }
                 
                 if (moon0.getLocation().getZ() < moon1.getLocation().getZ()) {
-                    newVelocities.merge(moon0, new Point3D(0, 0, 1), Point3D::add);
-                    newVelocities.merge(moon1, new Point3D(0, 0, -1), Point3D::add);
-                } else if (moon0.getLocation().getZ() < moon1.getLocation().getZ()) {
-                    newVelocities.merge(moon0, new Point3D(0, 0, -1), Point3D::add);
-                    newVelocities.merge(moon1, new Point3D(0, 0, 1), Point3D::add);
+                    point = point.add(new Point3D(0, 0, 1));
+                } else if (moon1.getLocation().getZ() < moon0.getLocation().getZ()) {
+                    point = point.add(new Point3D(0, 0, -1));
                 }
+                newVelocities.merge(moon0, point, Point3D::add);
             }
         }
         
