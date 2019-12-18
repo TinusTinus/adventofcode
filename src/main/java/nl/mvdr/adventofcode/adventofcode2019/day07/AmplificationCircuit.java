e770c699d17d3d670aeee92217b624d0a59c6b07
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-12-18 22:10:42 +0100

    Fiddling with powersets

--- src/main/java/nl/mvdr/adventofcode/adventofcode2019/day07/AmplificationCircuit.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2019/day07/AmplificationCircuit.java
@@ -70,7 +70,7 @@ abstract class AmplificationCircuit implements LongSolver {
                 .boxed()
                 .collect(Collectors.toSet());
         
-        return Permutations.generateAllPermutations(phaseSettings);
+        return Permutations.generatePermutations(phaseSettings);
     }
     
     /**
--- src/main/java/nl/mvdr/adventofcode/permutations/Permutations.java
+++ src/main/java/nl/mvdr/adventofcode/permutations/Permutations.java
@@ -23,7 +23,7 @@ public class Permutations {
      * @param values values
      * @return stream of all possible permutations of the given values
      */
-    public static <T> Stream<List<T>> generateAllPermutations(Set<T> values) {
+    public static <T> Stream<List<T>> generatePermutations(Set<T> values) {
         Stream<List<T>> result;
         
         if (values.isEmpty()) {
@@ -37,13 +37,43 @@ public class Permutations {
                         Set<T> remaining = new HashSet<>(values);
                         remaining.remove(value);
                         // Recursively determine all permutations of the remaining values
-                        return generateAllPermutations(remaining).map(permutation -> {
+                        return generatePermutations(remaining).map(permutation -> {
                             List<T> resultPermutation = new ArrayList<>(permutation.size() + 1);
                             resultPermutation.add(value);
                             resultPermutation.addAll(permutation);
                             return resultPermutation;
                         });
                     });
+        }
+        return result;
+    }
+    
+    /**
+     * Generates a powerset, that is: a set containing all subsets of the given set.
+     * 
+     * @param <T> value type
+     * @param values values
+     * @return stream containing the powerset of the given set
+     */
+    public static <T> Stream<Set<T>> powerSet(Set<T> values) {
+        Stream<Set<T>> result;
+        
+        if (values.isEmpty()) {
+            result = Stream.of(Set.of());
+        } else {
+            T value = values.iterator().next();
+            
+            Set<T> remaining = new HashSet<>(values);
+            remaining.remove(value);
+            
+            result = powerSet(remaining)
+                    .flatMap(set -> {
+                        Set<T> otherSet = new HashSet<>(set);
+                        otherSet.add(value);
+                        
+                        return Stream.of(set, otherSet);
+                    });
+            
         }
         return result;
     }
