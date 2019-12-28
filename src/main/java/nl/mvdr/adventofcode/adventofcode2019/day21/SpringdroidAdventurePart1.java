142f411deec0fde3d1ba399b555be43384012d5e
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-12-28 22:04:01 +0100

    Solved 2019 day 21 part 1

--- src/main/java/nl/mvdr/adventofcode/adventofcode2019/day21/SpringdroidAdventurePart1.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2019/day21/SpringdroidAdventurePart1.java
@@ -39,9 +39,7 @@ public class SpringdroidAdventurePart1 implements LongSolver {
     /** @return input for the springdroid */
     private String getSpringScript() {
         return """
-               NOT B T
                NOT C J
-               AND T J
                AND D J
                NOT A T
                OR T J
--- src/test/java/nl/mvdr/adventofcode/adventofcode2019/day21/SpringDroidAdventurePart1Test.java
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2019/day21/SpringDroidAdventurePart1Test.java
@@ -19,6 +19,6 @@ public class SpringDroidAdventurePart1Test extends SolverTest<SpringdroidAdventu
     /** Test case based on the accepted solution. */
     @Test
     public void testSolution() {
-        assertSolution("?", "input-day21-2019.txt"); 
+        assertSolution("19359752", "input-day21-2019.txt"); 
     }
 }
