3a7836a5a93bd879304d3a5d469627b3880c6e50
Martijn van de Rijdt <mvdrijdt@planet.nl>
2019-12-28 17:31:46 +0100

    Implemented input parsing partially

--- src/main/java/nl/mvdr/adventofcode/adventofcode2019/day20/Maze.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2019/day20/Maze.java
@@ -1,10 +1,15 @@
 package nl.mvdr.adventofcode.adventofcode2019.day20;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import nl.mvdr.adventofcode.point.Point;
 
@@ -15,20 +20,36 @@ import nl.mvdr.adventofcode.point.Point;
  */
 class Maze {
     
+    private static final Logger LOGGER = LoggerFactory.getLogger(Maze.class);
+
+    private final Point start;
+    private final Point finish;
+    private final Set<Point> openPassages;
+    // TODO portals
     
+    /**
+     * Parses the given input into a maze.
+     * 
+     * @param lines contents of the puzzle input
+     * @return maze
+     */
     static Maze parse(List<String> lines) {
         // Build a map containing all open passages (marked as '.' on the map) as its keys,
         // and their labels as its values.
-        Map<Point, Optional<String>> openPassages = new HashMap<>();
+        Set<Point> openPassages = new HashSet<>();
+        
+        Map<String, Set<Point>> labels = new HashMap<>();
         
         for (int y = 0; y != lines.size(); y++) {
             String line = lines.get(y);
-            for (int x = 0; x != line.length(); y++) {
+            for (int x = 0; x != line.length(); x++) {
                 if (line.charAt(x) == '.') {
                     Point point = new Point(x, y);
                     
+                    openPassages.add(point);
+                    
                     Optional<String> label;
-                    if (Character.isUpperCase(line.charAt(x - 2)) && Character.isUpperCase(line.charAt(x - 2))) {
+                    if (Character.isUpperCase(line.charAt(x - 2)) && Character.isUpperCase(line.charAt(x - 1))) {
                         // there is a label to the left of this passage
                         label = Optional.of(line.substring(x - 2, x));
                     } else if (Character.isUpperCase(line.charAt(x + 1)) && Character.isUpperCase(line.charAt(x + 2))) {
@@ -36,38 +57,40 @@ class Maze {
                         label = Optional.of(line.substring(x + 1, x + 3));
                     } else if (Character.isUpperCase(lines.get(y - 2).charAt(x)) && Character.isUpperCase(lines.get(y - 1).charAt(x))) {
                         // there is a label above this passage
-                        label = Optional.of("" + Character.isUpperCase(lines.get(y - 2).charAt(x)) + Character.isUpperCase(lines.get(y - 1).charAt(x)));
+                        label = Optional.of("" + lines.get(y - 2).charAt(x) + lines.get(y - 1).charAt(x));
                     } else if (Character.isUpperCase(lines.get(y + 1).charAt(x)) && Character.isUpperCase(lines.get(y + 2).charAt(x))) {
                         // there is a label below this passage
-                        label = Optional.of("" + Character.isUpperCase(lines.get(y + 1).charAt(x)) + Character.isUpperCase(lines.get(y + 2).charAt(x)));
+                        label = Optional.of("" + lines.get(y + 1).charAt(x) + lines.get(y + 2).charAt(x));
                     } else {
                         // no label found
                         label = Optional.empty();
                     }
+                    label.ifPresent(name -> labels.computeIfAbsent(name, k -> new HashSet<>()).add(point));
                     
-                    openPassages.put(point, label);
                 }
             }
         }
         
-        Point start = openPassages.entrySet()
-                .stream()
-                .filter(entry -> entry.getValue().equals("AA"))
-                .map(Entry::getKey)
-                .findFirst()
-                .orElseThrow();
+        LOGGER.debug("Open passages found: {}", openPassages);
+        LOGGER.debug("Labels: {}", labels);
         
-        Point finish = openPassages.entrySet()
-                .stream()
-                .filter(entry -> entry.getValue().equals("ZZ"))
-                .map(Entry::getKey)
-                .findFirst()
-                .orElseThrow();
+        Point start = labels.remove("AA").iterator().next();
+        Point finish = labels.remove("ZZ").iterator().next();
         
-        return new Maze(); // TODO
+        Set<Portal> portals = labels.values()
+                .stream()
+                .map(Set::iterator)
+                .map(iterator -> new Portal(iterator.next(), iterator.next()))
+                .collect(Collectors.toSet());
+        
+        return new Maze(start, finish, openPassages);
+    }
+
+    private Maze(Point start, Point finish, Set<Point> openPassages) {
+        super();
+        this.start = start;
+        this.finish = finish;
+        this.openPassages = openPassages;
     }
     
-    private Maze() {
-        
-    }
 }
---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2019/day20/Portal.java
@@ -0,0 +1,27 @@
+package nl.mvdr.adventofcode.adventofcode2019.day20;
+
+import nl.mvdr.adventofcode.point.Point;
+
+/**
+ * A portal connecting two points through teleportation.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class Portal {
+    // The two points connected by this portal.
+    // Note that the order of these points should not matter.
+    private final Point point0;
+    private final Point point1;
+
+    /**
+     * Constructor.
+     * 
+     * @param point0 first point
+     * @param point1 second point
+     */
+    Portal(Point point0, Point point1) {
+        super();
+        this.point0 = point0;
+        this.point1 = point1;
+    }
+}
