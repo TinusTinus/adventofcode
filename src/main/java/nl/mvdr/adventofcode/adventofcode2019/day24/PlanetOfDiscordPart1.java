c1067143e6ab19c7caef97f0be4ab9a192b7e240
Martijn van de Rijdt <mvdrijdt@planet.nl>
2020-05-21 22:19:50 +0200

    Solved 2019 day 24 part 1

---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2019/day24/PlanetOfDiscordPart1.java
@@ -0,0 +1,54 @@
+package nl.mvdr.adventofcode.adventofcode2019.day24;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nl.mvdr.adventofcode.IntSolver;
+
+/**
+ * Solution to the day 24 puzzle of 2019's Advent of Code:
+ * <a href="https://adventofcode.com/2019/day/24">Planet of Discord</a>.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class PlanetOfDiscordPart1 implements IntSolver {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PlanetOfDiscordPart1.class);
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @return the biodiversity rating for the first layout that appears twice 
+     */
+    @Override
+    public int solve(Stream<String> lines) {
+        Layout layout = Layout.parse(lines);
+        
+        Set<Layout> layouts = new HashSet<>();
+        
+        while (!layouts.contains(layout)) {
+            layouts.add(layout);
+            layout = layout.next();
+        }
+        
+        return layout.biodiversity();
+    }
+
+    /**
+     * Main method.
+     * 
+     * @param args commandline arguments; these are ignored
+     */
+    public static void main(String[] args) {
+        PlanetOfDiscordPart1 instance = new PlanetOfDiscordPart1();
+
+        String result = instance.solve("input-day24-2019.txt");
+
+        LOGGER.info(result);
+    }
+}
+ 
---
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2019/day24/DonutMazePart1Test.java
@@ -0,0 +1,60 @@
+package nl.mvdr.adventofcode.adventofcode2019.day24;
+
+import org.junit.jupiter.api.Test;
+
+import nl.mvdr.adventofcode.SolverTest;
+
+/**
+ * Unit test cases for {@link PlanetOfDiscordPart1}.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class DonutMazePart1Test extends SolverTest<PlanetOfDiscordPart1> {
+
+    /** Constructor. */
+    public DonutMazePart1Test() {
+        super(PlanetOfDiscordPart1.class);
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample0() {
+        assertSolution("2129920", "example-day24-2019-0.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample1() {
+        assertSolution("2129920", "example-day24-2019-1.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample2() {
+        assertSolution("2129920", "example-day24-2019-2.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample3() {
+        assertSolution("2129920", "example-day24-2019-3.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample4() {
+        assertSolution("2129920", "example-day24-2019-4.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample5() {
+        assertSolution("2129920", "example-day24-2019-5.txt");
+    }
+    
+    /** Test case based on the accepted solution. */
+    @Test
+    public void testSolution() {
+        assertSolution("28778811", "input-day24-2019.txt"); 
+    }
+}
