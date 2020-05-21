40396d881266566085d29533cbdacef2aef0aa4e
Martijn van de Rijdt <mvdrijdt@planet.nl>
2020-05-21 20:22:56 +0200

    First attempt at solving part 2

--- src/main/java/nl/mvdr/adventofcode/adventofcode2019/day23/CategorySixPart2.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2019/day23/CategorySixPart2.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.OptionalLong;
 import java.util.Queue;
 import java.util.Set;
 import java.util.stream.LongStream;
@@ -22,14 +23,14 @@ import nl.mvdr.adventofcode.adventofcode2019.intcode.Program;
  *
  * @author Martijn van de Rijdt
  */
-public class CategorySixPart1 implements LongSolver {
+public class CategorySixPart2 implements LongSolver {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CategorySixPart1.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(CategorySixPart2.class);
 
     /**
      * {@inheritDoc}
      * 
-     * @return the Y value of the first packet sent to address 255
+     * @return the first Y value delivered by the NAT to the computer at address 0 twice in a row
      */
     @Override
     public long solve(Stream<String> lines) {
@@ -48,13 +49,28 @@ public class CategorySixPart1 implements LongSolver {
                 .map(computer -> computer.withOutput(new OutputHandler(inputs, nat)::handleOutput))
                 .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
         
+        OptionalLong previousY = OptionalLong.empty();
+        boolean done = false;
+        
         int i = 0;
-        while (!nat.containsValue()) {
-            Program computer = computers.get(i);
-            computer = computer.executeInstruction();
-            computer = computer.executeUntilNextInput();
-            computers.set(i, computer);
-            i = (i + 1) % computers.size();
+        while (!done) {
+            if (nat.containsValue() && inputs.values().stream().allMatch(Queue::isEmpty)) {
+                // No more inputs: all computers are idling.
+                if (previousY.isPresent() && nat.getY() == previousY.getAsLong()) {
+                    // Same y value twice in a row
+                    done = true;
+                } else {
+                    Queue<Long> targetQueue = inputs.get(Long.valueOf(0L));
+                    targetQueue.add(Long.valueOf(nat.getX()));
+                    targetQueue.add(Long.valueOf(nat.getY()));
+                }
+            } else {
+                Program computer = computers.get(i);
+                computer = computer.executeInstruction();
+                computer = computer.executeUntilNextInput();
+                computers.set(i, computer);
+                i = (i + 1) % computers.size();
+            }
         }
         
         return nat.getY();
@@ -66,7 +82,7 @@ public class CategorySixPart1 implements LongSolver {
      * @param args commandline arguments; these are ignored
      */
     public static void main(String[] args) {
-        CategorySixPart1 instance = new CategorySixPart1();
+        CategorySixPart2 instance = new CategorySixPart2();
 
         String result = instance.solve("input-day23-2019.txt");
 
