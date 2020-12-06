5e727a2c4d78c54ff0a2ad3ce3b535fa8d06105c
Martijn van de Rijdt <mvdrijdt@planet.nl>
2020-12-06 01:05:15 +0100

    Refactoring: improved naming

--- src/main/java/nl/mvdr/adventofcode/adventofcode2020/day02/PasswordPolicy.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2020/day02/PasswordPolicy.java
@@ -5,12 +5,12 @@ import org.apache.commons.lang3.StringUtils;
 /**
  * Representation of a password policy.
  *
- * @param minimum lowest number of times the given letter must appear for the password to be valid
- * @param maximum highest number of times the given letter must appear for the password to be valid
+ * @param i first number in the password policy spec
+ * @param j second number in the password policy spec
  * @param letter letter
  * @author Martijn van de Rijdt
  */
-record PasswordPolicy(int minimum, int maximum, char letter) {
+record PasswordPolicy(int i, int j, char letter) {
     
     /**
      * Determines whether the given password satisfies this policy,
@@ -21,7 +21,7 @@ record PasswordPolicy(int minimum, int maximum, char letter) {
      */
     boolean isValidPart1(String password) {
         int matches = StringUtils.countMatches(password, letter);
-        return minimum <= matches && matches <= maximum;
+        return i <= matches && matches <= j;
     }
     
     /**
@@ -32,7 +32,7 @@ record PasswordPolicy(int minimum, int maximum, char letter) {
      * @return whether the password is valid
      */
     boolean isValidPart2(String password) {
-        return (password.charAt(minimum - 1) == letter && password.charAt(maximum - 1) != letter) || 
-                (password.charAt(minimum - 1) != letter && password.charAt(maximum - 1) == letter);
+        return (password.charAt(i - 1) == letter && password.charAt(j - 1) != letter) || 
+                (password.charAt(i - 1) != letter && password.charAt(j - 1) == letter);
     }
 }
