e7dbccf54a438fe5c392b20fa6a6c72d878f6c59
Martijn van de Rijdt <mvdrijdt@planet.nl>
2020-12-20 20:11:42 +0100

    Solved 2020 day 12 part 1

---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2020/day13/ShuttleSearchPart1.java
@@ -0,0 +1,76 @@
+package nl.mvdr.adventofcode.adventofcode2020.day13;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nl.mvdr.adventofcode.IntSolver;
+
+/**
+ * Solution to the day 13 puzzle of 2020's Advent of Code:
+ * <a href="https://adventofcode.com/2020/day/13">Shuttle Search</a>.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class ShuttleSearchPart1 implements IntSolver {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ShuttleSearchPart1.class);
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @return the ID of the earliest bus we can take to the airport multiplied by the number of minutes we'll need to wait for that bus
+     */
+    @Override
+    public int solve(Stream<String> linesStream) {
+        List<String> lines = linesStream.collect(Collectors.toList());
+        int earliestTimestamp = Integer.parseInt(lines.get(0));
+        int busId = Stream.of(lines.get(1).split(","))
+                .filter(id -> !"x".equals(id))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .min(Comparator.comparing(i -> Integer.valueOf(earliestDepartureAfter(i.intValue(), earliestTimestamp))))
+                .orElseThrow()
+                .intValue();
+        
+        int timestamp = earliestDepartureAfter(busId, earliestTimestamp);
+        int waitTime = timestamp - earliestTimestamp;
+        
+        return busId * waitTime;
+    }
+    
+    /**
+     * Finds the earliest departure time for the given bus, after the given timestamp.
+     * 
+     * That is, the smallest multiple of the given bus ID which is at least the given timestamp
+     * 
+     * @param busId bud ID; also its departure frequency
+     * @param timestamp timestamp
+     * @return earliest departure time
+     */
+    private int earliestDepartureAfter(int busId, int timestamp) {
+        int result = 0;
+        while (result <= timestamp) {
+            result += busId;
+        }
+        return result;
+    }
+
+    /**
+     * Main method.
+     * 
+     * @param args commandline arguments; these are ignored
+     */
+    public static void main(String[] args) {
+        ShuttleSearchPart1 instance = new ShuttleSearchPart1();
+
+        String result = instance.solve("input-day13-2020.txt");
+
+        LOGGER.info(result);
+    }
+}
+ 
---
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2020/day13/ShuttleSearchPart1Test.java
@@ -0,0 +1,37 @@
+package nl.mvdr.adventofcode.adventofcode2020.day13;
+
+import org.junit.jupiter.api.Test;
+
+import nl.mvdr.adventofcode.SolverTest;
+
+/**
+ * Unit test cases for {@link ShuttleSearchPart1}.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class ShuttleSearchPart1Test extends SolverTest<ShuttleSearchPart1> {
+
+    /** Constructor. */
+    public ShuttleSearchPart1Test() {
+        super(ShuttleSearchPart1.class);
+    }
+    
+    /**
+     * Test case based on an example from the puzzle text.
+     * 
+     * The earliest bus you could take is bus ID 59. It doesn't depart until
+     * timestamp 944, so you would need to wait 944 - 939 = 5 minutes before it
+     * departs. Multiplying the bus ID by the number of minutes you'd need to wait
+     * gives 295.
+     */
+    @Test
+    public void testExample() {
+        assertSolution("295", "example-day13-2020.txt");
+    }
+    
+    /** Test case based on the accepted solution. */
+    @Test
+    public void testSolution() {
+        assertSolution("3269", "input-day13-2020.txt");
+    }
+}
