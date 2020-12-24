e6f94eebc411c685c076d2a0f6804817e0138688
Martijn van de Rijdt <mvdrijdt@planet.nl>
2020-12-24 22:44:14 +0100

    Started part 2

---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2020/day14/DockingDataPart2.java
@@ -0,0 +1,75 @@
+package nl.mvdr.adventofcode.adventofcode2020.day14;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nl.mvdr.adventofcode.LongSolver;
+
+/**
+ * Solution to the day 14 puzzle of 2020's Advent of Code:
+ * <a href="https://adventofcode.com/2020/day/14">Docking Data</a>.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class DockingDataPart2 implements LongSolver {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DockingDataPart2.class);
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @return the sum of all values left in memory after the initialisation program completes
+     */
+    @Override
+    public long solve(Stream<String> linesStream) {
+        List<String> instructions = linesStream.filter(Predicate.not(String::isEmpty))
+                .collect(Collectors.toList());
+        
+        Map<Long, Long> memory = new HashMap<>();
+        
+        for (String instruction : instructions) {
+            String[] sides = instruction.split(" = ");
+            if ("mask".equals(sides[0])) {
+                String bitmask = sides[1];
+            } else {
+                String indexString = sides[0].substring(4, sides[0].length() - 1);
+                long index = Long.parseLong(indexString);
+                
+                long value = Long.parseLong(sides[1]);
+                
+                // TODO determine the indexes using the bitmask
+                Set<Long> indexes = Set.of(Long.valueOf(index));
+                
+                indexes.forEach(i -> memory.put(i, Long.valueOf(value)));
+            }
+        }
+        
+        return memory.values()
+                .stream()
+                .mapToLong(Long::valueOf)
+                .sum();
+    }
+    
+    /**
+     * Main method.
+     * 
+     * @param args commandline arguments; these are ignored
+     */
+    public static void main(String[] args) {
+        DockingDataPart2 instance = new DockingDataPart2();
+
+        String result = instance.solve("input-day14-2020.txt");
+
+        LOGGER.info(result);
+    }
+}
+ 
