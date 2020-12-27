d6c77778457c72c019e13358fb471e9e8019d394
Martijn van de Rijdt <mvdrijdt@planet.nl>
2020-12-27 16:58:13 +0100

    First attempt at day 15

---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2020/day15/RambunctiousRecitationPart1.java
@@ -0,0 +1,67 @@
+package nl.mvdr.adventofcode.adventofcode2020.day15;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nl.mvdr.adventofcode.IntSolver;
+
+/**
+ * Solution to the day 15 puzzle of 2020's Advent of Code:
+ * <a href="https://adventofcode.com/2020/day/15">Rambunctious Recitation</a>.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class RambunctiousRecitationPart1 implements IntSolver {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RambunctiousRecitationPart1.class);
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @return the 2020th number spoken
+     */
+    @Override
+    public int solve(Stream<String> linesStream) {
+        String startingNumbersLine = linesStream.findFirst().orElseThrow();
+        List<Integer> startingNumbers = Stream.of(startingNumbersLine.split(","))
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
+        
+        // Array contains, per index, the number of the last turn when it was spoken, or 0 if it was never spoken.
+        int[] spokenOnTurn = new int[2021];
+        
+        // First the starting numbers are spoken.
+        for (int i = 0; i != startingNumbers.size(); i++) {
+            spokenOnTurn[startingNumbers.get(i).intValue()] = i + 1;
+        }
+        
+        int lastSpoken = startingNumbers.get(startingNumbers.size() - 1).intValue();
+        for (int turn = startingNumbers.size() + 1; turn != 2021; turn++) {
+            if (spokenOnTurn[lastSpoken] == 0) {
+                lastSpoken = 0;
+            } else {
+                lastSpoken = turn - spokenOnTurn[lastSpoken];
+            }
+            spokenOnTurn[lastSpoken] = turn;
+        }
+        return lastSpoken;
+    }
+    
+    /**
+     * Main method.
+     * 
+     * @param args commandline arguments; these are ignored
+     */
+    public static void main(String[] args) {
+        RambunctiousRecitationPart1 instance = new RambunctiousRecitationPart1();
+
+        String result = instance.solve("input-day15-2020.txt");
+
+        LOGGER.info(result);
+    }
+}
+ 
---
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2020/day15/RambunctiousRecitationPart1Test.java
@@ -0,0 +1,66 @@
+package nl.mvdr.adventofcode.adventofcode2020.day15;
+
+import org.junit.jupiter.api.Test;
+
+import nl.mvdr.adventofcode.SolverTest;
+
+/**
+ * Unit test cases for {@link RambunctiousRecitationPart1}.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class RambunctiousRecitationPart1Test extends SolverTest<RambunctiousRecitationPart1> {
+
+    /** Constructor. */
+    public RambunctiousRecitationPart1Test() {
+        super(RambunctiousRecitationPart1.class);
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample0() {
+        assertSolution("436", "example-day15-2020-0.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample1() {
+        assertSolution("1", "example-day15-2020-1.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample2() {
+        assertSolution("10", "example-day15-2020-2.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample3() {
+        assertSolution("27", "example-day15-2020-3.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample4() {
+        assertSolution("78", "example-day15-2020-4.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample5() {
+        assertSolution("438", "example-day15-2020-5.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample6() {
+        assertSolution("1836", "example-day15-2020-6.txt");
+    }
+    
+    /** Test case based on the accepted solution. */
+    @Test
+    public void testSolution() {
+        assertSolution("?", "input-day15-2020.txt");
+    }
+}
