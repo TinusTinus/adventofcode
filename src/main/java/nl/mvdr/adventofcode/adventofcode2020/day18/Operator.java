9411797aa2bb250079a64c97788b5d71c06dc566
Martijn van de Rijdt <mvdrijdt@planet.nl>
2020-12-28 17:28:14 +0100

    Added the representation of an operator

---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2020/day18/Operator.java
@@ -0,0 +1,53 @@
+package nl.mvdr.adventofcode.adventofcode2020.day18;
+
+import java.util.stream.Stream;
+
+/**
+ * Representation of an operator.
+ *
+ * @author Martijn van de Rijdt
+ */
+enum Operator {
+    /** Addition, used to compute the sum of two values. */
+    ADDITION('+') {
+        @Override
+        int apply(int lhs, int rhs) {
+            return lhs + rhs;
+        }
+    },
+    
+    /** Multiplication, used to compute the product of two values. */
+    MULTIPLICATION('*') {
+        @Override
+        int apply(int lhs, int rhs) {
+            return lhs * rhs;
+        }
+    };
+    
+    private final char representation;
+    
+    /**
+     * Constructor.
+     * 
+     * @param representation character representation of this operator
+     */
+    Operator(char representation) {
+        this.representation = representation;
+    }
+    
+    static Operator of(char c) {
+        return Stream.of(values())
+                .filter(operator -> operator.representation == c)
+                .findFirst()
+                .orElseThrow();
+    }
+    
+    /**
+     * Apply the operation represented by this operator to the given values.
+     * 
+     * @param lhs left-hand side value
+     * @param rhs right-hand side value
+     * @return value after applying this operator
+     */
+    abstract int apply(int lhs, int rhs);
+}
