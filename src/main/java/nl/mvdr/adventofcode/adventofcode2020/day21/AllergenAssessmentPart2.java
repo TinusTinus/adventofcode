95af7610cdf0b699328cf5f42850c8626baced4d
Martijn van de Rijdt <mvdrijdt@planet.nl>
2021-02-17 17:28:54 +0100

    Solved 2020 day 21 part 2

---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2020/day21/AllergenAssessmentPart2.java
@@ -0,0 +1,55 @@
+package nl.mvdr.adventofcode.adventofcode2020.day21;
+
+import java.util.Comparator;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nl.mvdr.adventofcode.LinesSolver;
+
+/**
+ * Solution to the day 21 puzzle of 2020's Advent of Code:
+ * <a href="https://adventofcode.com/2020/day/17">Allergen Assessment</a>.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class AllergenAssessmentPart2 implements LinesSolver<String> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AllergenAssessmentPart2.class);
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @return canonical dangerous ingredient list
+     */
+    @Override
+    public String solve(Stream<String> lines) {
+        Set<Food> foods = Food.parse(lines);
+        
+        return Food.findIngredientsWithAllergens(foods)
+                .entrySet()
+                .stream()
+                .sorted(Comparator.comparing(entry -> entry.getKey().name()))
+                .map(Entry::getValue)
+                .map(Ingredient::name)
+                .collect(Collectors.joining(","));
+    }
+
+    /**
+     * Main method.
+     * 
+     * @param args commandline arguments; these are ignored
+     */
+    public static void main(String[] args) {
+        AllergenAssessmentPart2 instance = new AllergenAssessmentPart2();
+
+        String result = instance.solve("input-day21-2020.txt");
+
+        LOGGER.info(result);
+    }
+}
+ 
---
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2020/day21/AllergenAssessmentPart2Test.java
@@ -0,0 +1,30 @@
+package nl.mvdr.adventofcode.adventofcode2020.day21;
+
+import org.junit.jupiter.api.Test;
+
+import nl.mvdr.adventofcode.SolverTest;
+
+/**
+ * Unit test cases for {@link AllergenAssessmentPart2}.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class AllergenAssessmentPart2Test extends SolverTest<AllergenAssessmentPart2> {
+
+    /** Constructor. */
+    public AllergenAssessmentPart2Test() {
+        super(AllergenAssessmentPart2.class);
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample() {
+        assertSolution("mxmxvkd,sqjhc,fvjkl", "example-day21-2020.txt");
+    }
+    
+    /** Test case based on the accepted solution. */
+    @Test
+    public void testSolution() {
+        assertSolution("vpzxk,bkgmcsx,qfzv,tjtgbf,rjdqt,hbnf,jspkl,hdcj", "input-day21-2020.txt");
+    }
+}
