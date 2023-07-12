72c63b704e9516520fc8acc8ab31f766bbc347b5
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-07-12 14:08:37 +0200

    WIP

--- src/test/java/nl/mvdr/adventofcode/adventofcode2022/day13/ListValue.java
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2022/day13/ListValue.java
@@ -9,11 +9,13 @@ import java.util.List;
  */
 record ListValue(List<PacketValue> elements) implements PacketValue {
 
+    private static final ListComparator LIST_COMPARATOR = new ListComparator();
+
     @Override
     public int compareTo(PacketValue other) {
         int result;
         if (other instanceof ListValue otherListValue) {
-            result = new ListComparator().compare(elements, otherListValue.elements);
+            result = LIST_COMPARATOR.compare(elements, otherListValue.elements);
         } else if (other instanceof IntValue) {
             result = -(other.compareTo(this));
         } else {
--- src/test/java/nl/mvdr/adventofcode/adventofcode2022/day13/PacketPair.java
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2022/day13/PacketPair.java
@@ -1,11 +1,23 @@
 package nl.mvdr.adventofcode.adventofcode2022.day13;
 
+import java.util.List;
+
 /**
  * A pair of packets.
  *
  * @author Martijn van de Rijdt
  */
 record PacketPair(PacketValue leftPacket, PacketValue rightPacket) {
+    
+    /**
+     * Parses puzzle input.
+     * 
+     * @param lines puzzle input
+     * @return list of packet pairs
+     */
+    static List<PacketPair> parse(List<String> lines) {
+        return List.of(); // TODO implement
+    }
     
     /**
      * @return whether this pair's values are in the correct order
