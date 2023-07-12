c73c38482d581ccbcdc3564ef0e030aa0d65f756
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-07-12 17:18:13 +0200

    Added some logging for fun

--- src/main/java/nl/mvdr/adventofcode/adventofcode2022/day14/RegolithReservoirPart1.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2022/day14/RegolithReservoirPart1.java
@@ -26,8 +26,9 @@ public class RegolithReservoirPart1 implements IntSolver {
                 .flatMap(path -> path.allPoints().stream())
                 .collect(Collectors.toSet());
         var spring = new Point(500, 0);
-        var verticalSlice = new VerticalSlice(spring, rock, false);
-        return verticalSlice.tickUntilDone().countSettled();
+        var verticalSlice = new VerticalSlice(spring, rock, false).tickUntilDone();
+        LOGGER.info(verticalSlice.toString());
+        return verticalSlice.countSettled();
     }
 
     /**
