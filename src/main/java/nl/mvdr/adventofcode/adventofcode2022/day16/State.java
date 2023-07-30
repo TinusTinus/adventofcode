4a4cfa027242a0bb001d4eea332534d8fb02fa27
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-07-30 20:14:45 +0200

    Bugfix

--- src/main/java/nl/mvdr/adventofcode/adventofcode2022/day16/State.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2022/day16/State.java
@@ -24,16 +24,18 @@ record State(Network network, Valve position, Set<Valve> openValves, int remaini
      */
     Set<State> nextStates() {
         Set<State> result = new HashSet<>();
+        var newRemainingMinutes = remainingMinutes - 1;
         if (0 < position.flowRate() &&!openValves.contains(position)) {
             // We could open this valve. (Note that, if the flow rate is zero, there is no point in opening this valve.)
             Set<Valve> newOpenValves = new HashSet<>(openValves);
             newOpenValves.add(position);
-            var newPressureReleased = pressureReleased + position.flowRate() * remainingMinutes;
-            result.add(new State(network, position, newOpenValves, remainingMinutes - 1, newPressureReleased));
+            
+            var newPressureReleased = pressureReleased + position.flowRate() * newRemainingMinutes;
+            result.add(new State(network, position, newOpenValves, newRemainingMinutes, newPressureReleased));
         }
         // We could move through a tunnel.
         for (Valve tunnelExit : network.tunnels().get(position)) {
-            result.add(new State(network, tunnelExit, openValves, remainingMinutes - 1, pressureReleased));
+            result.add(new State(network, tunnelExit, openValves, newRemainingMinutes, pressureReleased));
         }
         // We could also just do nothing for a minute, but we may as well keep walking back and forth through tunnels.
         return result;
