7e5c7231fb7c8cbe19586f27c02982798cbf1952
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-07-30 21:15:03 +0200

    Refactoring

--- src/main/java/nl/mvdr/adventofcode/adventofcode2022/day16/State.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2022/day16/State.java
@@ -1,15 +1,22 @@
 package nl.mvdr.adventofcode.adventofcode2022.day16;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
- * A possible state of the network.
+ * A possible state of a network.
  *
+ * @param network the network
+ * @param position current position in the network
+ * @param closedValves valves which can still be opened to release pressure
+ * @param remainingMinutes the remaining minutes until the vulcano erupts
+ * @param pressureReleased amount of pressure released, as a result of the valves that have already been opened
+ * @param currentPath the path we are currently on to get to a specific closed valve; may be null, indicating that we are not (yet) on a path
  * @author Martijn van de Rijdt
  */
-record State(Network network, Valve position, Set<Valve> openValves, int remainingMinutes, int pressureReleased) {
+record State(Network network, Valve position, Set<Valve> closedValves, int remainingMinutes, int pressureReleased, List<Valve> currentPath) {
     
     /**
      * Constructor, for the initial state of a network.
@@ -23,10 +30,11 @@ record State(Network network, Valve position, Set<Valve> openValves, int remaini
                 network.graph()
                     .vertexSet()
                     .stream()
-                    .filter(valve -> valve.flowRate() == 0)
+                    .filter(valve -> valve.flowRate() != 0)
                     .collect(Collectors.toSet()),
                 30,
-                0);
+                0,
+                null);
     }
     
     /**
@@ -35,24 +43,37 @@ record State(Network network, Valve position, Set<Valve> openValves, int remaini
     Set<State> nextStates() {
         Set<State> result = new HashSet<>();
         var newRemainingMinutes = remainingMinutes - 1;
-        if (!openValves.contains(position)) {
-            Set<Valve> newOpenValves = new HashSet<>(openValves);
-            newOpenValves.add(position);
-            
-            var newPressureReleased = pressureReleased + position.flowRate() * newRemainingMinutes;
-            result.add(new State(network, position, newOpenValves, newRemainingMinutes, newPressureReleased));
-        }
-        // We could move through a tunnel.
-        for (Valve valve : network.graph().vertexSet()) {
-            if (network.graph().containsEdge(position, valve)) {
-                result.add(new State(network, valve, openValves, newRemainingMinutes, pressureReleased));
+        
+        if (currentPath == null) {
+            // TODO figure out new paths
+            if (closedValves.contains(position)) {
+                Set<Valve> newClosedValves = new HashSet<>(closedValves);
+                newClosedValves.remove(position);
+                
+                var newPressureReleased = pressureReleased + position.flowRate() * newRemainingMinutes;
+                result.add(new State(network, position, newClosedValves, newRemainingMinutes, newPressureReleased, null));
             }
-        }
-        if (result.isEmpty()) {
-            // Nothing to do. We could also just do nothing for a minute.
-            result.add(new State(network, position, openValves, newRemainingMinutes, pressureReleased));
+            // We could move through a tunnel.
+            for (Valve valve : network.graph().vertexSet()) {
+                if (network.graph().containsEdge(position, valve)) {
+                    result.add(new State(network, valve, closedValves, newRemainingMinutes, pressureReleased, null));
+                }
+            }
+        } else if (currentPath.isEmpty()) {
+            // Close this valve.
+            Set<Valve> newClosedValves = new HashSet<>(closedValves);
+            newClosedValves.remove(position);
+            var newPressureReleased = pressureReleased + position.flowRate() * newRemainingMinutes;
+            result.add(new State(network, position, newClosedValves, newRemainingMinutes, newPressureReleased, null));
+        } else {
+            // Move through the tunnel.
+            result.add(new State(network, currentPath.get(0), closedValves, newRemainingMinutes, pressureReleased, currentPath.subList(1, currentPath.size())));
         }
         
+        if (result.isEmpty()) {
+            // Nothing to do. Let's just do nothing for a minute.
+            result.add(new State(network, position, closedValves, newRemainingMinutes, pressureReleased, null));
+        }
         return result;
     }
 }
