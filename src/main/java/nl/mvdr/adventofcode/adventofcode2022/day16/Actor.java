6a0bb8a5e0055d9c6eb47471329ce2122816319f
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-08-06 17:01:23 +0200

    javadoc

--- src/main/java/nl/mvdr/adventofcode/adventofcode2022/day16/Actor.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2022/day16/Actor.java
@@ -3,7 +3,6 @@ package nl.mvdr.adventofcode.adventofcode2022.day16;
 /**
  * An actor who is capable of moving around and opening valves.
  *
- * @param startingPoint whether the actor is still in the starting position
  * @param currentTarget the valve that this actor is currently traveling towards; may be null if not yet determined
  * @param distance the distance to the current target; null if not applicable
  * @author Martijn van de Rijdt
