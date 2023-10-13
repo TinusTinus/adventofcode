7f0dc7178cae202cd9133282beda256dd9765a48
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-10-13 11:18:22 +0200

    Started on day 24

---
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2022/day24/BlizzardBasinPart1Test.java
@@ -0,0 +1,36 @@
+package nl.mvdr.adventofcode.adventofcode2022.day24;
+
+import org.junit.jupiter.api.Test;
+
+import nl.mvdr.adventofcode.SolverTest;
+
+/**
+ * Unit test cases for {@link BlizzardBasinPart1}.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class BlizzardBasinPart1Test extends SolverTest<BlizzardBasinPart1> {
+
+    /** Constructor. */
+    public BlizzardBasinPart1Test() {
+        super(BlizzardBasinPart1.class);
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample0() {
+        assertSolution("10", "example-day24-2022-0.txt");
+    }
+    
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExample1() {
+        assertSolution("18", "example-day24-2022-1.txt");
+    }
+    
+    /** Test case based on the accepted solution. */
+    @Test
+    public void testSolution() {
+        assertSolution("?", "input-day24-2022.txt");
+    }
+}
