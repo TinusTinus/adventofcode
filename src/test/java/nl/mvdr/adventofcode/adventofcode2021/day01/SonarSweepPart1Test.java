89de3fc7cd005b2558f89ff065ee79c042aa382a
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-11-10 18:28:44 +0100

    Solved 2021 day 1 part 1

---
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2021/day01/SonarSweepPart1Test.java
@@ -0,0 +1,30 @@
+package nl.mvdr.adventofcode.adventofcode2021.day01;
+
+import org.junit.jupiter.api.Test;
+
+import nl.mvdr.adventofcode.SolverTest;
+
+/**
+ * Unit test cases for {@link SonarSweepPart1}.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class SonarSweepPart1Test extends SolverTest<SonarSweepPart1> {
+
+    /** Constructor. */
+    public SonarSweepPart1Test() {
+        super(SonarSweepPart1.class);
+    }
+
+    /** Test case based on the example from the puzzle text. */
+    @Test
+    public void testExample() {
+        assertSolution("7", "example-day01-2021.txt");
+    }
+
+    /** Test case based on the accepted solution. */
+    @Test
+    public void testSolution() {
+        assertSolution("1448", "input-day01-2021.txt");
+    }
+}
