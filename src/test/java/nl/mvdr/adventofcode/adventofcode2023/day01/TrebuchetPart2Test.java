0a1f161737467ff7924b4e2e03e14ce90db00ee2
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-12-01 10:11:17 +0100

    Solved 2023 day 1 part 2

--- src/test/java/nl/mvdr/adventofcode/adventofcode2023/day01/TrebuchetPart2Test.java
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2023/day01/TrebuchetPart2Test.java
@@ -5,26 +5,32 @@ import org.junit.jupiter.api.Test;
 import nl.mvdr.adventofcode.SolverTest;
 
 /**
- * Unit test cases for {@link TrebuchetPart1}.
+ * Unit test cases for {@link TrebuchetPart2}.
  *
  * @author Martijn van de Rijdt
  */
-public class TrebuchetPart1Test extends SolverTest<TrebuchetPart1> {
+public class TrebuchetPart2Test extends SolverTest<TrebuchetPart2> {
 
     /** Constructor. */
-    public TrebuchetPart1Test() {
-        super(TrebuchetPart1.class);
+    public TrebuchetPart2Test() {
+        super(TrebuchetPart2.class);
     }
     
     /** Test case based on an example from the puzzle text. */
     @Test
-    public void testExample() {
+    public void testExamplePart1() {
         assertSolution("142", "example-day01-2023-part1.txt");
     }
     
+    /** Test case based on an example from the puzzle text. */
+    @Test
+    public void testExamplePart2() {
+        assertSolution("281", "example-day01-2023-part2.txt");
+    }
+
     /** Test case based on the accepted solution. */
     @Test
     public void testSolution() {
-        assertSolution("55108", "input-day01-2023.txt");
+        assertSolution("56324", "input-day01-2023.txt");
     }
 }
