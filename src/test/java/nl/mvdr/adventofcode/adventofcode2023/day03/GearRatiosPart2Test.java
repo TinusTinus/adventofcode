920469118ae5d67d56167fa0dabf24ccaead080b
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-12-03 13:30:56 +0100

    Got started on part 2

--- src/test/java/nl/mvdr/adventofcode/adventofcode2023/day03/GearRatiosPart2Test.java
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2023/day03/GearRatiosPart2Test.java
@@ -7,21 +7,21 @@ import org.junit.jupiter.params.provider.Arguments;
 import nl.mvdr.adventofcode.SolverTest;
 
 /**
- * Unit test cases for {@link GearRatiosPart1}.
+ * Unit test cases for {@link GearRatiosPart2}.
  *
  * @author Martijn van de Rijdt
  */
-public class GearRatiosPart1Test extends SolverTest<GearRatiosPart1> {
+public class GearRatiosPart2Test extends SolverTest<GearRatiosPart2> {
 
     /** Constructor. */
-    public GearRatiosPart1Test() {
-        super(GearRatiosPart1.class);
+    public GearRatiosPart2Test() {
+        super(GearRatiosPart2.class);
     }
     
     /** @return arguments for {@link SolverTest#testSolution(String, String)} */
     static Stream<Arguments> testSolution() {
         return Stream.of(
-                Arguments.of("4361", "example-day03-2023.txt"),
-                Arguments.of("521515", "input-day03-2023.txt"));
+                Arguments.of("467835", "example-day03-2023.txt"),
+                Arguments.of("?", "input-day03-2023.txt")); // 527438 was too high!
     }
 }
