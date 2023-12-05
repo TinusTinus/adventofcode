38bc396a1e083a2624f36a03db0d7dc49ebd45f9
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-12-05 14:36:39 +0100

    Added a bunch of helper methods

--- src/main/java/nl/mvdr/adventofcode/adventofcode2023/day05/Range.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2023/day05/Range.java
@@ -3,10 +3,45 @@ package nl.mvdr.adventofcode.adventofcode2023.day05;
 /**
  * A range of numbers.
  *
+ * @param start start of the range
+ * @param length length of the range
  * @author Martijn van de Rijdt
  */
 record Range(long start, long length) {
 
+    /**
+     * Constructor.
+     * 
+     * @param start start of the range
+     * @param length length of the range
+     */
+    Range(long start, long length) {
+        if (length < 0) {
+            throw new IllegalArgumentException("Invalid length: " + length);
+        }
+        this.start = start;
+        this.length = length;
+    }
+    
+    /**
+     * Creates a new range.
+     * 
+     * @param startInclusive start of the range, inclusive
+     * @param endExclusive end of the range, exclusive; if endExclusive <= startInclusive, the resulting range will be empty
+     * @return range
+     */
+    private static Range of(long startInclusive, long endExclusive) {
+        long resultLength = Math.max(0, Math.subtractExact(endExclusive, startInclusive));
+        return new Range(startInclusive, resultLength);
+    }
+
+    /**
+     * @return end of the range (exclusive)
+     */
+    private long endExclusive() {
+        return Math.addExact(start, length);
+    }
+    
     /**
      * Checks whether the given number falls within this range.
      * 
@@ -24,6 +59,50 @@ record Range(long start, long length) {
      * @return whether the given other range is entirely contained within this one
      */
     boolean contains(Range other) {
-        return start <= other.start() && other.start() + other.length() <= start + length;
+        return start <= other.start() && other.endExclusive() <= endExclusive();
     }
+    
+    /**
+     * @return whether this range is empty
+     */
+    boolean isEmpty() {
+        return length == 0;
+    }
+    
+    /**
+     * Determines which part of the given other range overlaps with this one, and returns it.
+     * 
+     * @param other other range
+     * @return overlapping part; note that this may be empty!
+     */
+    Range overlappingPart(Range other) {
+        var resultStart = Math.max(start, other.start());
+        var resultEnd = Math.min(endExclusive(), other.endExclusive());
+        return of(resultStart, resultEnd);
+    }
+    
+    /**
+     * Determines the part of the given range which preceeds this range.
+     * 
+     * @param other other range
+     * @return preceding part
+     */
+    Range precedingPart(Range other) {
+        long resultStart = other.start();
+        long resultEnd = Math.min(other.endExclusive(), start);
+        return of(resultStart, resultEnd);
+    }
+    
+    /**
+     * Determines the part of the given range which follows this range.
+     * 
+     * @param other other range
+     * @return following part
+     */
+    Range followingPart(Range other) {
+        long resultStart = Math.max(endExclusive(), other.start());
+        long resultEnd = other.endExclusive();
+        return of(resultStart, resultEnd);
+    }
+
 }
