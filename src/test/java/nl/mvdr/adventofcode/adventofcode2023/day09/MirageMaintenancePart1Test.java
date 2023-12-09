050fa000fc92357fee3f0e74d0ae7b741c9040fd
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-12-09 12:03:09 +0100

    Started day 9

--- src/test/java/nl/mvdr/adventofcode/adventofcode2023/day09/MirageMaintenancePart1Test.java
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2023/day09/MirageMaintenancePart1Test.java
@@ -7,21 +7,21 @@ import org.junit.jupiter.params.provider.Arguments;
 import nl.mvdr.adventofcode.SolverTest;
 
 /**
- * Unit test cases for {@link Day9Part1}.
+ * Unit test cases for {@link MirageMaintenancePart1}.
  *
  * @author Martijn van de Rijdt
  */
-public class Day9Part1Test extends SolverTest<Day9Part1> {
+public class MirageMaintenancePart1Test extends SolverTest<MirageMaintenancePart1> {
 
     /** Constructor. */
-    public Day9Part1Test() {
-        super(Day9Part1.class);
+    public MirageMaintenancePart1Test() {
+        super(MirageMaintenancePart1.class);
     }
     
     /** @return arguments for {@link SolverTest#testSolution(String, String)} */
     static Stream<Arguments> testSolution() {
         return Stream.of(
-                Arguments.of("?", "example-day09-2023.txt"),
+                Arguments.of("114", "example-day09-2023.txt"),
                 Arguments.of("?", "input-day09-2023.txt"));
     }
 }
