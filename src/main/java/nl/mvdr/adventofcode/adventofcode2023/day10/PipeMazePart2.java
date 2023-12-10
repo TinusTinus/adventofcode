3e0d8370337928d8b60f8b73d5f52ac3329fded3
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-12-10 12:14:18 +0100

    Got started on part 2

---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2023/day10/PipeMazePart2.java
@@ -0,0 +1,38 @@
+package nl.mvdr.adventofcode.adventofcode2023.day10;
+
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nl.mvdr.adventofcode.IntSolver;
+
+/**
+ * Solution to <a href="https://adventofcode.com/2023/day/10">Pipe Maze</a>.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class PipeMazePart2 implements IntSolver {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PipeMazePart2.class);
+
+    @Override
+    public int solve(Stream<String> lines) {
+        var maze = Maze.parse(lines.toList());
+        return 0; // TODO
+    }
+
+    /**
+     * Main method.
+     * 
+     * @param args commandline arguments; these are ignored
+     */
+    public static void main(String[] args) {
+        var instance = new PipeMazePart2();
+
+        var result = instance.solve("input-day10-2023.txt");
+
+        LOGGER.info(result);
+    }
+}
+ 
---
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2023/day10/PipeMazePart2Test.java
@@ -0,0 +1,34 @@
+package nl.mvdr.adventofcode.adventofcode2023.day10;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import nl.mvdr.adventofcode.SolverTest;
+
+/**
+ * Unit test cases for {@link PipeMazePart2}.
+ *
+ * @author Martijn van de Rijdt
+ */
+public class PipeMazePart2Test extends SolverTest<PipeMazePart2> {
+
+    /** Constructor. */
+    public PipeMazePart2Test() {
+        super(PipeMazePart2.class);
+    }
+    
+    /** @return arguments for {@link SolverTest#testSolution(String, String)} */
+    static Stream<Arguments> testSolution() {
+        return Stream.of(
+                Arguments.of("1", "example-day10-2023-0.txt"),
+                Arguments.of("1", "example-day10-2023-1.txt"),
+                Arguments.of("1", "example-day10-2023-2.txt"),
+                Arguments.of("1", "example-day10-2023-3.txt"),
+                Arguments.of("4", "example-day10-2023-4.txt"),
+                Arguments.of("4", "example-day10-2023-5.txt"),
+                Arguments.of("8", "example-day10-2023-6.txt"),
+                Arguments.of("10", "example-day10-2023-7.txt"),
+                Arguments.of("?", "input-day10-2023.txt"));
+    }
+}
