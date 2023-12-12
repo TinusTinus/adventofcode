d16635745f51b37300d2de6c6e6797f3ffbffba9
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-12-12 11:36:19 +0100

    Slight refactoring

--- src/main/java/nl/mvdr/adventofcode/adventofcode2023/day12/ConditionRecord.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2023/day12/ConditionRecord.java
@@ -59,32 +59,29 @@ record ConditionRecord(List<Condition> springs, List<Integer> contiguousGroupSiz
         } else if (springs.getFirst() == Condition.OPERATIONAL) {
             // We can just ignore this spring. Inspect the rest of the row.
             result = dropFirstSpring().countArrangements();
-        } else {
-            var firstSpring = springs.getFirst(); // Could be damaged or unknown (see the previous guard).
-            if (couldStartWithContiguousGroup()) {
-                var groupSize = contiguousGroupSizes.getFirst().intValue();
-                if (springs.size() == groupSize) {
-                    result = 1L;
-                } else {
-                    // Compute the number of valid arrangements, assuming the contiguous group does start here.
-                    var newSprings = springs.subList(groupSize + 1, springs.size());
-                    var newGroupSizes = contiguousGroupSizes.subList(1, contiguousGroupSizes.size());
-                    result = new ConditionRecord(newSprings, newGroupSizes).countArrangements();
-                    
-                    if (firstSpring == Condition.UNKNOWN) {
-                        // The first spring could also be operational.
-                        result = result + dropFirstSpring().countArrangements();
-                    }
-                }
+        } else if (couldStartWithContiguousGroup()) {
+            var groupSize = contiguousGroupSizes.getFirst().intValue();
+            if (springs.size() == groupSize) {
+                result = 1L;
             } else {
-                // No contiguous group can start here.
-                result = switch(firstSpring) {
-                    case DAMAGED -> 0L; // Invalid record.
-                    case UNKNOWN -> dropFirstSpring().countArrangements(); // First spring must be operational.
-                    case OPERATIONAL -> throw new IllegalStateException("Unexpected operational string found at the start of " + this); // Should be prevented by earlier checks
-                    default -> throw new IllegalStateException("Unexpected condition: " + firstSpring);
-                };
+                // Compute the number of valid arrangements, assuming the contiguous group does start here.
+                var newSprings = springs.subList(groupSize + 1, springs.size());
+                var newGroupSizes = contiguousGroupSizes.subList(1, contiguousGroupSizes.size());
+                result = new ConditionRecord(newSprings, newGroupSizes).countArrangements();
+                
+                if (springs.getFirst() == Condition.UNKNOWN) {
+                    // The first spring could also be operational.
+                    result = result + dropFirstSpring().countArrangements();
+                }
             }
+        } else {
+            // No contiguous group can start here.
+            result = switch(springs.getFirst()) {
+                case DAMAGED -> 0L; // Invalid record.
+                case UNKNOWN -> dropFirstSpring().countArrangements(); // First spring must be operational.
+                case OPERATIONAL -> throw new IllegalStateException("Unexpected operational spring found at the start of " + this); // Should be prevented by earlier checks
+                default -> throw new IllegalStateException("Unexpected condition: " + springs.getFirst());
+            };
         }
         
         return result;
