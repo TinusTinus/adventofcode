f3db9ded5ab91046f8de933b930ee295fb400740
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-12-15 10:22:49 +0100

    Removed unused imports

--- src/test/java/nl/mvdr/adventofcode/adventofcode2023/day15/HashAlgorithmTest.java
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2023/day15/HashAlgorithmTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import nl.mvdr.adventofcode.SolverTest;
-
 /**
  * Unit test class for {@link HashAlgorithm}.
  *
