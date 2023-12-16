26ae368c827c5f3389d6b12e60262d0f3d0431c2
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-12-16 18:43:43 +0100

    Further refactoring

--- src/main/java/nl/mvdr/adventofcode/adventofcode2023/day13/Pattern.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2023/day13/Pattern.java
@@ -1,7 +1,6 @@
 package nl.mvdr.adventofcode.adventofcode2023.day13;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -54,23 +53,9 @@ record Pattern(Map<Point, Terrain> map, int width, int height) {
      * @return pattern
      */
     private static Pattern parsePattern(List<String> lines) {
+        var map = Point.parse2DMap(lines, Terrain::of);
         var height = lines.size();
         var width = lines.getFirst().length();
-        
-        Map<Point, Terrain> map = new HashMap<>();
-        for (var y = 0; y != height; y++) {
-            var line = lines.get(y);
-            if (line.length() != width) {
-                throw new IllegalArgumentException("Input must be rectangular: all lines must have the same length ("
-                        + width + "). But the length of line " + y + " is: " + line.length());
-            }
-            for (var x = 0; x != width; x++) {
-                var terrain = Terrain.of(line.charAt(x));
-                var point = new Point(x, y);
-                map.put(point, terrain);
-            }
-        }
-        
         return new Pattern(map, width, height);
     }
     
