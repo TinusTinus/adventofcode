82cf30e669d6c9fec283efa076ee93fe3d458e7c
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-12-18 11:00:46 +0100

    Turns out colors do not matter at all

--- src/main/java/nl/mvdr/adventofcode/adventofcode2023/day18/DigPlanInstruction.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2023/day18/DigPlanInstruction.java
@@ -1,6 +1,5 @@
 package nl.mvdr.adventofcode.adventofcode2023.day18;
 
-import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,10 +11,9 @@ import nl.mvdr.adventofcode.point.Point;
  *
  * @param direction the direction to dig in
  * @param distance the distance to dig, in meters
- * @param color the edge's color
  * @author Martijn van de Rijdt
  */
-record DigPlanInstruction(Direction direction, int distance, Color color) {
+record DigPlanInstruction(Direction direction, int distance) {
     
     /**
      * Parses the textual representation of an instruction.
@@ -31,14 +29,7 @@ record DigPlanInstruction(Direction direction, int distance, Color color) {
         var direction = Direction.parse(parts[0]);
         var distance = Integer.parseInt(parts[1]);
         
-        var colorString = parts[2];
-        if (!colorString.startsWith("(") || !colorString.endsWith(")")) {
-            throw new IllegalArgumentException("Invalid edge color specification: " + colorString);
-        }
-        colorString = colorString.substring(1, colorString.length() - 1); // lose the brackets
-        var color = Color.decode(colorString);
-        
-        return new DigPlanInstruction(direction, distance, color);
+        return new DigPlanInstruction(direction, distance);
     }
     
     /**
