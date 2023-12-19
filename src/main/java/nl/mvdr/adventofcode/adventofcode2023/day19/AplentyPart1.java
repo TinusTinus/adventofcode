939f7c8f5b19d623d1c73fe707b33b722fba2dfc
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-12-19 10:43:55 +0100

    Started day 19

--- src/main/java/nl/mvdr/adventofcode/adventofcode2023/day19/AplentyPart1.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2023/day19/AplentyPart1.java
@@ -8,13 +8,13 @@ import org.slf4j.LoggerFactory;
 import nl.mvdr.adventofcode.IntSolver;
 
 /**
- * Solution to <a href="https://adventofcode.com/2023/day/19">?</a>.
+ * Solution to <a href="https://adventofcode.com/2023/day/19">Aplenty</a>.
  *
  * @author Martijn van de Rijdt
  */
-public class Day19Part1 implements IntSolver {
+public class AplentyPart1 implements IntSolver {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Day19Part1.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(AplentyPart1.class);
 
     @Override
     public int solve(Stream<String> lines) {
@@ -27,7 +27,7 @@ public class Day19Part1 implements IntSolver {
      * @param args commandline arguments; these are ignored
      */
     public static void main(String[] args) {
-        var instance = new Day19Part1();
+        var instance = new AplentyPart1();
 
         var result = instance.solve("input-day19-2023.txt");
 
--- src/test/java/nl/mvdr/adventofcode/adventofcode2023/day19/AplentyPart1Test.java
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2023/day19/AplentyPart1Test.java
@@ -7,21 +7,21 @@ import org.junit.jupiter.params.provider.Arguments;
 import nl.mvdr.adventofcode.SolverTest;
 
 /**
- * Unit test cases for {@link Day19Part1}.
+ * Unit test cases for {@link AplentyPart1}.
  *
  * @author Martijn van de Rijdt
  */
-public class Day19Part1Test extends SolverTest<Day19Part1> {
+public class AplentyPart1Test extends SolverTest<AplentyPart1> {
 
     /** Constructor. */
-    public Day19Part1Test() {
-        super(Day19Part1.class);
+    public AplentyPart1Test() {
+        super(AplentyPart1.class);
     }
     
     /** @return arguments for {@link SolverTest#testSolution(String, String)} */
     static Stream<Arguments> testSolution() {
         return Stream.of(
-                Arguments.of("?", "example-day19-2023.txt"),
+                Arguments.of("19114", "example-day19-2023.txt"),
                 Arguments.of("?", "input-day19-2023.txt"));
     }
 }
