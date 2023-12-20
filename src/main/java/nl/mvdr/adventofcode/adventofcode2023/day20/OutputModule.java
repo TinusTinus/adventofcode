53a76289bb317ca423a3d149a545eb2d1e0762fc
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-12-20 20:46:54 +0100

    Removed unused code

--- src/main/java/nl/mvdr/adventofcode/adventofcode2023/day20/OutputModule.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2023/day20/OutputModule.java
@@ -3,27 +3,13 @@ package nl.mvdr.adventofcode.adventofcode2023.day20;
 import java.util.List;
 import java.util.Set;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * An output module, which processes all incoming pulses and does not produce any output itself.
  *
  * @author Martijn van de Rijdt
  */
-record OutputModule(String name, boolean hasReceivedLowPulse) implements Module {
+record OutputModule(String name) implements Module {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(OutputModule.class);
-    
-    /**
-     * Convenience constructor.
-     * 
-     * @param name name of this module
-     */
-    OutputModule(String name) {
-        this(name, false);
-    }
-    
     @Override
     public Module init(Set<Module> modules) {
         return this;
@@ -36,14 +22,6 @@ record OutputModule(String name, boolean hasReceivedLowPulse) implements Module 
 
     @Override
     public HandlePulseResult handlePulse(Pulse pulse) {
-        LOGGER.debug("Output module {} received pulse {}", name, pulse);
-        
-        OutputModule newModule;
-        if (pulse.type() == PulseType.LOW) {
-            newModule = new OutputModule(name, true);
-        } else {
-            newModule = this;
-        }
-        return new HandlePulseResult(newModule, List.of());
+        return new HandlePulseResult(this, List.of());
     }
 }
