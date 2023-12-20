b1d5a7b6abf93f84e09f2a45f34b9164add187f8
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-12-20 16:43:04 +0100

    WIP

--- src/main/java/nl/mvdr/adventofcode/adventofcode2023/day20/ModuleConfiguration.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2023/day20/ModuleConfiguration.java
@@ -1,6 +1,7 @@
 package nl.mvdr.adventofcode.adventofcode2023.day20;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.function.Function;
@@ -12,7 +13,7 @@ import java.util.stream.Stream;
  *
  * @author Martijn van de Rijdt
  */
-record ModuleConfiguration(Map<String, Module> modules, Queue<Pulse> pulses) {
+record ModuleConfiguration(Map<String, Module> modules, Queue<Pulse> pulseQueue) {
     
     /**
      * Parses a module configuration.
@@ -37,14 +38,41 @@ record ModuleConfiguration(Map<String, Module> modules, Queue<Pulse> pulses) {
     /**
      * Presses the button once.
      * 
+     * @param pulseCounter mutable(!) map of pulse counters; will be updated during this method
      * @return updated module configuration
      */
-    private ModuleConfiguration pressButton() {
-        if (!pulses.isEmpty()) {
+    private ModuleConfiguration pressButton(Map<PulseType, Long> pulseCounter) {
+        if (!pulseQueue.isEmpty()) {
             throw new IllegalStateException("Still processing pulses.");
         }
-        Queue<Pulse> newPulses = new LinkedList<>();
-        newPulses.addAll(ButtonModule.INSTANCE.press());
+        var newPulses = addPulses(ButtonModule.INSTANCE.press(), pulseCounter);
         return new ModuleConfiguration(modules, newPulses);
+    }
+    
+    /**
+     * Processes all pulses currently in the queue.
+     * 
+     * @param pulseCounter mutable(!) map of pulse counters; will be updated during this method
+     * @return updated module configuration
+     */
+    private ModuleConfiguration handleQueue(Map<PulseType, Long> pulseCounter) {
+        return this; // TODO
+    }
+    
+    /**
+     * Adds the given pulses to the queue.
+     * 
+     * @param pulses pulses to add to the back of the queue
+     * @param pulseCounter mutable(!) map of pulse counters; will be updated during this method
+     * @return new copy of the queue with the given pulses added to it
+     */
+    private Queue<Pulse> addPulses(List<Pulse> pulses, Map<PulseType, Long> pulseCounter) {
+        Queue<Pulse> result = new LinkedList<>(pulseQueue);
+        pulses.forEach(pulse -> {
+            var newCount = pulseCounter.get(pulse.type()).longValue() + 1L;
+            pulseCounter.put(pulse.type(), Long.valueOf(newCount));
+            result.offer(pulse);
+        });
+        return result;
     }
 }
