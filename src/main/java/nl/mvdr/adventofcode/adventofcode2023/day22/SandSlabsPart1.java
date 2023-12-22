5b67c565a681dd33ad7124c557d86a7cbff477d2
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-12-22 08:37:55 +0100

    Started day 22

--- src/main/java/nl/mvdr/adventofcode/adventofcode2023/day22/SandSlabsPart1.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2023/day22/SandSlabsPart1.java
@@ -8,13 +8,13 @@ import org.slf4j.LoggerFactory;
 import nl.mvdr.adventofcode.IntSolver;
 
 /**
- * Solution to <a href="https://adventofcode.com/2023/day/22">?</a>.
+ * Solution to <a href="https://adventofcode.com/2023/day/22">Sand Slabs</a>.
  *
  * @author Martijn van de Rijdt
  */
-public class Day22Part1 implements IntSolver {
+public class SandSlabsPart1 implements IntSolver {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Day22Part1.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(SandSlabsPart1.class);
 
     @Override
     public int solve(Stream<String> lines) {
@@ -27,7 +27,7 @@ public class Day22Part1 implements IntSolver {
      * @param args commandline arguments; these are ignored
      */
     public static void main(String[] args) {
-        var instance = new Day22Part1();
+        var instance = new SandSlabsPart1();
 
         var result = instance.solve("input-day22-2023.txt");
 
--- src/test/java/nl/mvdr/adventofcode/adventofcode2023/day22/SandSlabsPart1Test.java
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2023/day22/SandSlabsPart1Test.java
@@ -7,21 +7,21 @@ import org.junit.jupiter.params.provider.Arguments;
 import nl.mvdr.adventofcode.SolverTest;
 
 /**
- * Unit test cases for {@link Day22Part1}.
+ * Unit test cases for {@link SandSlabsPart1}.
  *
  * @author Martijn van de Rijdt
  */
-public class Day22Part1Test extends SolverTest<Day22Part1> {
+public class SandSlabsPart1Test extends SolverTest<SandSlabsPart1> {
 
     /** Constructor. */
-    public Day22Part1Test() {
-        super(Day22Part1.class);
+    public SandSlabsPart1Test() {
+        super(SandSlabsPart1.class);
     }
     
     /** @return arguments for {@link SolverTest#testSolution(String, String)} */
     static Stream<Arguments> testSolution() {
         return Stream.of(
-                Arguments.of("?", "example-day22-2023.txt"),
+                Arguments.of("5", "example-day22-2023.txt"),
                 Arguments.of("?", "input-day22-2023.txt"));
     }
 }
