099aec790fcb582c1419b8407c8920e8c36440eb
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-12-22 12:34:09 +0100

    Refactored part 1 to be way more efficient

--- src/main/java/nl/mvdr/adventofcode/adventofcode2023/day22/SandSlabsPart1.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2023/day22/SandSlabsPart1.java
@@ -1,5 +1,9 @@
 package nl.mvdr.adventofcode.adventofcode2023.day22;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -24,9 +28,26 @@ public class SandSlabsPart1 implements LongSolver {
         
         LOGGER.debug("Bricks have settled: {}", settledBricks);
         
+        // For each brick which is not resting on the ground: determine which other bricks it is resting on.
+        // (Note that bricks already resting on the ground can never fall.)
+        var supportingBricks = settledBricks.stream()
+                .filter(Predicate.not(Brick::isOnTheGround))
+                .collect(Collectors.toMap(Function.identity(), brick -> brick.supportingBrickSet(settledBricks)));
+        
         return settledBricks.parallelStream()
-                .filter(brick -> brick.canBeDisintegrated(settledBricks))
+                .filter(brick -> canBeDisintegrated(brick, supportingBricks))
                 .count();
+    }
+    
+    /**
+     * Counts how many bricks would fall if the given brick was disintegrated.
+     * 
+     * @param brick brick to be disintegrated
+     * @param supportingBricks for each brick, not resting on the ground, which other bricks it is resting on
+     * @return number of bricks which would fall
+     */
+    private static boolean canBeDisintegrated(Brick brick, Map<Brick, Set<Brick>> supportingBricks) {
+        return !supportingBricks.values().contains(Set.of(brick));
     }
     
     /**
