0001bca0d0a0bf4e35ae906589099eb381bf0e3a
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2023-12-25 01:07:41 +0100

    Attempted to add a native Z3 dependency

--- src/main/java/nl/mvdr/adventofcode/adventofcode2023/day24/NeverTellMeTheOddsPart2.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2023/day24/NeverTellMeTheOddsPart2.java
@@ -5,6 +5,8 @@ import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.microsoft.z3.Context;
+
 import nl.mvdr.adventofcode.LongSolver;
 
 /**
@@ -18,28 +20,11 @@ public class NeverTellMeTheOddsPart2 implements LongSolver {
 
     @Override
     public long solve(Stream<String> lines) {
-        // Note: I struggled quite a bit with today's part 2.
-        // It seems less a programming problem and more a math problem
-        // (and I don't often solve these kinds of math problems these days).
-        // After looking up some hints I did not end up solving this problem using a Java program.
-        // Instead I used Z3 (https://github.com/Z3Prover/z3), in combination with a Python script,
+        try (var context = new Context()) {
+            
+        }
         
-        // We know that, for each hailstone, some timestamp t must exist so that:
-        //   hailstone.position + t * hailstone.velocity = rock.position + t * rock.velocity
-        // By using multiple hailstones, we can set up a system of equations,
-        // and then solve for the rock's position.
-        
-        // Interestingly, any three hailstones from the puzzle input
-        // should contain enough information to solve this system. I used the first three.
-        // The rest of the input is completely redundant, as far as part 2 is concerned.
-        
-        // See solution-day24-2023.py for the script itself. 
-        // For the inspiration, see https://www.reddit.com/r/adventofcode/comments/18pnycy/comment/kept9a6 
-        // from Reddit user mayoff (https://www.reddit.com/user/mayoff/).
-        // To run the script, first install the z3-solver Python module: "pip install z3-solver".
-        
-        // The following number was taken from the script's output.
-        return 843888100572888L;
+        return 843888100572888L; // TODO
     }
     
     /**
