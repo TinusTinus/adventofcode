2f2511d657a08fa60f35256eeebb71de0760687a
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-01-31 19:12:39 +0100

    Solved 2015 day 1 part 1

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/NotQuiteLispPart1.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/NotQuiteLispPart1.kt
@@ -1,20 +1,27 @@
 package nl.mvdr.adventofcode.adventofcode2015
 
-fun main() {
-    val lines = object {}.javaClass.getResourceAsStream("input-day01-2015.txt")!!.bufferedReader().readLines()
-    println(solve(lines))
+import nl.mvdr.adventofcode.LinesSolver
+import java.util.stream.Stream
+
+class NotQuiteLispPart1: LinesSolver<Int> {
+    override fun solve(lines: Stream<String>?): Int = solve(lines!!.toList())
+
+    private fun solve(lines: List<String>): Int = countInstructions(lines.first())
+
+    private fun countInstructions(text: String): Int {
+        return text.toCharArray()
+            .map {
+                when (it) {
+                    '(' -> 1
+                    ')' -> -1
+                    else -> throw IllegalArgumentException("Unexpected character found in input: $it")
+                }
+            }
+            .sum()
+    }
 }
 
-fun solve(lines: List<String>): Int = solve(lines.first())
-
-fun solve(text: String): Int {
-    return text.toCharArray()
-        .map {
-            when (it) {
-                '(' -> 1
-                ')' -> -1
-                else -> throw IllegalArgumentException("Unexpected character found in input: $it")
-            }
-        }
-        .sum()
+fun main() {
+    val result = NotQuiteLispPart1().solve("input-day01-2015.txt")
+    println(result) // TODO use a logger instead?
 }
