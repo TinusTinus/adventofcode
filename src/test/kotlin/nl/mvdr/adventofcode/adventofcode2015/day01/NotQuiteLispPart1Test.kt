5a1dd71a8fd7b7a1557fba5f3ae60f3ee092c01b
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-01-31 20:10:02 +0100

    Added unit test cases based on examples

--- src/test/kotlin/nl/mvdr/adventofcode/adventofcode2015/day01/NotQuiteLispPart1Test.kt
+++ src/test/kotlin/nl/mvdr/adventofcode/adventofcode2015/day01/NotQuiteLispPart1Test.kt
@@ -8,7 +8,18 @@ class NotQuiteLispPart1Test: SolverTest<NotQuiteLispPart1>(NotQuiteLispPart1::cl
         /** @return arguments for {@link SolverTest#testSolution(String, String)} */
         @JvmStatic
         fun testSolution(): List<Arguments> {
-            return listOf(Arguments.of("280", "input-day01-2015.txt"))
+            return listOf(
+                Arguments.of("280", "input-day01-2015.txt"),
+                Arguments.of("0", "example-day01-2015-0.txt"),
+                Arguments.of("0", "example-day01-2015-1.txt"),
+                Arguments.of("3", "example-day01-2015-2.txt"),
+                Arguments.of("3", "example-day01-2015-3.txt"),
+                Arguments.of("3", "example-day01-2015-4.txt"),
+                Arguments.of("-1", "example-day01-2015-5.txt"),
+                Arguments.of("-1", "example-day01-2015-6.txt"),
+                Arguments.of("-3", "example-day01-2015-7.txt"),
+                Arguments.of("-3", "example-day01-2015-8.txt")
+            )
         }
     }
 }
