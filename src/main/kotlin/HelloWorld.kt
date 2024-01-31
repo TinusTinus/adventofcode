095498e6d883ee6c45253638933e76a8d6528cba
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-01-31 17:10:42 +0100

    Playing around with imports and operators

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -2,6 +2,29 @@ import nl.mvdr.adventofcode.point.Point
 
 fun main() {
     println("Hello world, this is a Kotlin function")
-    val point = Point(0, 1)
-    println(point)
+
+    // Add some Kotlin operators to the Point class
+    operator fun Point.plus(other: Point) = this.add(other)
+    operator fun Point.minus(other: Point) = this.subtract(other)
+    operator fun Point.unaryMinus() = this.negate()
+    operator fun Point.times(i: Int) = this.times(i)
+    operator fun Int.times(point: Point) = point.times(this)
+
+    val point0 = Point(0, 1)
+    val point1 = Point(2, 3)
+
+    val sum = point0 + point1
+    println("$point0 + $point1 = $sum")
+
+    val negative = -point1
+    println("-($point1) = $negative")
+
+    val lessThan = point0 < point1
+    println("$point0 < $point1 : $lessThan")
+
+    val product = point0 * 3
+    println("$point0 * 3 = $product")
+
+    val product1 = 3 * point0
+    println("3 * $point0 = $product1")
 }
