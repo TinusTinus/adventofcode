e33766630d07629311d3f1664392a476a9401738
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-02-09 19:37:48 +0100

    Added a helper function

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day04/AdventCoin.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day04/AdventCoin.kt
@@ -2,12 +2,9 @@ package nl.mvdr.adventofcode.adventofcode2015.day04
 
 import org.apache.commons.codec.digest.DigestUtils
 
-/**
- * Mines a coin. Searches for an MD5 hash with the given [prefix], based on the puzzle [input].
- */
-fun mine(input: List<String>, prefix: String): Int {
-    val secretKey = input.first()
-    return generateSequence(0, Int::inc)
-        .filter { DigestUtils.md5Hex(secretKey + it).startsWith(prefix) }
-        .first()
-}
+/** Mines a coin. Searches for an MD5 hash with the given [prefix], based on the puzzle [input]. */
+fun mine(input: List<String>, prefix: String): Int = mine(input.first(), prefix)
+
+private fun mine(secretKey: String, prefix: String) =
+    generateSequence(0, Int::inc)
+        .first { DigestUtils.md5Hex(secretKey + it).startsWith(prefix) }
