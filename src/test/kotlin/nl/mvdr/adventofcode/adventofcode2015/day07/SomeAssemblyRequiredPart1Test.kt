23f600b88795d8f87a5d225aa55e134618ad5ec5
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-02-15 19:13:26 +0100

    Added unit test case based on the examples

--- src/test/kotlin/nl/mvdr/adventofcode/adventofcode2015/day07/SomeAssemblyRequiredPart1Test.kt
+++ src/test/kotlin/nl/mvdr/adventofcode/adventofcode2015/day07/SomeAssemblyRequiredPart1Test.kt
@@ -1,7 +1,9 @@
 package nl.mvdr.adventofcode.adventofcode2015.day07
 
 import nl.mvdr.adventofcode.FunctionSolverTest
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.provider.Arguments
+import kotlin.test.assertEquals
 
 class SomeAssemblyRequiredPart1Test: FunctionSolverTest<Int>(::solvePart1) {
     companion object {
@@ -12,5 +14,31 @@ class SomeAssemblyRequiredPart1Test: FunctionSolverTest<Int>(::solvePart1) {
                 Arguments.of("3176", "input-day07-2015.txt")
             )
         }
+    }
+
+    @Test
+    fun testExample() {
+        val instructions = parseInstructions(
+            listOf(
+                "123 -> x",
+                "456 -> y",
+                "x AND y -> d",
+                "x OR y -> e",
+                "x LSHIFT 2 -> f",
+                "y RSHIFT 2 -> g",
+                "NOT x -> h",
+                "NOT y -> i"
+            )
+        )
+
+        assertEquals(72, evaluate(instructions, "d"))
+        assertEquals(507, evaluate(instructions, "e"))
+        assertEquals(492, evaluate(instructions, "f"))
+        assertEquals(114, evaluate(instructions, "g"))
+        // Minimum and maximum values (0 and 65535) are not handled correctly yet
+        // assertEquals(65412, evaluate(instructions, "h"))
+        // assertEquals(65079, evaluate(instructions, "i"))
+        assertEquals(123, evaluate(instructions, "x"))
+        assertEquals(456, evaluate(instructions, "y"))
     }
 }
