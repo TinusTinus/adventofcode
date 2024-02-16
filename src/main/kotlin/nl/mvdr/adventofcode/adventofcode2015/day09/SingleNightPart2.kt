7ef8e03d69fd8083df53ef3e72fdd0dc49899458
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-02-16 17:28:24 +0100

    Started part 2

---
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day09/SingleNightPart2.kt
@@ -0,0 +1,15 @@
+package nl.mvdr.adventofcode.adventofcode2015.day09
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import nl.mvdr.adventofcode.FunctionSolver
+
+private val logger = KotlinLogging.logger{}
+
+fun solvePart2(lines: List<String>): Int {
+    return 0 // TODO
+}
+
+fun main() {
+    val result = FunctionSolver(::solvePart2).solve("input-day09-2015.txt")
+    logger.info { result }
+}
---
+++ src/test/kotlin/nl/mvdr/adventofcode/adventofcode2015/day09/SingleNightPart2Test.kt
@@ -0,0 +1,17 @@
+package nl.mvdr.adventofcode.adventofcode2015.day09
+
+import nl.mvdr.adventofcode.FunctionSolverTest
+import org.junit.jupiter.params.provider.Arguments
+
+class SingleNightPart2Test: FunctionSolverTest<Int>(::solvePart2) {
+    companion object {
+
+        @JvmStatic
+        fun testSolution(): List<Arguments> {
+            return listOf(
+                Arguments.of("982", "example-day09-2015.txt"),
+                Arguments.of("?", "input-day09-2015.txt")
+            )
+        }
+    }
+}
