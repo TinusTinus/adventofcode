7ea46e73730f01ed7aba4de2252d0c5aab571a12
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-02-22 15:10:15 +0100

    Recursive solution to day 10 part 1

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day10/ElvesLookElvesSayPart1.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day10/ElvesLookElvesSayPart1.kt
@@ -8,12 +8,22 @@ private val logger = KotlinLogging.logger{}
 fun solvePart1(lines: List<String>): Int = lookAndSay(lines.first(), 40).length
 
 fun lookAndSay(input: String, times: Int): String = when {
-        times < 0 -> throw IllegalArgumentException("times was $times, must be positive")
-        times == 0 -> input
-        else -> lookAndSay(lookAndSay(input), times - 1)
-    }
+    times < 0 -> throw IllegalArgumentException("times was $times, must be positive")
+    times == 0 -> input
+    else -> lookAndSay(lookAndSay(input), times - 1)
+}
 
-fun lookAndSay(input: String) = "3" // TODO
+fun lookAndSay(input: String): String = when {
+    input.isEmpty() -> input
+    else -> {
+        val firstCharacter = input.first()
+        var count = input.indexOfFirst { firstCharacter != it }
+        if (count < 0) {
+            count = input.length
+        }
+        "$count$firstCharacter" + lookAndSay(input.substring(count))
+    }
+}
 
 fun main() {
     val result = FunctionSolver(::solvePart1).solve("input-day10-2015.txt")
