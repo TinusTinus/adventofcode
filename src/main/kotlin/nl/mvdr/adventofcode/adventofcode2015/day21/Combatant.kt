72cd7081922af916650a418a100e621911d01778
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-02-23 11:05:40 +0100

    Slight refactoring

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day21/Combatant.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day21/Combatant.kt
@@ -9,8 +9,8 @@ interface Combatant {
 
     /** Calculates how many turns it would take for this combatant to kill the given [opponent]. */
     fun turnsToKill(opponent: Combatant): Int = when {
-        damage <= opponent.armor -> Int.MAX_VALUE // effectively infinity: the opponent will never take damage
-        else -> ceil(opponent.hitPoints.toDouble() / (damage - opponent.armor).toDouble()).toInt()
+        opponent.armor < damage -> ceil(opponent.hitPoints.toDouble() / (damage - opponent.armor).toDouble()).toInt()
+        else -> Int.MAX_VALUE // the opponent has too much armor and will never take damage
     }
 }
 
