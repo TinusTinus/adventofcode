45b512a0cbc10d0170b47900492e755b08fabeb8
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-03-01 10:34:58 +0100

    Added some test cases

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day11/CorporatePolicyPart1.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day11/CorporatePolicyPart1.kt
@@ -9,6 +9,10 @@ fun solvePart1(lines: List<String>): String = findNextPassword(lines.first())
 
 fun findNextPassword(currentPassword: String): String = "derp" // TODO implement
 
+fun increment(password: String): String = "derp" // TODO implement
+
+fun isValidPassword(password: String) = true // TODO implement
+
 fun main() {
     val result = FunctionSolver(::solvePart1).solve("input-day11-2015.txt")
     logger.info { result }
--- src/test/kotlin/nl/mvdr/adventofcode/adventofcode2015/day11/CorporatePolicyPart1Test.kt
+++ src/test/kotlin/nl/mvdr/adventofcode/adventofcode2015/day11/CorporatePolicyPart1Test.kt
@@ -1,13 +1,39 @@
 package nl.mvdr.adventofcode.adventofcode2015.day11
 
 import nl.mvdr.adventofcode.FunctionSolverTest
+import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import kotlin.test.assertEquals
 
 class CorporatePolicyPart1Test: FunctionSolverTest<String>(::solvePart1) {
+    @ParameterizedTest
+    @MethodSource
+    fun testIncrement(expected: String, currentPassword: String) = assertEquals(expected, increment(currentPassword))
+
+    @ParameterizedTest
+    @MethodSource
+    fun testFindNextPassword(expected: String, currentPassword: String) = assertEquals(expected, findNextPassword(currentPassword))
+
     companion object {
         @JvmStatic
         fun testSolution(): List<Arguments> = listOf(
             Arguments.of("?", "input-day11-2015.txt")
+        )
+
+        @JvmStatic
+        fun testIncrement(): List<Arguments> = listOf(
+            Arguments.of("xy", "xx"),
+            Arguments.of("xz", "xy"),
+            Arguments.of("ya", "xy"),
+            Arguments.of("yb", "ya")
+        )
+
+        @JvmStatic
+        fun testFindNextPassword(): List<Arguments> = listOf(
+            Arguments.of("abcdffaa", "abcdefgh"),
+            Arguments.of("ghjaabcc", "ghijklmn"),
+            Arguments.of("?", "cqjxjnds")
         )
     }
 }
