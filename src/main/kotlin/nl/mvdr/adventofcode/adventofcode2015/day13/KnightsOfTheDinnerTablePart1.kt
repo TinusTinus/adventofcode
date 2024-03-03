14a05c799dfab8e5df47629d1840c25756c3ebf8
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-03-03 16:51:11 +0100

    Started day 13

---
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day13/KnightsOfTheDinnerTablePart1.kt
@@ -0,0 +1,55 @@
+package nl.mvdr.adventofcode.adventofcode2015.day13
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import nl.mvdr.adventofcode.FunctionSolver
+import nl.mvdr.adventofcode.permutations.Permutations
+import kotlin.streams.asSequence
+
+private val logger = KotlinLogging.logger{}
+
+fun solvePart1(lines: List<String>): Int {
+    val happiness = parseHappiness(lines)
+
+    val names = happiness.keys
+        .map { it.first }
+        .toSet()
+
+    return Permutations.generatePermutations(names)
+        .asSequence()
+        .maxOf { calculateTotalHappinessChange(it, happiness) }
+}
+
+/**
+ * Parses the [lines] of the puzzle input as a map containing changes in happiness.
+ *
+ * The value
+ *   "Alice would gain 54 happiness units by sitting next to Bob."
+ * translates to:
+ *   happiness[(Alice, Bob)] = 54
+ *
+ * The value
+ *   "Alice would lose 79 happiness units by sitting next to Carol."
+ * translates to
+ *   happiness[(Alice, Carol)] = -79
+ */
+private fun parseHappiness(lines: List<String>): Map<Pair<String, String>, Int> {
+    val result = mutableMapOf<Pair<String, String>, Int>()
+    for (line in lines) {
+        val (name, happinessModifier, neighbour) = line.split(" would ", " happiness units by sitting next to ")
+        val (signumString, happinessValueString) = happinessModifier.split(" ")
+        val signum = when(signumString) {
+            "gain" -> 1
+            "lose" -> -1
+            else -> throw IllegalArgumentException("Unexpected value: '$signumString'")
+        }
+        result[Pair(name, neighbour)] = signum * happinessValueString.toInt()
+    }
+    return result
+}
+
+private fun calculateTotalHappinessChange(seating: List<String>, happiness: Map<Pair<String, String>, Int>) = 0 // TODO implement!
+
+fun main() {
+    val result = FunctionSolver(::solvePart1).solve("input-day13-2015.txt")
+    logger.info { result }
+}
---
+++ src/test/kotlin/nl/mvdr/adventofcode/adventofcode2015/day13/KnightsOfTheDinnerTablePart1Test.kt
@@ -0,0 +1,14 @@
+package nl.mvdr.adventofcode.adventofcode2015.day13
+
+import nl.mvdr.adventofcode.FunctionSolverTest
+import org.junit.jupiter.params.provider.Arguments
+
+class KnightsOfTheDinnerTablePart1Test: FunctionSolverTest<Int>(::solvePart1) {
+    companion object {
+        @JvmStatic
+        fun testSolution(): List<Arguments> = listOf(
+            Arguments.of("330", "example-day13-2015.txt"),
+            Arguments.of("?", "input-day13-2015.txt")
+        )
+    }
+}
