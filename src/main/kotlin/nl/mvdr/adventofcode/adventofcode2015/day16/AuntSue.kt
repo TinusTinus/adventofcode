5bd5acd5fb9c1e36abe8991b69b135e2e7570035
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-03-04 19:34:36 +0100

    Refactoring

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day16/AuntSue.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day16/AuntSue.kt
@@ -1,14 +1,16 @@
 package nl.mvdr.adventofcode.adventofcode2015.day16
 
 data class AuntSue(val number: Int, private val compounds: Map<Compound, Int>) {
-    fun matchesCompounds(outdatedRetroencabulator: Boolean = false) = compounds.all { matches(it.key, it.value, outdatedRetroencabulator) }
+    fun matches(outdatedRetroencabulator: Boolean = false) = compounds.keys.all { matches(it, outdatedRetroencabulator) }
+
+    private fun matches(compound: Compound, outdatedRetroencabulator: Boolean) = when {
+        outdatedRetroencabulator && (compound == Compound.CATS || compound == Compound.TREES) -> compound.requiredNumber < compounds[compound]!!
+        outdatedRetroencabulator && (compound == Compound.POMERANIANS || compound == Compound.GOLDFISH) -> compounds[compound]!! < compound.requiredNumber
+        else -> compounds[compound] == compound.requiredNumber
+    }
 }
 
-private fun matches(compound: Compound, value: Int, outdatedRetroencabulator: Boolean) = when {
-    outdatedRetroencabulator && (compound == Compound.CATS || compound == Compound.TREES) -> compound.requiredNumber < value
-    outdatedRetroencabulator && (compound == Compound.POMERANIANS || compound == Compound.GOLDFISH) -> value < compound.requiredNumber
-    else -> value == compound.requiredNumber
-}
+
 
 /**
  * Parses the given [text] as the string representation of an Aunt Sue.
--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day16/AuntSuePart2.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day16/AuntSuePart2.kt
@@ -5,7 +5,7 @@ import nl.mvdr.adventofcode.FunctionSolver
 
 private val logger = KotlinLogging.logger{}
 
-fun solvePart2(lines: List<String>) = lines.map(::parseSue).find { it.matchesCompounds(true) }!!.number
+fun solvePart2(lines: List<String>) = lines.map(::parseSue).find { it.matches(true) }!!.number
 
 fun main() {
     val result = FunctionSolver(::solvePart2).solve("input-day16-2015.txt")
