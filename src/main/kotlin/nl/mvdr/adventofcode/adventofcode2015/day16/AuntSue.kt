05be4414a4aa72b6bc29444a7990dbbcca93607b
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-03-04 19:28:04 +0100

    Refactoring

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day16/AuntSue.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day16/AuntSue.kt
@@ -1,26 +1,13 @@
 package nl.mvdr.adventofcode.adventofcode2015.day16
 
-private val requiredCompounds = mapOf(
-    Pair(Compound.CHILDREN, 3),
-    Pair(Compound.CATS, 7),
-    Pair(Compound.SAMOYEDS, 2),
-    Pair(Compound.POMERANIANS, 3),
-    Pair(Compound.AKITAS, 0),
-    Pair(Compound.VIZSLAS, 0),
-    Pair(Compound.GOLDFISH, 5),
-    Pair(Compound.TREES, 3),
-    Pair(Compound.CARS, 2),
-    Pair(Compound.PERFUMES, 1)
-)
-
 data class AuntSue(val number: Int, private val compounds: Map<Compound, Int>) {
     fun matchesCompounds(outdatedRetroencabulator: Boolean = false) = compounds.all { matches(it.key, it.value, outdatedRetroencabulator) }
 }
 
 private fun matches(compound: Compound, value: Int, outdatedRetroencabulator: Boolean) = when {
-    outdatedRetroencabulator && (compound == Compound.CATS || compound == Compound.TREES) -> requiredCompounds[compound]!! < value
-    outdatedRetroencabulator && (compound == Compound.POMERANIANS || compound == Compound.GOLDFISH) -> value < requiredCompounds[compound]!!
-    else -> value == requiredCompounds[compound]
+    outdatedRetroencabulator && (compound == Compound.CATS || compound == Compound.TREES) -> compound.requiredNumber < value
+    outdatedRetroencabulator && (compound == Compound.POMERANIANS || compound == Compound.GOLDFISH) -> value < compound.requiredNumber
+    else -> value == compound.requiredNumber
 }
 
 /**
--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day16/Compound.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day16/Compound.kt
@@ -1,16 +1,16 @@
 package nl.mvdr.adventofcode.adventofcode2015.day16
 
-enum class Compound(val text: String) {
-    CHILDREN("children"),
-    CATS("cats"),
-    SAMOYEDS("samoyeds"),
-    POMERANIANS("pomeranians"),
-    AKITAS("akitas"),
-    VIZSLAS("vizslas"),
-    GOLDFISH("goldfish"),
-    TREES("trees"),
-    CARS("cars"),
-    PERFUMES("perfumes")
+enum class Compound(val text: String, val requiredNumber: Int) {
+    CHILDREN("children", 3),
+    CATS("cats", 7),
+    SAMOYEDS("samoyeds", 2),
+    POMERANIANS("pomeranians", 3),
+    AKITAS("akitas", 0),
+    VIZSLAS("vizslas", 0),
+    GOLDFISH("goldfish", 5),
+    TREES("trees", 3),
+    CARS("cars", 2),
+    PERFUMES("perfumes", 1)
 }
 
 fun parseCompound(text: String) = Compound.entries.find { it.text == text }!!
