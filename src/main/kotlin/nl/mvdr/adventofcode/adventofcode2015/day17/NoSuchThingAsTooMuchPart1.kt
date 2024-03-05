e2f4be5607c84325047c63ef5dc39d2a78eb4ad8
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-03-05 15:11:26 +0100

    First attempt at day 17

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day17/NoSuchThingAsTooMuchPart1.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day17/NoSuchThingAsTooMuchPart1.kt
@@ -2,12 +2,44 @@ package nl.mvdr.adventofcode.adventofcode2015.day17
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import nl.mvdr.adventofcode.FunctionSolver
+import nl.mvdr.adventofcode.permutations.Permutations
+import kotlin.streams.asSequence
 
 private val logger = KotlinLogging.logger{}
 
-fun solvePart1(lines: List<String>) = 3 // TODO
+/**
+ * A container.
+ *
+ * Note that this is not a data class.
+ * Two different container instances with the same size are purposefully NOT considered equal.
+ */
+class Container(val size: Int) {
+    constructor(size: String) : this(size.toInt())
+}
+
+fun solvePart1(lines: List<String>, eggnogVolume: Int = 150): Int {
+        val containers = lines.map { Container(it) }.toSet()
+        return Permutations.generatePermutations(containers)
+            .asSequence()
+            .map { fitInContainers(it, eggnogVolume) }
+            .filterNotNull()
+            .distinct()
+            .count()
+}
+
+private fun fitInContainers(containers: List<Container>, volume: Int): List<Container>? = when {
+    volume < 0 -> null
+    volume == 0 -> listOf()
+    containers.isNotEmpty() && containers.first().size <= volume -> {
+        when (val tail = fitInContainers(containers, volume - containers.first().size)) {
+            null -> null
+            else -> listOf(containers.first()) + tail
+        }
+    }
+    else -> null
+}
 
 fun main() {
-    val result = FunctionSolver(::solvePart1).solve("input-day16-2015.txt")
+    val result = FunctionSolver(::solvePart1).solve("input-day17-2015.txt")
     logger.info { result }
 }
--- src/test/kotlin/nl/mvdr/adventofcode/adventofcode2015/day17/NoSuchThingAsTooMuchPart1Test.kt
+++ src/test/kotlin/nl/mvdr/adventofcode/adventofcode2015/day17/NoSuchThingAsTooMuchPart1Test.kt
@@ -1,14 +1,23 @@
 package nl.mvdr.adventofcode.adventofcode2015.day17
 
+import nl.mvdr.adventofcode.FunctionSolver
 import nl.mvdr.adventofcode.FunctionSolverTest
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.provider.Arguments
 
 class NoSuchThingAsTooMuchPart1Test: FunctionSolverTest<Int>(::solvePart1) {
     companion object {
         @JvmStatic
         fun testSolution(): List<Arguments> = listOf(
-            Arguments.of("4", "example-day17-2015.txt"),
             Arguments.of("?", "input-day17-2015.txt")
         )
+    }
+
+    @Test
+    fun testExample() {
+        val solver = FunctionSolver { solvePart1(it, 25) }
+        val inputFile = "example-day17-2015.txt"
+
+        assertSolution(solver, "4", inputFile)
     }
 }
