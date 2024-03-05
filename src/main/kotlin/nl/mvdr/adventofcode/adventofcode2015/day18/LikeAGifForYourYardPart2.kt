9203b7b412c09f7d1ba67684f75b03d57a7fc4b4
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-03-05 18:27:07 +0100

    Started part 2

---
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day18/LikeAGifForYourYardPart2.kt
@@ -0,0 +1,13 @@
+package nl.mvdr.adventofcode.adventofcode2015.day18
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import nl.mvdr.adventofcode.FunctionSolver
+
+private val logger = KotlinLogging.logger{}
+
+fun solvePart2(lines: Sequence<String>, steps: Int = 100): Int = parseGrid(lines).step(steps).count() // TODO
+
+fun main() {
+    val result = FunctionSolver(::solvePart2).solve("input-day18-2015.txt")
+    logger.info { result }
+}
---
+++ src/test/kotlin/nl/mvdr/adventofcode/adventofcode2015/day18/LikeAGifForYourYardPart2Test.kt
@@ -0,0 +1,36 @@
+package nl.mvdr.adventofcode.adventofcode2015.day18
+
+import nl.mvdr.adventofcode.FunctionSolver
+import nl.mvdr.adventofcode.FunctionSolverTest
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class LikeAGifForYourYardPart2Test: FunctionSolverTest<Int>(::solvePart2) {
+
+    companion object {
+        @JvmStatic
+        fun testSolution(): List<Arguments> = listOf(
+            Arguments.of("?", "input-day18-2015.txt")
+        )
+
+        @JvmStatic
+        fun testExample(): List<Arguments> = listOf(
+            Arguments.of(0, "15"),
+            Arguments.of(1, "18"),
+            Arguments.of(2, "18"),
+            Arguments.of(3, "17"),
+            Arguments.of(4, "14"),
+            Arguments.of(5, "17"),
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    fun testExample(steps: Int, expectedSolution: String) {
+        val solver = FunctionSolver { solvePart2(it, steps) }
+        val inputFile = "example-day18-2015.txt"
+
+        assertSolution(solver, expectedSolution, inputFile)
+    }
+}
