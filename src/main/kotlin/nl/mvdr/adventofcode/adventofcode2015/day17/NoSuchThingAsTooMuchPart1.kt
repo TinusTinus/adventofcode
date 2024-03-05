f9c9d05b4b973d626f977bc05cab504dee6f8629
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-03-05 15:45:51 +0100

    Refactoring: no need to sort the containers

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day17/NoSuchThingAsTooMuchPart1.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day17/NoSuchThingAsTooMuchPart1.kt
@@ -5,12 +5,7 @@ import nl.mvdr.adventofcode.FunctionSolver
 
 private val logger = KotlinLogging.logger{}
 
-fun solvePart1(lines: List<String>, eggnogVolume: Int = 150): Int = countWaysToFitInContainers(parseContainers(lines), eggnogVolume)
-
-/**
- * Returns a list of the containers, ordered small-to-large, as specified in the given [lines] from the puzzle input.
- */
-private fun parseContainers(lines: List<String>): List<Int> = lines.map(String::toInt).sorted()
+fun solvePart1(lines: List<String>, eggnogVolume: Int = 150): Int = countWaysToFitInContainers(lines.map(String::toInt), eggnogVolume)
 
 private fun countWaysToFitInContainers(containers: List<Int>, eggnogVolume: Int): Int = when {
     eggnogVolume < 0 -> 0 // does not fit
