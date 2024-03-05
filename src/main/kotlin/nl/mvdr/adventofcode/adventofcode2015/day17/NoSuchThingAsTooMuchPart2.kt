97b14b9c17d51f72be663cac882af1c3e55f700f
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-03-05 16:00:29 +0100

    Started part 2

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day17/NoSuchThingAsTooMuchPart2.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day17/NoSuchThingAsTooMuchPart2.kt
@@ -5,9 +5,10 @@ import nl.mvdr.adventofcode.FunctionSolver
 
 private val logger = KotlinLogging.logger{}
 
-fun solvePart1(lines: List<String>, eggnog: Int = 150): Int = countWaysToFitInContainers(lines.map(String::toInt), eggnog)
+fun solvePart2(lines: List<String>, eggnog: Int = 150): Int = countWaysToFitInContainers(lines.map(String::toInt), eggnog) // TODO
+
 
 fun main() {
-    val result = FunctionSolver(::solvePart1).solve("input-day17-2015.txt")
+    val result = FunctionSolver(::solvePart2).solve("input-day17-2015.txt")
     logger.info { result }
 }
