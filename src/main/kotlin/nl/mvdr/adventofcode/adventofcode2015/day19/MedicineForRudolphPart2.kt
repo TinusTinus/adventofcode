3e1d683ac91f041e5180e1f7eab048e80d1559c4
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-03-07 14:11:39 +0100

    Added caching

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day19/MedicineForRudolphPart2.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day19/MedicineForRudolphPart2.kt
@@ -24,21 +24,32 @@ fun solvePart2(linesSequence: Sequence<String>): Int {
     // We also attempt to reduce the length of the string by the largest amount possible in each step,
     // to get to a single-character value "e" as quickly as possible.
     val reverseReplacements = replacements.map(Replacement::reverse).sortedWith(compareBy { it.to.length - it.from.length })
-    return countSteps(medicineMolecule, ELECTRON, reverseReplacements)!!
+    logger.debug { "Reversed replacements: $reverseReplacements" }
+    return countSteps(medicineMolecule, reverseReplacements)!!
 }
+
+/**
+ * Cache of source molecules which have already been inspected.
+ */
+private val observed = mutableSetOf<String>()
 
 /**
  * Finds the smallest number of steps in which [sourceMolecule] can be transformed into [targetMolecule],
  * by applying any of the given [replacements].
  */
-private fun countSteps(sourceMolecule: String, targetMolecule: String, replacements: List<Replacement>): Int? = when(sourceMolecule) {
-    targetMolecule -> 0
-    else -> replacements.asSequence()
-        .flatMap { it.apply(sourceMolecule) }
-        .map { countSteps(it, targetMolecule, replacements) }
-        .filterNotNull()
-        .map { it + 1 }
-        .firstOrNull()
+private fun countSteps(sourceMolecule: String, replacements: List<Replacement>): Int? = when {
+    observed.contains(sourceMolecule) -> null
+    sourceMolecule == ELECTRON -> 0
+    else -> {
+        logger.debug { "counting steps for $sourceMolecule" }
+        observed.add(sourceMolecule)
+        replacements.asSequence()
+            .flatMap { it.apply(sourceMolecule) }
+            .map { countSteps(it, replacements) }
+            .filterNotNull()
+            .map { it + 1 }
+            .firstOrNull()
+    }
 }
 
 fun main() {
