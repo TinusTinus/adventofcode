0949d3b268bb177feff88c1997263f62ed31bdab
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-03-08 12:12:52 +0100

    Solved 2015 day 20 part 2

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day20/ElvesAndHouses.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2015/day20/ElvesAndHouses.kt
@@ -19,20 +19,21 @@ fun presentsDeliveredAt(house: Int, presentsPerHouse: Int, housesPerElf: Int?): 
 private fun sumDivisors(n: Int, max: Int?): Int {
     require(1 < n)
     val maxDivisor = sqrt(n.toDouble()).toInt()
-    var sum = 1
+    var sum = 0
+    if (max == null || n <= max) {
+        sum += 1
+    }
     for (i in 2..maxDivisor) {
         if (n % i == 0) {
-            if (true) { // TODO only if elf i delivers to house n
+            val d: Int = n / i
+            if (max == null || d <= max) {
                 sum += i
             }
-            val d: Int = n / i
-            if (d != i && true) { // TODO only if elf d delivers to house n
+            if (d != i && (max == null || i <= max)) {
                 sum += d
             }
         }
     }
-    if (max == null || n <= max) {
-        sum += n
-    }
+    sum += n
     return sum
 }
--- src/test/kotlin/nl/mvdr/adventofcode/adventofcode2015/day20/InfiniteElvesAndInfiniteHousesPart2Test.kt
+++ src/test/kotlin/nl/mvdr/adventofcode/adventofcode2015/day20/InfiniteElvesAndInfiniteHousesPart2Test.kt
@@ -7,11 +7,7 @@ class InfiniteElvesAndInfiniteHousesPart2Test: FunctionSolverTest<Int>(::solvePa
     companion object {
         @JvmStatic
         fun testSolution(): List<Arguments> = listOf(
-            Arguments.of("?", "input-day20-2015.txt")
-            //  693000 is too low
-            //  831600 is the minimum  (= part 1 answer)
-            // 1164240 is too high
-            // 3272727 is too high
+            Arguments.of("884520", "input-day20-2015.txt")
         )
     }
 }
