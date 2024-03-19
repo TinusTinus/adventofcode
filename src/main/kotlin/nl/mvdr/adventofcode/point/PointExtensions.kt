e2b438619f63461e4ea792f33e10d64d397b86a8
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-03-19 15:07:25 +0100

    Slight refactoring

--- src/main/kotlin/nl/mvdr/adventofcode/point/PointExtensions.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/point/PointExtensions.kt
@@ -5,4 +5,4 @@ operator fun Point.plus(other: Point): Point = this.add(other)
 operator fun Point.minus(other: Point): Point = this.subtract(other)
 operator fun Point.unaryMinus(): Point = this.negate()
 operator fun Point.times(i: Int): Point = this.multiply(i)
-operator fun Int.times(point: Point): Point = point.multiply(this)
+operator fun Int.times(point: Point): Point = point * this
