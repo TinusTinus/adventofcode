d5a607ab2e132bba1ff92f5acab59dd05fc705c8
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-01 22:09:28 +0200

    Added some helper functions

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day08/SevenSegmentSearchPart2.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day08/SevenSegmentSearchPart2.kt
@@ -8,39 +8,57 @@ private val logger = KotlinLogging.logger{}
 fun solvePart2(lines: Sequence<String>) = lines.sumOf(::decodeOutputValue)
 
 /**
- * Counts how many times digits 1, 4, 7, or 8 appear in the given entry's output value.
+ * Decodes the output value in the given [entry].
  */
 private fun decodeOutputValue(entry: String): Int {
+    // Parse the entry.
+    // Each digit is represented as a set of characters,
+    // where each character represents a segment of the display.
     val (uniqueSignalPatterns, outputValue) = entry.split(" | ")
         .map { it.split(" ").map { pattern -> pattern.toCharArray().toSet() } }
 
-    val patterns = mutableMapOf<Int, Set<Char>>()
+    val decoder = createDecoder(uniqueSignalPatterns)
+
+    val outputValueDigits = outputValue.map { decoder[it]!! }
+    return outputValueDigits.joinToString("").toInt()
+}
+
+/**
+ * Creates a decoder, based on the given [uniqueSignalPatterns].
+ * The decoder maps a signal pattern to the corresponding digit.
+ */
+private fun createDecoder(uniqueSignalPatterns: List<Set<Char>>): Map<Set<Char>, Int> =
+    findPatterns(uniqueSignalPatterns).entries.associate { (digit, pattern) -> pattern to digit }
+
+/**
+ * Finds, for each digit, the corresponding unique signal pattern.
+ */
+private fun findPatterns(uniqueSignalPatterns: List<Set<Char>>): Map<Int, Set<Char>> {
+    val result = mutableMapOf<Int, Set<Char>>()
 
     // Two segments: must be a one
-    patterns[1] = uniqueSignalPatterns.first { it.size == 2 }
+    result[1] = uniqueSignalPatterns.first { it.size == 2 }
 
     // Four segments: must be a four
-    patterns[4] = uniqueSignalPatterns.first { it.size == 4 }
+    result[4] = uniqueSignalPatterns.first { it.size == 4 }
 
     // Three segments: must be a seven
-    patterns[7] = uniqueSignalPatterns.first { it.size == 3 }
+    result[7] = uniqueSignalPatterns.first { it.size == 3 }
 
     // Seven segments: must be an eight
-    patterns[8] = uniqueSignalPatterns.first { it.size == 7 }
+    result[8] = uniqueSignalPatterns.first { it.size == 7 }
 
     // Six segments: could be a zero, a six or a nine
-    patterns[9] = uniqueSignalPatterns.first { it.size == 6 && it.containsAll(patterns[4]!!) }
-    patterns[0] = uniqueSignalPatterns.first { it.size == 6 && it != patterns[9] && it.containsAll(patterns[1]!!) }
-    patterns[6] = uniqueSignalPatterns.first { it.size == 6 && it != patterns[9] && it != patterns[0] }
+    result[9] = uniqueSignalPatterns.first { it.size == 6 && it.containsAll(result[4]!!) }
+    result[0] = uniqueSignalPatterns.first { it.size == 6 && it != result[9] && it.containsAll(result[1]!!) }
+    result[6] = uniqueSignalPatterns.first { it.size == 6 && it != result[9] && it != result[0] }
 
     // Five segments: could be a two, a three or a five
-    patterns[3] = uniqueSignalPatterns.first { it.size == 5 && it.containsAll(patterns[1]!!) }
-    patterns[5] = uniqueSignalPatterns.first { it.size == 5 && it != patterns[3] && patterns[6]!!.containsAll(it) }
-    patterns[2] = uniqueSignalPatterns.first { it.size == 5 && it != patterns[3] && it != patterns[5] }
+    result[3] = uniqueSignalPatterns.first { it.size == 5 && it.containsAll(result[1]!!) }
+    result[5] = uniqueSignalPatterns.first { it.size == 5 && it != result[3] && result[6]!!.containsAll(it) }
+    result[2] = uniqueSignalPatterns.first { it.size == 5 && it != result[3] && it != result[5] }
 
-    val decoder = patterns.entries.associate { (digit, pattern) -> pattern to digit }
-
-    return outputValue.map { decoder[it]!! }.joinToString("").toInt()
+    return result
 }
 
 fun main() {
