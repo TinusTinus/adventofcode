7e5a9d8c1a9bfb36986c2af7375b63afa4b50c13
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-01 21:57:12 +0200

    Solved 2021 day 8 part 2

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day08/SevenSegmentSearchPart2.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day08/SevenSegmentSearchPart2.kt
@@ -11,41 +11,37 @@ fun solvePart2(lines: Sequence<String>) = lines.sumOf(::decodeOutputValue)
  * Counts how many times digits 1, 4, 7, or 8 appear in the given entry's output value.
  */
 private fun decodeOutputValue(entry: String): Int {
-    val (uniqueSignalPatterns, outputValue) = entry.split(" | ").map { it.split(" ") }
+    val (uniqueSignalPatterns, outputValue) = entry.split(" | ")
+        .map { it.split(" ").map { pattern -> pattern.toCharArray().toSet() } }
 
-    val patterns = mutableMapOf<Int, String>()
+    val patterns = mutableMapOf<Int, Set<Char>>()
 
     // Two segments: must be a one
-    patterns[1] = uniqueSignalPatterns.first { it.length == 2 }
+    patterns[1] = uniqueSignalPatterns.first { it.size == 2 }
 
     // Four segments: must be a four
-    patterns[4] = uniqueSignalPatterns.first { it.length == 4 }
+    patterns[4] = uniqueSignalPatterns.first { it.size == 4 }
 
     // Three segments: must be a seven
-    patterns[7] = uniqueSignalPatterns.first { it.length == 3 }
+    patterns[7] = uniqueSignalPatterns.first { it.size == 3 }
 
     // Seven segments: must be an eight
-    patterns[8] = uniqueSignalPatterns.first { it.length == 7 }
+    patterns[8] = uniqueSignalPatterns.first { it.size == 7 }
 
     // Six segments: could be a zero, a six or a nine
-    patterns[9] = uniqueSignalPatterns.first { it.length == 6 && containsAll(it, patterns[4]!!) }
-    patterns[0] = uniqueSignalPatterns.first { it.length == 6 && it != patterns[9] && containsAll(it, patterns[1]!!) }
-    patterns[6] = uniqueSignalPatterns.first { it.length == 6 && it != patterns[9] && it != patterns[0] }
+    patterns[9] = uniqueSignalPatterns.first { it.size == 6 && it.containsAll(patterns[4]!!) }
+    patterns[0] = uniqueSignalPatterns.first { it.size == 6 && it != patterns[9] && it.containsAll(patterns[1]!!) }
+    patterns[6] = uniqueSignalPatterns.first { it.size == 6 && it != patterns[9] && it != patterns[0] }
 
     // Five segments: could be a two, a three or a five
-    patterns[3] = uniqueSignalPatterns.first { it.length == 5 && containsAll(it, patterns[1]!!) }
-    patterns[5] = uniqueSignalPatterns.first { it.length == 5 && it != patterns[3] && containsAll(patterns[6]!!, it) }
-    patterns[2] = uniqueSignalPatterns.first { it.length == 5 && it != patterns[3] && it != patterns[5] }
+    patterns[3] = uniqueSignalPatterns.first { it.size == 5 && it.containsAll(patterns[1]!!) }
+    patterns[5] = uniqueSignalPatterns.first { it.size == 5 && it != patterns[3] && patterns[6]!!.containsAll(it) }
+    patterns[2] = uniqueSignalPatterns.first { it.size == 5 && it != patterns[3] && it != patterns[5] }
 
     val decoder = patterns.entries.associate { (digit, pattern) -> pattern to digit }
 
     return outputValue.map { decoder[it]!! }.joinToString("").toInt()
 }
-
-/**
- * Checks whether [lhs] contains all characters in [rhs].
- */
-private fun containsAll(lhs: String, rhs: String) = lhs.toCharArray().toList().containsAll(rhs.toCharArray().toList())
 
 fun main() {
     val result = FunctionSolver(::solvePart2).solve("input-day08-2021.txt")
