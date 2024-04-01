d0d75944e36c3188cdc166c683dcc0f2c60fad7c
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-01 20:29:44 +0200

    Added some helper functions for readability.

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day07/TreacheryOfWhales.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day07/TreacheryOfWhales.kt
@@ -6,10 +6,20 @@ import kotlin.math.abs
  * Solves the puzzle, given the [lines] from the puzzle input.
  * The [fuelFunction] specifies how to compute the fuel cost when moving a given distance.
  */
-fun solve(lines: Sequence<String>, fuelFunction: (Int) -> Int): Int {
-    val initialPositions = lines.first().split(",").map(String::toInt)
-    return (initialPositions.min() .. initialPositions.max()).minOf { computeFuelCost(initialPositions, it, fuelFunction) }
-}
+fun solve(lines: Sequence<String>, fuelFunction: (Int) -> Int) = solve(parse(lines), fuelFunction)
+
+/**
+ * Parses the puzzle input as a list containing the crabs' initial positions.
+ * The first of the given [lines] should contain a comma-separated list of horizontal positions.
+ * For example: "16,1,2,0,4,2,7,1,2,14"
+ */
+private fun parse(lines: Sequence<String>) = lines.first().split(",").map(String::toInt)
+
+/**
+ * Solves the puzzle, given the crabs' [initialPositions].
+ */
+private fun solve(initialPositions: List<Int>, fuelFunction: (Int) -> Int) =
+    (initialPositions.min()..initialPositions.max()).minOf { computeFuelCost(initialPositions, it, fuelFunction) }
 
 /**
  * Computes the total fuel cost for the crabs to move from their given [initialPositions] to the given [targetPosition].
