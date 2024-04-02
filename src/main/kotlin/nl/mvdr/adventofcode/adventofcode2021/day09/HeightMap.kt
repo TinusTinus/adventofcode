f59670c356e416f9e6eaabec8e5a22e6512d5483
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-02 16:09:19 +0200

    Refactoring

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day09/HeightMap.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day09/HeightMap.kt
@@ -5,15 +5,13 @@ import nl.mvdr.adventofcode.point.Point
 data class HeightMap(private val heights: Map<Point, Int>) {
     constructor(lines: Sequence<String>) : this(Point.parse2DMap(lines.toList()) { character -> character.toString().toInt() })
 
-    fun sumLowPointRiskLevels() = heights.entries
-        .filter { isLowPoint(it.key) }
-        .sumOf { it.value + 1 }
+    fun sumLowPointRiskLevels() = findLowPoints().sumOf(this::riskValue)
 
-    /**
-     * Checks whether the given [point] is a low point.
-     * The given point must be an existing point in the height map.
-     */
+    private fun findLowPoints(): List<Point> = heights.keys.filter(this::isLowPoint)
+
     private fun isLowPoint(point: Point) = point.neighbours()
         .map { heights[it] }
         .all { it == null || heights[point]!! < it }
+
+    private fun riskValue(point: Point) = heights[point]!! + 1
 }
