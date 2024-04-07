c2ad2321dd01b2c48babc111c622211a0a034dc0
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-07 12:11:50 +0200

    Solved 2021 day 13 part 1

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day13/FoldInstruction.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day13/FoldInstruction.kt
@@ -5,8 +5,18 @@ import nl.mvdr.adventofcode.point.Point
 data class FoldInstruction(val axis: Axis, val index: Int) {
     fun perform(dots: Set<Point>) = dots.map(this::fold).toSet()
 
-    private fun fold(point: Point): Point {
-        return point // TODO implement!
+    /**
+     * Determines the new location of the given [dot] after performing this fold instruction.
+     */
+    private fun fold(dot: Point): Point = axis.updateValue(dot, fold(axis.getValue(dot)))
+
+    /**
+     * Determines the new location for the given [value] along this instruction's [axis].
+     */
+    private fun fold(value: Int) = when {
+        value < index -> value
+        value == index -> throw IllegalStateException("Dot found along folding axis.")
+        else -> index - (value - index)
     }
 }
 
