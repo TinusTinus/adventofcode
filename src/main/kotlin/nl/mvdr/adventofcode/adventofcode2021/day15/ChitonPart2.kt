6a746916acc20839ce77e2728492fde2cff85553
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-10 14:24:41 +0200

    WIP on part 2

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day15/ChitonPart2.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day15/ChitonPart2.kt
@@ -3,13 +3,24 @@ package nl.mvdr.adventofcode.adventofcode2021.day15
 import io.github.oshai.kotlinlogging.KotlinLogging
 import nl.mvdr.adventofcode.FunctionSolver
 import nl.mvdr.adventofcode.point.Point
+import nl.mvdr.adventofcode.point.plus
 
 private val logger = KotlinLogging.logger{}
 
 fun solvePart2(lines: Sequence<String>) = solve(expand(parseRisks(lines)))
 
 private fun expand(risks: Map<Point, Int>): Map<Point, Int> {
-    return risks // TODO
+    val result = mutableMapOf<Point, Int>()
+
+    for (xOffset in 0 until 5) {
+        for (yOffset in 0 until 5) {
+            val pointOffset = Point(xOffset, yOffset)
+            val valueOffset = xOffset + yOffset
+            risks.entries.forEach { result[it.key + pointOffset] = (it.value + valueOffset) % 10 } // TODO this risk offset calculation is incorrect
+        }
+    }
+
+    return result
 }
 
 
