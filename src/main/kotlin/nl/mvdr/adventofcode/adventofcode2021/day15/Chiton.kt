08798ac9e546343b30fa1803e82f8248fec3d758
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-10 14:10:25 +0200

    Refactoring in preparation for part 2

---
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day15/Chiton.kt
@@ -0,0 +1,49 @@
+package nl.mvdr.adventofcode.adventofcode2021.day15
+
+import nl.mvdr.adventofcode.point.Point
+import org.jgrapht.Graph
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath
+import org.jgrapht.graph.DefaultEdge
+import org.jgrapht.graph.SimpleDirectedGraph
+import org.jgrapht.graph.SimpleDirectedWeightedGraph
+
+/**
+ * Parses the [lines] from the puzzle input as a map containing risk values.
+ */
+fun parseRisks(lines: Sequence<String>): MutableMap<Point, Int> =
+    Point.parse2DMap(lines.toList()) { char -> char.toString().toInt() }
+
+/**
+ * Finds the total risk of the least risky path, for the given [risks].
+ */
+fun solve(risks: MutableMap<Point, Int>): Int {
+    val graph = createGraph(risks)
+
+    val start = Point(0, 0)
+    val finish = Point(Point.maxX(risks.keys), Point.maxY(risks.keys))
+
+    val path = DijkstraShortestPath.findPathBetween(graph, start, finish)
+
+    return path.weight.toInt()
+}
+
+private fun createGraph(risks: MutableMap<Point, Int>): Graph<Point, DefaultEdge> {
+    val graph = SimpleDirectedWeightedGraph<Point, DefaultEdge>(DefaultEdge::class.java)
+
+    // Add vertices
+    risks.keys.forEach(graph::addVertex)
+
+    // Add edges
+    risks.keys.forEach { point -> point
+        .neighbours()
+        .filter(graph::containsVertex)
+        .forEach { neighbour -> addWeightedEdge(graph, point, neighbour, risks[neighbour]!!) }
+    }
+
+    return graph
+}
+
+private fun addWeightedEdge(graph: SimpleDirectedGraph<Point, DefaultEdge>, source: Point, target: Point, risk: Int) {
+    val edge = graph.addEdge(source, target)
+    graph.setEdgeWeight(edge, risk.toDouble())
+}
--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day15/ChitonPart1.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day15/ChitonPart1.kt
@@ -3,45 +3,10 @@ package nl.mvdr.adventofcode.adventofcode2021.day15
 import io.github.oshai.kotlinlogging.KotlinLogging
 import nl.mvdr.adventofcode.FunctionSolver
 import nl.mvdr.adventofcode.point.Point
-import org.jgrapht.Graph
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath
-import org.jgrapht.graph.*
 
 private val logger = KotlinLogging.logger{}
 
-fun solvePart1(lines: Sequence<String>): Int {
-    val risks = Point.parse2DMap(lines.toList()) { char -> char.toString().toInt() }
-
-    val graph = createGraph(risks)
-
-    val start = Point(0, 0)
-    val finish = Point(Point.maxX(risks.keys), Point.maxY(risks.keys))
-
-    val path = DijkstraShortestPath.findPathBetween(graph, start, finish)
-
-    return path.weight.toInt()
-}
-
-private fun createGraph(risks: MutableMap<Point, Int>): Graph<Point, DefaultEdge> {
-    val graph = SimpleDirectedWeightedGraph<Point, DefaultEdge>(DefaultEdge::class.java)
-
-    // Add vertices
-    risks.keys.forEach(graph::addVertex)
-
-    // Add edges
-    risks.keys.forEach { point -> point
-        .neighbours()
-        .filter(graph::containsVertex)
-        .forEach { neighbour -> createEdge(graph, point, neighbour, risks) }
-    }
-
-    return graph
-}
-
-private fun createEdge(graph: SimpleDirectedGraph<Point, DefaultEdge>, source: Point, target: Point, risks: MutableMap<Point, Int>) {
-    val edge = graph.addEdge(source, target)
-    graph.setEdgeWeight(edge, risks[target]!!.toDouble())
-}
+fun solvePart1(lines: Sequence<String>) = solve(parseRisks(lines))
 
 fun main() {
     val result = FunctionSolver(::solvePart1).solve("input-day15-2021.txt")
