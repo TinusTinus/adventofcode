1ea7aa8502d5733700922c690794508e8c7f93b1
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-14 21:10:11 +0200

    Started to add a unit test for addition

--- src/test/kotlin/nl/mvdr/adventofcode/adventofcode2021/day18/SnailfishNumberTest.kt
+++ src/test/kotlin/nl/mvdr/adventofcode/adventofcode2021/day18/SnailfishNumberTest.kt
@@ -17,6 +17,18 @@ class SnailfishNumberTest {
         assertEquals(magnitude, result)
     }
 
+    @ParameterizedTest
+    @MethodSource
+    fun testPlus(leftText: String, rightText: String, sumText: String) {
+        val left = parseSnailfishNumber(leftText)
+        val right = parseSnailfishNumber(rightText)
+        val sum = parseSnailfishNumber(sumText)
+
+        val result = left + right
+
+        assertEquals(sum, result)
+    }
+
     companion object {
         @JvmStatic
         fun testMagnitude(): List<Arguments> = listOf(
@@ -30,6 +42,23 @@ class SnailfishNumberTest {
             Arguments.of("[[[[5,0],[7,4]],[5,5]],[6,6]]", 1137),
             Arguments.of("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", 3488),
             Arguments.of("[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]", 4140)
+        )
+
+        @JvmStatic
+        fun testPlus(): List<Arguments> = listOf(
+            Arguments.of("[[[[4,3],4],4],[7,[[8,4],9]]]", "[1,1]", "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"),
+            Arguments.of("[]", "[]", "[]"),
+            Arguments.of("[]", "[]", "[]"),
+            Arguments.of("[]", "[]", "[]"),
+            Arguments.of("[]", "[]", "[]"),
+            Arguments.of("[]", "[]", "[]"),
+            Arguments.of("[]", "[]", "[]"),
+            Arguments.of("[]", "[]", "[]"),
+            Arguments.of("[]", "[]", "[]"),
+            Arguments.of("[]", "[]", "[]"),
+            Arguments.of("[]", "[]", "[]"),
+            Arguments.of("[]", "[]", "[]"),
+
         )
     }
 }
