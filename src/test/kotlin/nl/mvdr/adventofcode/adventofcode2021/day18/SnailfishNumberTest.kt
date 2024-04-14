e40f2d5fb1b67b82c95ca794743c75164516a0ef
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-14 21:02:03 +0200

    Added a unit test for magnitude calculation

---
+++ src/test/kotlin/nl/mvdr/adventofcode/adventofcode2021/day18/SnailfishNumberTest.kt
@@ -0,0 +1,35 @@
+package nl.mvdr.adventofcode.adventofcode2021.day18
+
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import kotlin.test.assertEquals
+
+class SnailfishNumberTest {
+
+    @ParameterizedTest
+    @MethodSource
+    fun testMagnitude(snailfishNumberText: String, magnitude: Int) {
+        val number = parseSnailfishNumber(snailfishNumberText)
+
+        val result = number.magnitude()
+
+        assertEquals(magnitude, result)
+    }
+
+    companion object {
+        @JvmStatic
+        fun testMagnitude(): List<Arguments> = listOf(
+            Arguments.of("[9,1]", 29),
+            Arguments.of("[1,9]", 21),
+            Arguments.of("[[9,1],[1,9]]", 129),
+            Arguments.of("[[1,2],[[3,4],5]]", 143),
+            Arguments.of("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", 1384),
+            Arguments.of("[[[[1,1],[2,2]],[3,3]],[4,4]]", 445),
+            Arguments.of("[[[[3,0],[5,3]],[4,4]],[5,5]]", 791),
+            Arguments.of("[[[[5,0],[7,4]],[5,5]],[6,6]]", 1137),
+            Arguments.of("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", 3488),
+            Arguments.of("[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]", 4140)
+        )
+    }
+}
