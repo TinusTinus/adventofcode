f38c00c062904e6bff3e6094ea1c8647c32bd3ec
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-19 12:40:07 +0200

    Comments

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day21/DiracGame.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day21/DiracGame.kt
@@ -1,5 +1,9 @@
 package nl.mvdr.adventofcode.adventofcode2021.day21
 
+// There are many different universes to examine, but many are equivalent.
+// For example, rolling 1, 2, 3 results in the same game state as rolling 3, 2, 1.
+// For another, if player 1 rolls 1, 1, 1 then 3, 3, 3, this results in the same game state as rolling 3, 3, 3 then 1, 1, 1.
+// Because of this, caching the outcome for each game speeds up computation significantly.
 private val cache = mutableMapOf<DiracGame, Map<String, Long>>()
 
 /**
@@ -26,12 +30,17 @@ data class DiracGame(val players: Pair<Player, Player>) {
         }
     }
 
+    /**
+     * Returns the game state in each possible universe after playing one turn.
+     */
     private fun turn() = (1 .. 3)
         .flatMap { firstRoll -> (1 .. 3)
             .flatMap { secondRoll -> (1 .. 3)
                 .map { thirdRoll -> firstRoll + secondRoll + thirdRoll } } }
         .map(this::move)
 
-
+    /**
+     * Moves the first player by the given number of spaces.
+     */
     private fun move(spaces: Int) = DiracGame(Pair(players.second, players.first.move(spaces)))
 }
