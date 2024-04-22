c4e66ae8f7af6cae0e3ceebc90586d4351a24565
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-22 17:05:24 +0200

    Refactoring

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day22/Cuboid.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day22/Cuboid.kt
@@ -3,18 +3,26 @@ package nl.mvdr.adventofcode.adventofcode2021.day22
 import nl.mvdr.adventofcode.point.Axis3D
 import nl.mvdr.adventofcode.point.Point3D
 
-data class Cuboid(val ranges: Map<Axis3D, IntRange>) {
-    val x get() = ranges[Axis3D.X] ?: IntRange.EMPTY
-    val y get() = ranges[Axis3D.Y] ?: IntRange.EMPTY
-    val z get() = ranges[Axis3D.Z] ?: IntRange.EMPTY
-    val cubes get() = x.flatMap { xValue -> y.flatMap { yValue -> z.map { zValue -> Point3D(xValue, yValue, zValue) } } }.toSet()
-}
+data class Cuboid(val x: IntRange, val y: IntRange, val z: IntRange) {
+    constructor(ranges: Map<Axis3D, IntRange>) : this(
+        ranges[Axis3D.X] ?: IntRange.EMPTY,
+        ranges[Axis3D.Y] ?: IntRange.EMPTY,
+        ranges[Axis3D.Z] ?: IntRange.EMPTY)
 
-/**
- * Parses the [text] representation of a cuboid.
- * For example: "x=11..13,y=11..13,z=11..13"
- */
-fun parseCuboid(text: String) = Cuboid(parseCoordinateRanges(text))
+    /**
+     * Parses the [text] representation of a cuboid.
+     * For example: "x=11..13,y=11..13,z=11..13"
+     */
+    constructor(text: String) : this(parseCoordinateRanges(text))
+
+    val cubes get() = x.flatMap { xValue -> y.flatMap { yValue -> z.map { zValue -> Point3D(xValue, yValue, zValue) } } }.toSet()
+
+    fun getRange(axis: Axis3D) = when(axis) {
+        Axis3D.X -> x
+        Axis3D.Y -> y
+        Axis3D.Z -> z
+    }
+}
 
 private fun parseCoordinateRanges(text: String) = text.split(",").associate(::parseCoordinateRange)
 
