c84189c50aff7eb9ccb122af5241a11119e02b19
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-22 16:59:39 +0200

    Added properties

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day22/Cuboid.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day22/Cuboid.kt
@@ -1,8 +1,14 @@
 package nl.mvdr.adventofcode.adventofcode2021.day22
 
 import nl.mvdr.adventofcode.point.Axis3D
+import nl.mvdr.adventofcode.point.Point3D
 
-data class Cuboid(val ranges: Map<Axis3D, IntRange>)
+data class Cuboid(val ranges: Map<Axis3D, IntRange>) {
+    val x get() = ranges[Axis3D.X] ?: IntRange.EMPTY
+    val y get() = ranges[Axis3D.Y] ?: IntRange.EMPTY
+    val z get() = ranges[Axis3D.Z] ?: IntRange.EMPTY
+    val cubes get() = x.flatMap { xValue -> y.flatMap { yValue -> z.map { zValue -> Point3D(xValue, yValue, zValue) } } }.toSet()
+}
 
 /**
  * Parses the [text] representation of a cuboid.
