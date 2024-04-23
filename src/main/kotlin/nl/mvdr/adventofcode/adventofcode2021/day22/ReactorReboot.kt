210c237349e943de140d8b5c95116af297ff424b
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-23 13:45:00 +0200

    Cleanup

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day22/ReactorReboot.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day22/ReactorReboot.kt
@@ -1,15 +1,17 @@
 package nl.mvdr.adventofcode.adventofcode2021.day22
 
+/**
+ * Solves the puzzle.
+ * [lines]: input file, containing a textual representation of the reboot steps.
+ * [limitToInitializationProcedureArea]: whether to limit the procedure
+ * to the initialization procedure area: x=-50..50,y=-50..50,z=-50..50.
+ */
 fun solve(lines: Sequence<String>, limitToInitializationProcedureArea: Boolean): Long {
     val rebootSteps = lines.map { parseRebootStep(it, limitToInitializationProcedureArea) }.toList()
 
-    // Note that the cuboids in the following steps must not overlap:
-    // there must not be two or more cuboids containing the same point.
     var turnedOn = emptySet<Cuboid>()
-
     for (rebootStep in rebootSteps) {
         turnedOn = rebootStep.perform(turnedOn)
     }
-
     return turnedOn.sumOf(Cuboid::countCubes)
 }
