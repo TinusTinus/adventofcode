474faa5bffb2673217576c828254f13a92022d9a
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-23 13:46:34 +0200

    Formatting

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day22/ReactorReboot.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day22/ReactorReboot.kt
@@ -2,8 +2,8 @@ package nl.mvdr.adventofcode.adventofcode2021.day22
 
 /**
  * Solves the puzzle.
- * [lines]: input file, containing a textual representation of the reboot steps.
- * [limitToInitializationProcedureArea]: whether to limit the procedure
+ * * [lines]: input file, containing a textual representation of the reboot steps.
+ * * [limitToInitializationProcedureArea]: whether to limit the procedure
  * to the initialization procedure area: x=-50..50,y=-50..50,z=-50..50.
  */
 fun solve(lines: Sequence<String>, limitToInitializationProcedureArea: Boolean): Long {
