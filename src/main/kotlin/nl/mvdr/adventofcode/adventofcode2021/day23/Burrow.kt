05e8a599f52059195fd67e92ac95748b78db5846
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-26 11:35:09 +0200

    Refactoring

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day23/Burrow.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day23/Burrow.kt
@@ -2,8 +2,32 @@ package nl.mvdr.adventofcode.adventofcode2021.day23
 
 import nl.mvdr.adventofcode.point.Point
 
+/**
+ * Representation of a burrow, of the following form:
+ * ```
+ * #############
+ * #...........#
+ * ###.#.#.#.###
+ *   #.#.#.#.#
+ *   #########
+ * ```
+ */
 object Burrow {
-    private val hallwaySpaces = setOf(
+
+    private val sideRooms = setOf(
+        RoomSpace(3, 2, AmphipodType.AMBER),
+        RoomSpace(3, 3, AmphipodType.AMBER),
+        RoomSpace(5, 2, AmphipodType.BRONZE),
+        RoomSpace(5, 3, AmphipodType.BRONZE),
+        RoomSpace(7, 2, AmphipodType.COPPER),
+        RoomSpace(7, 3, AmphipodType.COPPER),
+        RoomSpace(9, 2, AmphipodType.DESERT),
+        RoomSpace(9, 3, AmphipodType.DESERT)
+    )
+
+    // Note that the hallway spaces in this collection only include the ones where an amphipod may stop.
+    // That is, the spaces right in front of a side room are not included.
+    private val hallway = setOf(
         HallwaySpace(1),
         HallwaySpace(2),
         HallwaySpace(4),
@@ -13,18 +37,7 @@ object Burrow {
         HallwaySpace(11)
     )
 
-    private val roomSpaces = setOf(
-        RoomSpace(3, 2, AmphipodType.AMBER),
-        RoomSpace(3, 3, AmphipodType.AMBER),
-        RoomSpace(5, 2, AmphipodType.BRONZE),
-        RoomSpace(5, 3, AmphipodType.BRONZE),
-        RoomSpace(7, 2, AmphipodType.COPPER),
-        RoomSpace(7, 3, AmphipodType.COPPER),
-        RoomSpace(9, 2, AmphipodType.DESERT),
-        RoomSpace(9, 3, AmphipodType.DESERT),
-    )
-
-    private val spaces get() = hallwaySpaces + roomSpaces
+    private val spaces get() = hallway + sideRooms
 
     fun getSpace(location: Point) = spaces.first { it.location == location }
 }
