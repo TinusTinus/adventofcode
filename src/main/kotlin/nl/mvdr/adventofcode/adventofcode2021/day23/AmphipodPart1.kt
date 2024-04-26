6a116e0760d5d1bb91d8010fcb8be1952123bf22
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-26 17:13:17 +0200

    Refactoring

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day23/AmphipodPart1.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day23/AmphipodPart1.kt
@@ -2,39 +2,10 @@ package nl.mvdr.adventofcode.adventofcode2021.day23
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import nl.mvdr.adventofcode.FunctionSolver
-import org.jgrapht.alg.interfaces.ShortestPathAlgorithm
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath
-import org.jgrapht.graph.DefaultEdge
-import org.jgrapht.graph.SimpleDirectedWeightedGraph
 
 private val logger = KotlinLogging.logger{}
 
-fun solvePart1(lines: Sequence<String>): Int {
-    val startState = State(lines)
-    val endState = State(Burrow.sideRooms.map { Amphipod(it.type, it.location) }.toSet())
-
-    val graph = SimpleDirectedWeightedGraph<State, DefaultEdge>(DefaultEdge::class.java)
-    graph.addVertex(startState)
-
-    val latestStates = mutableSetOf(startState)
-
-    while (latestStates.isNotEmpty()) {
-        val state = latestStates.first()
-        latestStates.remove(state)
-        for (nextState in state.nextStates()) {
-            graph.addVertex(nextState.first)
-            if (!graph.containsEdge(state, nextState.first)) {
-                val edge = graph.addEdge(state, nextState.first)
-                graph.setEdgeWeight(edge, nextState.second.toDouble())
-            }
-            latestStates.add(nextState.first)
-        }
-    }
-
-    val algorithm: ShortestPathAlgorithm<State, DefaultEdge> = DijkstraShortestPath(graph)
-    val path = algorithm.getPath(startState, endState)
-    return path.weight.toInt()
-}
+fun solvePart1(lines: Sequence<String>) = State(lines).computeEnergyCost()
 
 fun main() {
     val result = FunctionSolver(::solvePart1).solve("input-day23-2021.txt")
--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day23/State.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day23/State.kt
@@ -1,15 +1,57 @@
 package nl.mvdr.adventofcode.adventofcode2021.day23
 
 import nl.mvdr.adventofcode.point.Point
+import org.jgrapht.Graph
+import org.jgrapht.alg.interfaces.ShortestPathAlgorithm
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath
+import org.jgrapht.graph.DefaultEdge
+import org.jgrapht.graph.SimpleDirectedWeightedGraph
 
 data class State(val amphipods: Set<Amphipod>) {
     constructor(lines: Sequence<String>) : this(parseAmphipods(lines.toList()))
 
     /**
+     * Determines the minimum amount of energy needed to organize the amphipods from this (start) state.
+     */
+    fun computeEnergyCost(): Int {
+        val graph = createGraph()
+        val algorithm: ShortestPathAlgorithm<State, DefaultEdge> = DijkstraShortestPath(graph)
+        val endState = State(Burrow.sideRooms.map { Amphipod(it.type, it.location) }.toSet())
+        val path = algorithm.getPath(this, endState)
+        return path.weight.toInt()
+    }
+
+    /**
+     * Creates a directed, weighted graph based on this state.
+     * Vertices in the resulting graph are states which can be reached from this one by moving the amphipods.
+     * Edges are single movements, with the corresponding edge weight being the movement's energy cost.
+     */
+    private fun createGraph(): Graph<State, DefaultEdge> {
+        val graph = SimpleDirectedWeightedGraph<State, DefaultEdge>(DefaultEdge::class.java)
+        graph.addVertex(this)
+
+        val latestStates = mutableSetOf(this)
+
+        while (latestStates.isNotEmpty()) {
+            val state = latestStates.first()
+            latestStates.remove(state)
+            for (nextState in state.nextStates()) {
+                graph.addVertex(nextState.first)
+                if (!graph.containsEdge(state, nextState.first)) {
+                    val edge = graph.addEdge(state, nextState.first)
+                    graph.setEdgeWeight(edge, nextState.second.toDouble())
+                }
+                latestStates.add(nextState.first)
+            }
+        }
+        return graph
+    }
+
+    /**
      * Determines possible next states, after moving a single amphipod.
      * Returns pairs of the next state and the corresponding energy cost.
      */
-    fun nextStates(): Set<Pair<State, Int>> = setOf()
+    private fun nextStates(): Set<Pair<State, Int>> = setOf() // TODO implement!
 }
 
 private fun parseAmphipods(lines: List<String>) = lines.indices.flatMap { y -> lines[y].indices.map { x -> Point(x, y) } }
