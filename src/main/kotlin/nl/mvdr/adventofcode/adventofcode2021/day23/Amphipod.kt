4988e9d3665fc4e871d098153096ecbbc17ce357
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-26 11:30:20 +0200

    Removed validation

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day23/Amphipod.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day23/Amphipod.kt
@@ -3,13 +3,6 @@ package nl.mvdr.adventofcode.adventofcode2021.day23
 import nl.mvdr.adventofcode.point.Point
 
 data class Amphipod(val type: AmphipodType, val location: Point) {
-    init {
-        try {
-            Burrow.getSpace(location)
-        } catch (e: NoSuchElementException) {
-            throw IllegalArgumentException("Not a valid location for an amphipod: $location", e)
-        }
-    }
 
     fun isInHallway() = location.y == 1
 
