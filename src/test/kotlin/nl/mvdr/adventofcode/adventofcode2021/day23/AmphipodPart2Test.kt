2fbdbd65aa7e640ef378874abe68e199eefaef61
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-28 17:43:55 +0200

    Disabled long-running test cases

--- src/test/kotlin/nl/mvdr/adventofcode/adventofcode2021/day23/AmphipodPart2Test.kt
+++ src/test/kotlin/nl/mvdr/adventofcode/adventofcode2021/day23/AmphipodPart2Test.kt
@@ -1,8 +1,10 @@
 package nl.mvdr.adventofcode.adventofcode2021.day23
 
 import nl.mvdr.adventofcode.FunctionSolverTest
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.params.provider.Arguments
 
+@Disabled // These test cases take about one minute each to run.
 class AmphipodPart2Test: FunctionSolverTest<Any>(::solvePart2) {
     companion object {
         @JvmStatic
