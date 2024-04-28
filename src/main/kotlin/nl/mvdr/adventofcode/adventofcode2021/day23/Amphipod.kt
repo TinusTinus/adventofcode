b58a3acde27b2e64413a4c370810f8ce56dcc8d5
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-28 16:59:58 +0200

    Improved exception handling

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day23/Amphipod.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day23/Amphipod.kt
@@ -4,5 +4,11 @@ import nl.mvdr.adventofcode.point.Point
 
 data class Amphipod(val type: AmphipodType, val location: Point) {
 
-    fun isAtDestination(burrow: Burrow) = burrow.isInSideRoom(location) && (burrow.getSpace(location) as RoomSpace).type == type
+    fun isAtDestination(burrow: Burrow): Boolean {
+        try {
+            return burrow.isInSideRoom(location) && (burrow.getSpace(location) as RoomSpace).type == type
+        } catch (e: NoSuchElementException) {
+            throw IllegalStateException("Amphipod $this is at $location, but there is no room there in the burrow.", e)
+        }
+    }
 }
