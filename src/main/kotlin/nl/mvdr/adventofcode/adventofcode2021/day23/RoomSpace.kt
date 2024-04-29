3876a4cdd7accbdd02d3a5f3d6aa21fe544b670a
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-04-29 11:32:41 +0200

    Use default parameter value instead of constructor overloading

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day23/RoomSpace.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day23/RoomSpace.kt
@@ -6,9 +6,7 @@ import nl.mvdr.adventofcode.point.Point
  * A space in one of the side rooms.
  * The [type] indicates which type of amphipod has this side room as its destination.
  */
-data class RoomSpace(override val location: Point, val type: AmphipodType): Space {
-
-    constructor(location: Point) : this(location, AmphipodType.entries.first { it.x == location.x })
+data class RoomSpace(override val location: Point, val type: AmphipodType = AmphipodType.entries.first { it.x == location.x }): Space {
 
     constructor(x: Int, y: Int, type: AmphipodType) : this(Point(x, y), type)
 }
