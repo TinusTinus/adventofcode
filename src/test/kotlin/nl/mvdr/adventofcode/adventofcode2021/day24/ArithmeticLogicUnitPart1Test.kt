df71c36111cea290e687511906ed20cff4d705ba
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-05-03 18:13:26 +0200

    Solved 2021 day 24 part 1

--- src/test/kotlin/nl/mvdr/adventofcode/adventofcode2021/day24/ArithmeticLogicUnitPart1Test.kt
+++ src/test/kotlin/nl/mvdr/adventofcode/adventofcode2021/day24/ArithmeticLogicUnitPart1Test.kt
@@ -1,13 +1,15 @@
 package nl.mvdr.adventofcode.adventofcode2021.day24
 
 import nl.mvdr.adventofcode.FunctionSolverTest
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.params.provider.Arguments
 
+@Disabled // Test case takes a pretty long time to run
 class ArithmeticLogicUnitPart1Test: FunctionSolverTest<Any>(::solvePart1) {
     companion object {
         @JvmStatic
         fun testSolution() = listOf(
-            Arguments.of("?", "input-day24-2021.txt")
+            Arguments.of("12996997829399", "input-day24-2021.txt")
         )
     }
 }
