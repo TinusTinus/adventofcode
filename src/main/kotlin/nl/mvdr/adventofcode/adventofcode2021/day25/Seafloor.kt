45d41f3d1b208f83641376e748b52fe3295e8929
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-05-04 19:31:11 +0200

    First attempt at a solution

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day25/Seafloor.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2021/day25/Seafloor.kt
@@ -1,19 +1,43 @@
 package nl.mvdr.adventofcode.adventofcode2021.day25
 
-import nl.mvdr.adventofcode.point.CharacterMapper
 import nl.mvdr.adventofcode.point.Direction
 import nl.mvdr.adventofcode.point.Point
 
 data class Seafloor(val width: Int, val height: Int, val eastMovingHerd: Set<Point>, val southMovingHerd: Set<Point>) {
+    /**
+     * The number of steps until the sea cucumbers on the floor stop moving.
+     */
     val stepsToStopMoving: Int get() = when (val nextState = next) {
         this -> 0
         else -> 1 + nextState.stepsToStopMoving
     }
 
+    /**
+     * The next state, after taking a single step.
+     */
     private val next: Seafloor get() {
-        return this // TODO
+        val newEastMovingHerd = eastMovingHerd.map(this::moveEast).toSet()
+        val newSouthMovingHerd = southMovingHerd.map(this::moveSouth).toSet()
+        return copy(eastMovingHerd = newEastMovingHerd, southMovingHerd = newSouthMovingHerd)
     }
 
+
+    private fun moveEast(point: Point) = moveIfPossible(point, Point((point.x + 1) % width, point.y))
+
+    private fun moveSouth(point: Point) = moveIfPossible(point, Point(point.x, (point.y + 1) % height))
+
+    /**
+     * Checks whether it is possible for a sea cucumber to move from its [currentLocation] to the [newLocation].
+     * Returns the new location if the move is possible, and the current location if it is not.
+     */
+    private fun moveIfPossible(currentLocation: Point, newLocation: Point) = when {
+        isOccupied(newLocation) -> currentLocation
+        else -> newLocation
+    }
+
+    /**
+     * Checks whether the given [point] is occupied by a sea cucumber.
+     */
     private fun isOccupied(point: Point) = eastMovingHerd.contains(point) || southMovingHerd.contains(point)
 }
 
