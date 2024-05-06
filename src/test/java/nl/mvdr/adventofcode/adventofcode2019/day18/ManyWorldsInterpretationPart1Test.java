9dcd2a4c0f5f05f42626cf57b7ac724bc2cb02be
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-05-06 12:51:42 +0200

    Fixed test cases

--- src/test/java/nl/mvdr/adventofcode/adventofcode2019/day18/ManyWorldsInterpretationPart1Test.java
+++ src/test/java/nl/mvdr/adventofcode/adventofcode2019/day18/ManyWorldsInterpretationPart1Test.java
@@ -3,6 +3,9 @@ package nl.mvdr.adventofcode.adventofcode2019.day18;
 import org.junit.jupiter.api.Test;
 
 import nl.mvdr.adventofcode.SolverTest;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
 
 /**
  * Unit test cases for {@link ManyWorldsInterpretationPart1}.
@@ -15,52 +18,17 @@ public class ManyWorldsInterpretationPart1Test extends SolverTest<ManyWorldsInte
     public ManyWorldsInterpretationPart1Test() {
         super(ManyWorldsInterpretationPart1.class);
     }
-    
-    /** Test case based on an example from the puzzle text. */
-    @Test
-    public void testExample0() {
-        assertSolution("8", "example-day18-2019-0.txt");
-    }
-    
-    /** Test case based on an example from the puzzle text. */
-    @Test
-    public void testExample1() {
-        assertSolution("6", "example-day18-2019-1.txt");
-    }
-    
-    /** Test case based on an example from the puzzle text. */
-    @Test
-    public void testExample2() {
-        assertSolution("0", "example-day18-2019-2.txt");
-    }
 
-    /** Test case based on an example from the puzzle text. */
-    @Test
-    public void testExample3() {
-        assertSolution("86", "example-day18-2019-3.txt");
-    }
-
-    /** Test case based on an example from the puzzle text. */
-    @Test
-    public void testExample4() {
-        assertSolution("132", "example-day18-2019-4.txt");
-    }
-    
-    /** Test case based on an example from the puzzle text. */
-    @Test
-    public void testExample5() {
-        assertSolution("136", "example-day18-2019-5.txt");
-    }
-    
-    /** Test case based on an example from the puzzle text. */
-    @Test
-    public void testExample6() {
-        assertSolution("81", "example-day18-2019-6.txt");
-    }
-
-    /** Test case based on the accepted solution. */
-    @Test
-    public void testSolution() {
-        assertSolution("?", "input-day18-2019.txt"); 
+    /** @return arguments for {@link SolverTest#testSolution(String, String)} */
+    static Stream<Arguments> testSolution() {
+        return Stream.of(
+                Arguments.of("8", "example-day18-2019-0.txt"),
+                Arguments.of("6", "example-day18-2019-1.txt"),
+                Arguments.of("0", "example-day18-2019-2.txt"),
+                Arguments.of("86", "example-day18-2019-3.txt"),
+                Arguments.of("132", "example-day18-2019-4.txt"),
+                Arguments.of("136", "example-day18-2019-5.txt"),
+                Arguments.of("81", "example-day18-2019-6.txt"),
+                Arguments.of("?", "input-day18-2019.txt"));
     }
 }
