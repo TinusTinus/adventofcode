300c6a6dcadd66750d0720e22e8fcb0270b9fb55
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-05-07 00:13:19 +0200

    Started part 2

---
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2019/day18/ManyWorldsInterpretationPart2.kt
@@ -0,0 +1,26 @@
+package nl.mvdr.adventofcode.adventofcode2019.day18
+
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import nl.mvdr.adventofcode.FunctionSolver
+import nl.mvdr.adventofcode.point.Point
+
+private val logger = KotlinLogging.logger{}
+
+fun solvePart2(linesSequence: Sequence<String>): Int {
+    val lines = linesSequence.toMutableList()
+
+    val point = (0 until lines.size).flatMap { y -> (0 until lines[y].length).map { x -> Point(x, y) } }
+        .first { lines[it.x][it.y] == '@' }
+
+    lines[point.y - 1] = lines[point.y - 1].replaceRange(point.x - 1 .. point.y + 1, "@#@")
+    lines[point.y] = lines[point.y].replaceRange(point.x - 1 .. point.y + 1, "###")
+    lines[point.y + 1] = lines[point.y + 1].replaceRange(point.x - 1 .. point.y + 1, "@#@")
+
+    return parseInput(lines).solve()
+}
+
+fun main() {
+    val result = FunctionSolver(::solvePart2).solve("input-day18-2019.txt")
+    logger.info { result }
+}
