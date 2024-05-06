bfdc37cce90a83ea8f653e85ba5d17377dfdce62
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-05-06 13:30:15 +0200

    Converted the main class to a Kotlin function

---
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2019/day18/ManyWorldsInterpretationPart1.kt
@@ -0,0 +1,15 @@
+package nl.mvdr.adventofcode.adventofcode2019.day18;
+
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import nl.mvdr.adventofcode.FunctionSolver
+import kotlin.streams.asStream
+
+private val logger = KotlinLogging.logger{}
+
+fun solvePart1(lines: Sequence<String>) = Vault.parse(lines.asStream()).shortestPathToPickUpAllKeys()
+
+fun main() {
+    val result = FunctionSolver(::solvePart1).solve("input-day18-2019.txt")
+    logger.info { result }
+}
