5fbf805de04cfd01856af14768898cbdcf6e70f1
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-05-06 20:23:51 +0200

    Added a graph

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2019/day18/Vault.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2019/day18/Vault.kt
@@ -1,5 +1,21 @@
 package nl.mvdr.adventofcode.adventofcode2019.day18
 
 import nl.mvdr.adventofcode.point.Point
+import org.jgrapht.Graph
+import org.jgrapht.graph.DefaultEdge
+import org.jgrapht.graph.SimpleGraph
 
-data class Vault(private val openPassages: Set<Point>, private val doors: Map<Point, Door>, val keys: Map<Key, Point>)
+data class Vault(private val openPassages: Set<Point>, private val doors: Map<Point, Door>, val keys: Map<Key, Point>) {
+    /**
+     * A graph representing this vault, assuming that all doors are closed.
+     */
+    private val graph: Graph<Point, DefaultEdge> = SimpleGraph(DefaultEdge::class.java)
+
+    init {
+        openPassages.forEach(graph::addVertex)
+        openPassages.forEach { passage ->
+            passage.neighbours()
+                .filter(openPassages::contains)
+                .forEach { neighbour -> graph.addEdge(passage, neighbour) } }
+    }
+}
