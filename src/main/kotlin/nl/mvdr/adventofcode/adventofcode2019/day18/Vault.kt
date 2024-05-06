05bfeed5f5e71edec08aa701ba98c142d4e29f8f
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-05-06 23:19:42 +0200

    Avoid repeatedly reconstructing the same graph

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2019/day18/Vault.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2019/day18/Vault.kt
@@ -1,25 +1,44 @@
 package nl.mvdr.adventofcode.adventofcode2019.day18
 
 import nl.mvdr.adventofcode.point.Point
+import org.jgrapht.Graph
 import org.jgrapht.alg.interfaces.ShortestPathAlgorithm
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath
 import org.jgrapht.graph.DefaultEdge
 import org.jgrapht.graph.SimpleGraph
 
 data class Vault(private val openPassages: Set<Point>, private val doors: Map<Point, Door>, val keys: Map<Key, Point>) {
-    /**
-     * Returns a shortest path algorithm for traversing this vault, if the given [ownedKeys] are in the traveler's possession.
-     */
-    fun createShortestPathAlgorithm(ownedKeys: Set<Key>): ShortestPathAlgorithm<Point, DefaultEdge> {
-        val openDoors = doors.filter { entry -> ownedKeys.any { key -> key.opens(entry.value) } }.keys
-        val accessiblePassages = openPassages + openDoors + keys.values
 
-        val graph = SimpleGraph<Point, DefaultEdge>(DefaultEdge::class.java)
+    private val accessiblePassages = openPassages + keys.values
+
+    private val graph: Graph<Point, DefaultEdge> = SimpleGraph(DefaultEdge::class.java)
+
+    init {
         accessiblePassages.forEach(graph::addVertex)
         accessiblePassages.forEach { passage ->
             passage.neighbours()
                 .filter(accessiblePassages::contains)
                 .forEach { neighbour -> graph.addEdge(passage, neighbour) } }
+
+        doors.keys.forEach(graph::addVertex)
+    }
+
+    /**
+     * Returns a shortest path algorithm for traversing this vault, if the given [ownedKeys] are in the traveler's possession.
+     * Note: the returned algorithm should no longer be used after calling this method again,
+     * as this method modifies the backing graph.
+     */
+    // This should probably be refactored to be less fragile. (See the note in the KDOc above.)
+    fun createShortestPathAlgorithm(ownedKeys: Set<Key>): ShortestPathAlgorithm<Point, DefaultEdge> {
+        for ((doorLocation, door) in doors) {
+            for (neighbour in doorLocation.neighbours()) {
+                graph.removeEdge(doorLocation, neighbour)
+                if (ownedKeys.any { it.opens(door) } &&
+                        (accessiblePassages.contains(neighbour) || doors.keys.contains(neighbour))) {
+                    graph.addEdge(doorLocation, neighbour)
+                }
+            }
+        }
         return DijkstraShortestPath(graph)
     }
 }
