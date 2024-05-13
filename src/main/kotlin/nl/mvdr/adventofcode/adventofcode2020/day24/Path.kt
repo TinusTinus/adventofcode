2766986db57a62bb50bff6ff2e4d093daf53a584
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-05-13 15:16:26 +0200

    Refactoring

--- src/main/kotlin/nl/mvdr/adventofcode/adventofcode2020/day24/Path.kt
+++ src/main/kotlin/nl/mvdr/adventofcode/adventofcode2020/day24/Path.kt
@@ -3,19 +3,18 @@ package nl.mvdr.adventofcode.adventofcode2020.day24
 import nl.mvdr.adventofcode.point.Point
 
 data class Path(private val steps: List<Direction>) {
+    /**
+     * Constructs a path based on its [text] representation.
+     * For example: "nwwswee".
+     */
     constructor(text: String) : this(parseSteps(text))
 
-    fun move(startingPoint: Point = Point.ORIGIN): Point {
-        var result = startingPoint
-        for (step in steps) {
-            result = step.move(result)
-        }
-        return result
-    }
+    fun move(startingPoint: Point = Point.ORIGIN) = steps.fold(startingPoint) { point, step -> step.move(point)}
 }
 
 /**
  * Parses the [text] representation of a path as a list of steps.
+ * For example: "nwwswee".
  */
 private fun parseSteps(text: String): List<Direction> = when (text) {
     "" -> emptyList()
