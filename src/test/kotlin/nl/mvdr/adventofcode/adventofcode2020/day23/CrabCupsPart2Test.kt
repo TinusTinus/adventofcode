caf81fea956e740561e2db5fb713a8300b7665cf
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-12-01 11:23:15 +0100

    Ported test cases to Kotlin
    - removes all dependencies from Java code on Kotlin code
    - this is convenient when working on the Java code in an IDE that doesn't support Kotlin

---
+++ src/test/kotlin/nl/mvdr/adventofcode/adventofcode2020/day23/CrabCupsPart2Test.kt
@@ -0,0 +1,14 @@
+package nl.mvdr.adventofcode.adventofcode2020.day23
+
+import nl.mvdr.adventofcode.FunctionSolverTest
+import org.junit.jupiter.params.provider.Arguments
+
+class CrabCupsPart2Test: FunctionSolverTest<Any>(::solvePart2) {
+    companion object {
+        @JvmStatic
+        fun testSolution() = listOf(
+            Arguments.of("149245887792", "example-day23-2020.txt"),
+            Arguments.of("186715244496", "input-day23-2020.txt")
+        )
+    }
+}
