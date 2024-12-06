b564feed9bc198068be1d86320cdc88dab9c2fa8
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-12-06 20:56:09 +0100

    Another attempt

--- src/main/java/nl/mvdr/adventofcode/adventofcode2024/day05/Part2.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2024/day05/Part2.java
@@ -1,10 +1,5 @@
 package nl.mvdr.adventofcode.adventofcode2024.day05;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -30,35 +25,13 @@ public class Part2 implements LongSolver {
     			.map(Rule::parse)
     			.collect(Collectors.toSet());
     	
-    	Comparator<Integer> pageComparator = createPageComparator(rules);
-    	
         return lines.subList(indexOfEmptyLine + 1, lines.size())
                 .stream()
                 .map(Update::parse)
                 .filter(update -> !update.satisfies(rules))
-                .map(update -> update.sort(pageComparator))
+                .map(update -> update.sort(rules))
                 .mapToInt(Update::middlePage)
-                .sum();// TODO
-    }
-    
-    private static Comparator<Integer> createPageComparator(Set<Rule> rules) {
-        // Find all page numbers in the rules
-        Set<Integer> remainingPages = rules.stream()
-                .flatMap(rule -> Stream.of(Integer.valueOf(rule.lhs()), Integer.valueOf(rule.rhs())))
-                .collect(Collectors.toCollection(HashSet::new));
-
-        // Sort these according to the rules
-        List<Integer> sortedPages = new ArrayList<>();
-        while (!remainingPages.isEmpty()) {
-            Integer nextPage = remainingPages.stream()
-                    .filter(page -> rules.stream().noneMatch(rule -> rule.rhs() == page.intValue() && !sortedPages.contains(Integer.valueOf(rule.lhs()))))
-                    .findFirst()
-                    .orElseThrow(() -> new IllegalStateException("Unable to find next page. Sorted: " + sortedPages + ", remaining: " + remainingPages));
-            remainingPages.remove(nextPage);
-            sortedPages.add(nextPage);
-        }
-        
-        return (lhs, rhs) -> sortedPages.indexOf(rhs) - sortedPages.indexOf(lhs);
+                .sum();
     }
 
     public static void main(String[] args) {
--- src/main/java/nl/mvdr/adventofcode/adventofcode2024/day05/Update.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2024/day05/Update.java
@@ -1,6 +1,7 @@
 package nl.mvdr.adventofcode.adventofcode2024.day05;
 
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
@@ -32,8 +33,22 @@ record Update(List<Integer> pages) {
 	    return pages.get(pages.size() / 2).intValue();
 	}
 	
-	Update sort(Comparator<Integer> pageComparator) {
-	    List<Integer> sortedPages = pages.stream().sorted(pageComparator).toList();
+	Update sort(Set<Rule> rules) {
+        Set<Integer> remainingPages = new HashSet<>(pages);
+
+        // Sort these according to the rules
+        List<Integer> sortedPages = new ArrayList<>();
+        while (!remainingPages.isEmpty()) {
+            Integer nextPage = remainingPages.stream()
+                    .filter(page -> rules.stream().noneMatch(rule -> 
+                        rule.rhs() == page.intValue() 
+                            && !sortedPages.contains(Integer.valueOf(rule.lhs()))))
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalStateException("Unable to find next page. Sorted: " + sortedPages + ", remaining: " + remainingPages));
+            remainingPages.remove(nextPage);
+            sortedPages.add(nextPage);
+        }
+	    
 	    return new Update(sortedPages);
 	}
 }
