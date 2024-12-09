ff9efdec94ce907c7e05dde0734002de180d58f4
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-12-09 18:31:39 +0100

    First attempt at part 1

--- src/main/java/nl/mvdr/adventofcode/adventofcode2024/day09/Disk.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2024/day09/Disk.java
@@ -2,12 +2,12 @@ package nl.mvdr.adventofcode.adventofcode2024.day09;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
-import kotlin.ranges.IntRange;
-
 record Disk(List<DiskElement> elements) {
+    
     static Disk parse(String diskMap) {
         List<DiskElement> elements = new ArrayList<>();
         elements.add(new File(0, Character.getNumericValue(diskMap.charAt(0))));
@@ -28,13 +28,53 @@ record Disk(List<DiskElement> elements) {
     }
     
     Disk compact() {
-        return this; // TODO implement
+        var result = this.filterEmptyElements();
+        while (result.findFirstEmptySpace().isPresent()) {
+            var firstEmptySpace = result.findFirstEmptySpace().orElseThrow();
+            var lastFile = result.findLastFile();
+            var blocksToMove = Math.min(firstEmptySpace.blocks(), lastFile.blocks());
+            
+            System.out.println("First empty space: " + firstEmptySpace); // TODO remove
+            System.out.println("Last file: " + lastFile); // TODO remove
+            System.out.println("Blocks to move: " + blocksToMove); // TODO remove
+            
+            List<DiskElement> newElements = new ArrayList<>();
+            var elementIndex = 0;
+            while (result.elements().get(elementIndex) != firstEmptySpace) {
+                newElements.add(result.elements().get(elementIndex));
+                elementIndex++;
+            }
+            newElements.add(new File(lastFile.idNumber(), blocksToMove));
+            newElements.add(new EmptySpace(firstEmptySpace.blocks() - blocksToMove));
+            elementIndex++;
+            while (result.elements().get(elementIndex) != lastFile) {
+                newElements.add(result.elements().get(elementIndex));
+                elementIndex++;
+            }
+            newElements.add(new File(lastFile.idNumber(), lastFile.blocks() - blocksToMove));
+            result = new Disk(newElements);
+            
+            result = result.filterEmptyElements();
+            System.out.println(result); // TODO remove
+        }
+        
+        return result;
     }
     
-    private int blocks() {
-        return elements.stream()
-                .mapToInt(DiskElement::blocks)
-                .sum();
+    private Optional<EmptySpace> findFirstEmptySpace() {
+        return elements().stream()
+                .filter(EmptySpace.class::isInstance)
+                .map(EmptySpace.class::cast)
+                .findFirst();
+    }
+    
+    private File findLastFile() {
+        return elements.reversed()
+                .stream()
+                .filter(File.class::isInstance)
+                .map(File.class::cast)
+                .findFirst()
+                .orElseThrow();
     }
     
     int checksum() {
