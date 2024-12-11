0452a4c49472b506fd61fd8304248b018a12b59e
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-12-11 11:34:29 +0100

    First attempt at part 2

--- src/main/java/nl/mvdr/adventofcode/adventofcode2024/day11/Part1.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2024/day11/Part1.java
@@ -1,5 +1,8 @@
 package nl.mvdr.adventofcode.adventofcode2024.day11;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
@@ -11,6 +14,8 @@ public class Part1 implements LongSolver {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Part1.class);
 
+    private static Map<StoneNumberAndBlinkCount, Long> cache = new HashMap<>();
+    
     private final int numberOfBlinks;
     
     Part1(int numberOfBlinks) {
@@ -24,10 +29,40 @@ public class Part1 implements LongSolver {
     @Override
     public long solve(Stream<String> lines) {
         var line = lines.findFirst().orElseThrow();
-        var arrangement = StoneArrangement.parse(line);
-        return arrangement.blink(numberOfBlinks).stones().size();
+        return Stream.of(line.split(" "))
+                .mapToLong(Long::parseLong)
+                .map(stoneNumber -> countStones(stoneNumber, numberOfBlinks))
+                .sum();
     }
-
+    
+    private static long countStones(long stoneNumber, int blinks) {
+        return switch (blinks) {
+            case 0 -> 1L;
+            default -> cache.computeIfAbsent(
+                    new StoneNumberAndBlinkCount(stoneNumber, blinks),
+                    key -> Long.valueOf(blinkStone(stoneNumber).map(s -> countStones(s, blinks - 1)).sum()))
+                .longValue();
+        };
+    }
+    
+    private static LongStream blinkStone(long stoneNumber) {
+        LongStream result;
+        if (stoneNumber == 0) {
+            result = LongStream.of(1L);
+        } else {
+            String stoneNumberString = String.valueOf(stoneNumber);
+            int digits = stoneNumberString.length();
+            if (digits % 2 == 0) {
+                long firstNumber = Long.parseLong(stoneNumberString.substring(0, digits / 2));
+                long secondNumber = Long.parseLong(stoneNumberString.substring(digits / 2, stoneNumberString.length()));
+                result = LongStream.of(firstNumber, secondNumber);
+            } else {
+                result = LongStream.of(Math.multiplyExact(2024L, stoneNumber));
+            }
+        }
+        return result;
+    }
+    
     public static void main(String[] args) {
         var instance = new Part1();
 
---
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2024/day11/Part2.java
@@ -0,0 +1,22 @@
+package nl.mvdr.adventofcode.adventofcode2024.day11;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Part2 extends Part1 {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Part2.class);
+
+    public Part2() {
+        super(75);
+    }
+
+    public static void main(String[] args) {
+        var instance = new Part2();
+
+        var result = instance.solve("input-day11-2024.txt");
+
+        LOGGER.info(result);
+    }
+}
+ 
