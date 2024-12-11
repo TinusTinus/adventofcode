bccefbe1428579d2157856d6ddb6b6826a1b1875
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-12-11 11:14:44 +0100

    Solved 2024 day 11 part 1

--- src/main/java/nl/mvdr/adventofcode/adventofcode2024/day11/StoneArrangement.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2024/day11/StoneArrangement.java
@@ -1,15 +1,15 @@
 package nl.mvdr.adventofcode.adventofcode2024.day11;
 
 import java.util.List;
-import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
-record StoneArrangement(List<Integer> stones) {
+record StoneArrangement(List<Long> stones) {
     
     static StoneArrangement parse(String line) {
         
-        List<Integer> stones = Stream.of(line.split(" "))
-                .map(Integer::valueOf)
+        List<Long> stones = Stream.of(line.split(" "))
+                .map(Long::valueOf)
                 .toList();
         return new StoneArrangement(stones);
     }
@@ -24,26 +24,26 @@ record StoneArrangement(List<Integer> stones) {
 
     
     private StoneArrangement blink() {
-        List<Integer> newStones = stones.stream()
+        List<Long> newStones = stones.stream()
                 .map(StoneArrangement::blinkStone)
-                .flatMap(IntStream::boxed)
+                .flatMap(LongStream::boxed)
                 .toList();
         return new StoneArrangement(newStones);
     }
     
-    private static IntStream blinkStone(int stoneNumber) {
-        IntStream result;
+    private static LongStream blinkStone(long stoneNumber) {
+        LongStream result;
         if (stoneNumber == 0) {
-            result = IntStream.of(1);
+            result = LongStream.of(1L);
         } else {
             String stoneNumberString = String.valueOf(stoneNumber);
             int digits = stoneNumberString.length();
             if (digits % 2 == 0) {
-                int firstNumber = Integer.parseInt(stoneNumberString.substring(0, digits / 2));
-                int secondNumber = Integer.parseInt(stoneNumberString.substring(digits / 2, stoneNumberString.length()));
-                result = IntStream.of(firstNumber, secondNumber);
+                long firstNumber = Long.parseLong(stoneNumberString.substring(0, digits / 2));
+                long secondNumber = Long.parseLong(stoneNumberString.substring(digits / 2, stoneNumberString.length()));
+                result = LongStream.of(firstNumber, secondNumber);
             } else {
-                result = IntStream.of(2024 * stoneNumber);
+                result = LongStream.of(Math.multiplyExact(2024L, stoneNumber));
             }
         }
         return result;
