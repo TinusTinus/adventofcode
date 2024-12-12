5a9665a55908cb70a82a5a9039d701b0ad7854f8
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-12-12 09:21:30 +0100

    Solved 2024 day 12 part 1

--- src/main/java/nl/mvdr/adventofcode/adventofcode2024/day12/Part1.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2024/day12/Part1.java
@@ -1,11 +1,13 @@
 package nl.mvdr.adventofcode.adventofcode2024.day12;
 
+import nl.mvdr.adventofcode.point.Point;
 import nl.mvdr.adventofcode.solver.IntSolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Part1 implements IntSolver {
@@ -16,7 +18,18 @@ public class Part1 implements IntSolver {
     public int solve(Stream<String> lines) {
     	Set<Region> regions = new HashSet<>();
     	
-    	// TODO fill set based on input
+    	Point.parse2DMap(lines.toList(), (plot, plant) -> {
+    		var neighbouringRegions = regions.stream()
+    			.filter(region -> region.plant() == plant)
+    			.filter(region -> plot.neighbours().stream().anyMatch(neighbour -> region.plots().contains(neighbour)))
+    			.collect(Collectors.toSet());
+    		
+    		neighbouringRegions.forEach(regions::remove);
+    		Set<Point> newRegionPlots = new HashSet<>();
+    		newRegionPlots.add(plot);
+    		neighbouringRegions.forEach(neighbouringRegion -> newRegionPlots.addAll(neighbouringRegion.plots()));
+    		regions.add(new Region(plant, newRegionPlots));
+    	});
     	
         return regions.stream()
         		.mapToInt(Region::cost)
