82a566bf48d3be3ec423b55497e6500383ab1346
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2024-12-24 19:21:50 +0100

    Naive solution for part 2

--- src/main/java/nl/mvdr/adventofcode/adventofcode2024/day23/Part2.java
+++ src/main/java/nl/mvdr/adventofcode/adventofcode2024/day23/Part2.java
@@ -1,6 +1,13 @@
 package nl.mvdr.adventofcode.adventofcode2024.day23;
 
+import java.util.Comparator;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
 import org.jgrapht.graph.DefaultEdge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,9 +16,32 @@ public class Part2 extends LanPartySolver<String> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Part2.class);
 
+    /// Finds the largest interconnected set of computers containing all of the computers in the given set.
+    ///
+    /// @param network the entire network
+    /// @param computers nonempty set of interconnected computers
+    /// @return the largest possible set of interconnected computers containing all of the given ones
+    private static Set<String> largestInterconnectedSet(Graph<String, DefaultEdge> network, Set<String> interconnectedComputers) {
+        return Graphs.neighborSetOf(network, interconnectedComputers.iterator().next())
+                .stream()
+                .filter(Predicate.not(interconnectedComputers::contains))
+                .filter(computer -> interconnectedComputers.stream().allMatch(interconnectedComputer -> network.containsEdge(computer, interconnectedComputer)))
+                .map(computer -> Stream.concat(Stream.of(computer), interconnectedComputers.stream()).collect(Collectors.toSet()))
+                .map(newInterconnectedComputers -> largestInterconnectedSet(network, newInterconnectedComputers))
+                .max(Comparator.comparing(Set::size))
+                .orElse(interconnectedComputers);
+    }
+    
     @Override
     public String solve(Graph<String, DefaultEdge> network) {
-        return "derp"; // TODO
+        return network.vertexSet()
+                .parallelStream()
+                .map(computer -> largestInterconnectedSet(network, Set.of(computer)))
+                .max(Comparator.comparing(Set::size))
+                .orElseThrow()
+                .stream()
+                .sorted()
+                .collect(Collectors.joining(","));
     }
     
     public static void main(String[] args) {
