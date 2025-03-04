b0b6983cd113b76dafc7202f7b4134ce3386f1ab
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2025-03-04 20:24:04 +0100

    First attempt at day 11

--- adventofcode-2016/src/main/java/nl/mvdr/adventofcode/adventofcode2016/day11/Part1.java
+++ adventofcode-2016/src/main/java/nl/mvdr/adventofcode/adventofcode2016/day11/Part1.java
@@ -2,6 +2,11 @@ package nl.mvdr.adventofcode.adventofcode2016.day11;
 
 import java.util.stream.Stream;
 
+import org.jgrapht.Graph;
+import org.jgrapht.alg.interfaces.ShortestPathAlgorithm;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,7 +20,18 @@ public class Part1 implements IntSolver {
     public int solve(Stream<String> lines) {
         var initialState = State.parse(lines);
         
-        return 0; // TODO
+        Graph<State, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
+        
+        initialState.allValidStates().forEach(graph::addVertex);
+        
+        graph.vertexSet()
+                .stream()
+                .forEach(state -> state.takeElevator().forEach(targetState -> graph.addEdge(state, targetState)));
+        
+        
+        ShortestPathAlgorithm<State, DefaultEdge> algorithm = new DijkstraShortestPath<>(graph);
+        var path = algorithm.getPath(initialState, initialState.endState());
+        return path.getLength();
     }
     
     public static void main(String[] args) {
--- adventofcode-2016/src/main/java/nl/mvdr/adventofcode/adventofcode2016/day11/State.java
+++ adventofcode-2016/src/main/java/nl/mvdr/adventofcode/adventofcode2016/day11/State.java
@@ -3,6 +3,9 @@ package nl.mvdr.adventofcode.adventofcode2016.day11;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 record State(Map<Item, Floor> itemLocations) {
@@ -20,7 +23,9 @@ record State(Map<Item, Floor> itemLocations) {
             var contents = parts[1].substring(0, parts[1].length() - 1);
             
             if (!"nothing relevant".equals(contents)) {
-                Stream.of(contents.split("/(, and|,| and /)"))
+                Stream.of(contents.split(", and"))
+                        .flatMap(string -> Stream.of(string.split(" and ")))
+                        .flatMap(string -> Stream.of(string.split(", ")))
                         .map(Item::parse)
                         .forEach(item -> itemLocations.put(item, floor));
             }
@@ -28,8 +33,31 @@ record State(Map<Item, Floor> itemLocations) {
         
         return new State(itemLocations);
     }
+
+    /// @return all possible valid states containing the items in this state
+    Stream<State> allValidStates() {
+        var emptyState = new State(Map.of());
+        var result = Stream.of(emptyState);
+        
+        for (var item : itemLocations.keySet()) {
+            result = result.flatMap(state -> state.add(item));
+        }
+        
+        return result.filter(State::isValid);
+    }
     
-    boolean isValid() {
+    private Stream<State> add(Item item) {
+        return Stream.of(Floor.values())
+                .map(floor -> add(item, floor));
+    }
+    
+    private State add(Item item, Floor floor) {
+        var newItemLocations = new HashMap<>(itemLocations);
+        newItemLocations.put(item, floor);
+        return new State(newItemLocations);
+    }
+    
+    private boolean isValid() {
         return itemLocations.keySet()
                 .stream()
                 .filter(Microchip.class::isInstance)
@@ -59,5 +87,52 @@ record State(Map<Item, Floor> itemLocations) {
         var generatorFloor = itemLocations.get(new RadioisotopeThermoelectricGenerator(chip.radioisotope()));
         return chipFloor == generatorFloor;
     }
-        
+
+    /// @return the desired end state, where all items have been gathered on the fourth floor
+    State endState() {
+        var endItemLocations = itemLocations.keySet()
+                .stream()
+                .collect(Collectors.toMap(Function.identity(), item -> Floor.FOURTH));
+        return new State(endItemLocations);
+    }
+    
+    /// @return possible transitions, by taking the elevator to another floor, taking one or two items
+    Stream<State> takeElevator() {
+        return Stream.of(Floor.values())
+                .filter(floor -> floor != itemLocations.get(Elevator.INSTANCE))
+                .flatMap(this::takeElevator)
+                .filter(State::isValid);
+    }
+    
+    private Stream<State> takeElevator(Floor targetFloor) {
+        return Stream.concat(takeElevatorAndSingleItem(targetFloor), takeElevatorAndTwoItems(targetFloor));
+    }
+    
+    private Stream<State> takeElevatorAndSingleItem(Floor targetFloor) {
+        var currentFloor = itemLocations.get(Elevator.INSTANCE);
+        return itemsFromFloor(currentFloor)
+                .map(item -> moveItems(targetFloor, Elevator.INSTANCE, item));
+    }
+    
+    private Stream<Item> itemsFromFloor(Floor floor) {
+        return itemLocations.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() == floor)
+                .map(Entry::getKey)
+                .filter(item -> item != Elevator.INSTANCE);
+    }
+    
+    private State moveItems(Floor targetFloor, Item... items) {
+        Map<Item, Floor> newItemLocations = new HashMap<>(itemLocations);
+        Stream.of(items).forEach(item -> newItemLocations.put(item, targetFloor));
+        return new State(newItemLocations);
+    }
+    
+    private Stream<State> takeElevatorAndTwoItems(Floor targetFloor) {
+        var currentFloor = itemLocations.get(Elevator.INSTANCE);
+        return itemsFromFloor(currentFloor)
+                .flatMap(item -> itemsFromFloor(currentFloor)
+                        .filter(otherItem -> item != otherItem)
+                        .map(otherItem -> moveItems(targetFloor, Elevator.INSTANCE, item, otherItem)));
+    }
 }
