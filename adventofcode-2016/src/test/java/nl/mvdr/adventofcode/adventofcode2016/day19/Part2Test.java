8fe617885928c25a9feeaefe6cf23b81d8eb8a1a
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2025-06-01 11:24:23 +0200

    Added a test case to try and find a pattern

--- adventofcode-2016/src/test/java/nl/mvdr/adventofcode/adventofcode2016/day19/Part2Test.java
+++ adventofcode-2016/src/test/java/nl/mvdr/adventofcode/adventofcode2016/day19/Part2Test.java
@@ -2,15 +2,21 @@ package nl.mvdr.adventofcode.adventofcode2016.day19;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import nl.mvdr.adventofcode.solver.SolverTest;
 
 class Part2Test extends SolverTest<Part2> {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(Part2Test.class);
     
     Part2Test() {
         super(Part2.class);
@@ -31,5 +37,13 @@ class Part2Test extends SolverTest<Part2> {
         var result = solver.solve(startingElves);
         
         assertEquals(expectedResult, result);
+    }
+    
+    @Test
+    void test() {
+        var solver = new Part2();
+        
+        IntStream.range(1, 1_000)
+                .forEach(startingElves -> LOGGER.debug("{} starting elves: elf {} wins", startingElves, solver.solve(startingElves)));
     }
 }
