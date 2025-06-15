4240592972509ca7ceb476c369112867096ad785
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2025-06-15 22:10:08 +0200

    WIP

--- adventofcode-2016/src/main/java/nl/mvdr/adventofcode/adventofcode2016/day22/Grid.java
+++ adventofcode-2016/src/main/java/nl/mvdr/adventofcode/adventofcode2016/day22/Grid.java
@@ -1,6 +1,7 @@
 package nl.mvdr.adventofcode.adventofcode2016.day22;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import nl.mvdr.adventofcode.point.Point;
@@ -20,5 +21,22 @@ public record Grid(Set<Node> nodes, Point goalDataLocation) {
     
     boolean isGoalDataAccessible() {
         return Point.ORIGIN.equals(goalDataLocation);
+    }
+    
+    Stream<Grid> step() {
+        var viablePairs = nodes.stream()
+                .flatMap(a -> neighbours(a).map(b -> new NodePair(a, b)))
+                .filter(NodePair::isViable)
+                .collect(Collectors.toSet());
+        
+        return Stream.of(this); // TODO
+    }
+    
+    // TODO the following could be made more efficient by storing nodes in a map, indexed by their location
+    private Stream<Node> neighbours(Node node) {
+        var neighbourLocations = node.location().neighbours();
+        
+        return nodes.stream()
+                .filter(otherNode -> neighbourLocations.contains(otherNode.location()));
     }
 }
