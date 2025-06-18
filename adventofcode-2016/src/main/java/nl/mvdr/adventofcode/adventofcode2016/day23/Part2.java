8b357e5ee7890f2c35efb7f47d1ba2b5603617eb
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2025-06-18 14:21:58 +0200

    Naive solution to part 2

---
+++ adventofcode-2016/src/main/java/nl/mvdr/adventofcode/adventofcode2016/day23/Part2.java
@@ -0,0 +1,34 @@
+package nl.mvdr.adventofcode.adventofcode2016.day23;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nl.mvdr.adventofcode.adventofcode2016.assembunny.AssembunnySolver;
+import nl.mvdr.adventofcode.adventofcode2016.assembunny.Register;
+import nl.mvdr.adventofcode.solver.IntSolver;
+
+public class Part2 implements IntSolver {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Part2.class);
+
+    @Override
+    public int solve(Stream<String> lines) {
+        Map<Register, Integer> startingRegisterValues = Map.of(
+                Register.A, 12,
+                Register.B, 0,
+                Register.C, 0,
+                Register.D, 0);
+        return new AssembunnySolver(startingRegisterValues).solve(lines);
+    }
+    
+    public static void main(String[] args) {
+        Part2 instance = new Part2();
+
+        String result = instance.solve("input-day23.txt");
+
+        LOGGER.info(result);
+    }
+}
