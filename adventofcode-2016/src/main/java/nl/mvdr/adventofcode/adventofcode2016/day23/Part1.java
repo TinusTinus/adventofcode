c0a2416590f1bb867d549c4631ab2ccbc700df5d
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2025-06-18 13:20:57 +0200

    Started day 23

---
+++ adventofcode-2016/src/main/java/nl/mvdr/adventofcode/adventofcode2016/day23/Part1.java
@@ -0,0 +1,36 @@
+package nl.mvdr.adventofcode.adventofcode2016.day23;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nl.mvdr.adventofcode.adventofcode2016.assembunny.AssembunnySolver;
+import nl.mvdr.adventofcode.adventofcode2016.assembunny.Register;
+import nl.mvdr.adventofcode.adventofcode2016.assembunny.State;
+import nl.mvdr.adventofcode.solver.IntSolver;
+
+public class Part1 implements IntSolver {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Part1.class);
+
+    @Override
+    public int solve(Stream<String> lines) {
+        Map<Register, Integer> startingRegisterValues = Map.of(
+                Register.A, 7,
+                Register.B, 0,
+                Register.C, 0,
+                Register.D, 0);
+        State startState = new State(startingRegisterValues, 0);
+        return new AssembunnySolver(startState).solve(lines);
+    }
+    
+    public static void main(String[] args) {
+        Part1 instance = new Part1();
+
+        String result = instance.solve("input-day23.txt");
+
+        LOGGER.info(result);
+    }
+}
