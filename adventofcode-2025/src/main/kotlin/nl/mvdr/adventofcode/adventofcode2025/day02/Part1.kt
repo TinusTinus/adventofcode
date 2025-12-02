dc5cb17775d175cddcd1900075d5a76388bca640
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2025-12-02 20:21:45 +0100

    Solved day 2 part 1

--- adventofcode-2025/src/main/kotlin/nl/mvdr/adventofcode/adventofcode2025/day02/Part1.kt
+++ adventofcode-2025/src/main/kotlin/nl/mvdr/adventofcode/adventofcode2025/day02/Part1.kt
@@ -5,8 +5,19 @@ import nl.mvdr.adventofcode.solver.FunctionSolver
 
 private val logger = KotlinLogging.logger{}
 
-fun solvePart1(lines: Sequence<String>): Int {
-    return 3 // TODO implement
+fun solvePart1(lines: Sequence<String>): Long =
+    lines.first()
+        .split(",")
+        .map { it -> it.split("-") }
+        .map { it -> it.first().toLong() .. it.last().toLong() }
+        .flatMap { it -> it.asSequence() }
+        .filter(::isInvalid)
+        .sum()
+
+private fun isInvalid(id: Long): Boolean {
+    val idString = id.toString()
+    val length = idString.length
+    return length % 2 == 0 && idString.take(length / 2) == idString.substring(length / 2)
 }
 
 fun main() {
