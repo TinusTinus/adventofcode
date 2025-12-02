0247d1a4f88030edb7c67d76f9e77c8b212b9b47
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2025-12-02 19:11:17 +0100

    Solved 2025 day 1 part 2

--- adventofcode-2025/src/main/kotlin/nl/mvdr/adventofcode/adventofcode2025/day01/Part2.kt
+++ adventofcode-2025/src/main/kotlin/nl/mvdr/adventofcode/adventofcode2025/day01/Part2.kt
@@ -3,16 +3,35 @@ package nl.mvdr.adventofcode.adventofcode2025.day01
 import io.github.oshai.kotlinlogging.KotlinLogging
 import nl.mvdr.adventofcode.point.Direction
 import nl.mvdr.adventofcode.solver.FunctionSolver
-import kotlin.math.absoluteValue
-import kotlin.math.min
 
 private val logger = KotlinLogging.logger{}
 
 fun solvePart2(lines: Sequence<String>): Int {
-    return 3 // TODO implement!
+    var dial = 50
+    var zeroes = 0
+    for (instruction in lines) {
+        var distance = instruction.substring(1).toInt()
+        val direction = when(Direction.parse(instruction.take(1))) {
+            Direction.LEFT -> -1
+            Direction.RIGHT -> 1
+            else -> throw IllegalArgumentException("Unexpected direction: {direction}")
+        }
+
+        // Let's just brute force it.
+        // I'm sure this can be done by cleverly using div and mod, but the edge cases seem tricky.
+        while (0 < distance) {
+            dial = (dial + direction) % 100
+            if (dial == 0) {
+                zeroes++
+            }
+            distance--
+        }
+    }
+
+    return zeroes
 }
 
 fun main() {
-    val result = FunctionSolver(::solvePart2).solve("input-day01.txt") // 6603 is too high
+    val result = FunctionSolver(::solvePart2).solve("input-day01.txt")
     logger.info { result }
 }
