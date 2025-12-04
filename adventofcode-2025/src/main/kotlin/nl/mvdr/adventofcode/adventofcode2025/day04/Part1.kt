55db1401234dd5f62aef40912353afe5d01c3b08
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2025-12-04 08:10:47 +0100

    Refactoring

--- adventofcode-2025/src/main/kotlin/nl/mvdr/adventofcode/adventofcode2025/day04/Part1.kt
+++ adventofcode-2025/src/main/kotlin/nl/mvdr/adventofcode/adventofcode2025/day04/Part1.kt
@@ -1,15 +1,15 @@
 package nl.mvdr.adventofcode.adventofcode2025.day04
 
 import io.github.oshai.kotlinlogging.KotlinLogging
+import nl.mvdr.adventofcode.point.Point
 import nl.mvdr.adventofcode.solver.FunctionSolver
 
 private val logger = KotlinLogging.logger{}
 
-fun solvePart1(lines: Sequence<String>): Int {
-    val rolls = parseInput(lines)
-    return rolls.count { roll -> roll.neighboursIncludingDiagonals().count(rolls::contains) < 4 }
-}
+fun solvePart1(lines: Sequence<String>) = solve(parseInput(lines))
 
+private fun solve(rolls: Set<Point>) =
+    rolls.count { roll -> roll.neighboursIncludingDiagonals().count(rolls::contains) < 4 }
 
 fun main() {
     val result = FunctionSolver(::solvePart1).solve("input-day04.txt")
--- adventofcode-2025/src/main/kotlin/nl/mvdr/adventofcode/adventofcode2025/day04/Part2.kt
+++ adventofcode-2025/src/main/kotlin/nl/mvdr/adventofcode/adventofcode2025/day04/Part2.kt
@@ -1,22 +1,19 @@
 package nl.mvdr.adventofcode.adventofcode2025.day04
 
 import io.github.oshai.kotlinlogging.KotlinLogging
+import nl.mvdr.adventofcode.point.Point
 import nl.mvdr.adventofcode.solver.FunctionSolver
 
 private val logger = KotlinLogging.logger{}
 
-fun solvePart2(lines: Sequence<String>): Int {
-    var rolls = parseInput(lines)
-    val startingRolls = rolls.size
+fun solvePart2(lines: Sequence<String>) = solve(parseInput(lines))
 
-    var done = false
-    while (!done) {
-        val newRolls = rolls.filter { roll -> 4 <= roll.neighboursIncludingDiagonals().count(rolls::contains) }.toSet()
-        done = newRolls == rolls
-        rolls = newRolls
+private fun solve(rolls: Set<Point>): Int {
+    val newRolls = rolls.filter { roll -> 4 <= roll.neighboursIncludingDiagonals().count(rolls::contains) }.toSet()
+    return when(val removed = rolls.size - newRolls.size) {
+        0 -> 0
+        else -> removed + solve(newRolls)
     }
-
-    return startingRolls - rolls.size
 }
 
 fun main() {
