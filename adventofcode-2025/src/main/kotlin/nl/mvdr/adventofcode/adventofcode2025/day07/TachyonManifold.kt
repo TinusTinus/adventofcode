e2c06ff6ec9416b0d4b91bec2acf1ccbd6f88b03
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2025-12-07 16:16:22 +0100

    Added caching

--- adventofcode-2025/src/main/kotlin/nl/mvdr/adventofcode/adventofcode2025/day07/TachyonManifold.kt
+++ adventofcode-2025/src/main/kotlin/nl/mvdr/adventofcode/adventofcode2025/day07/TachyonManifold.kt
@@ -4,6 +4,7 @@ import nl.mvdr.adventofcode.point.Point
 
 data class TachyonManifold(val start: Point, val splitters: Set<Point>) {
     private val maxY = Point.maxY(splitters)
+    private val timelineCache = mutableMapOf<Point, Int>()
 
     fun countSplits() = countSplitsStartingFrom(start.y, setOf(start.x), 0)
 
@@ -25,12 +26,19 @@ data class TachyonManifold(val start: Point, val splitters: Set<Point>) {
     fun countTimelines() = countTimelinesStartingFrom(start)
 
     private fun countTimelinesStartingFrom(point: Point): Int =
-        if (point.y == maxY + 1) {
-            1
-        } else if (splitters.contains(point)) {
-            countTimelinesStartingFrom(Point(point.x - 1, point.y + 1)) + countTimelinesStartingFrom(Point(point.x + 1, point.y + 1))
-        } else {
-            countTimelinesStartingFrom(Point(point.x, point.y + 1))
+        timelineCache.getOrPut(point) {
+            if (point.y == maxY + 1) {
+                1
+            } else if (splitters.contains(point)) {
+                countTimelinesStartingFrom(
+                    Point(
+                        point.x - 1,
+                        point.y + 1
+                    )
+                ) + countTimelinesStartingFrom(Point(point.x + 1, point.y + 1))
+            } else {
+                countTimelinesStartingFrom(Point(point.x, point.y + 1))
+            }
         }
 }
 
