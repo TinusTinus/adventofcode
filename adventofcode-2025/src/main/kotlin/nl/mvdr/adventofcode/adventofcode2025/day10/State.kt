b6634782363c5d0042d687f0cdb62ddd72c9621c
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2025-12-17 20:22:31 +0100

    Slightly less naive implementation

--- adventofcode-2025/src/main/kotlin/nl/mvdr/adventofcode/adventofcode2025/day10/State.kt
+++ adventofcode-2025/src/main/kotlin/nl/mvdr/adventofcode/adventofcode2025/day10/State.kt
@@ -1,9 +1,5 @@
 package nl.mvdr.adventofcode.adventofcode2025.day10
 
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath
-import org.jgrapht.graph.DefaultEdge
-import org.jgrapht.graph.SimpleDirectedGraph
-
 interface State<S : State<S>> {
     fun update(button: Button): S
     fun getInitialState(): S
@@ -11,14 +7,19 @@ interface State<S : State<S>> {
 }
 
 fun <S: State<S>> computeFewestButtonPressesToReach(targetState: S, buttons: Set<Button>): Int {
-    val initialState = targetState.getInitialState()
-    val possibleStates = targetState.getPossibleStates()
+    val visited = mutableSetOf<S>()
+    var current = setOf(targetState.getInitialState())
+    var buttonPresses = 0
 
-    val graph = SimpleDirectedGraph<S, DefaultEdge>(DefaultEdge::class.java)
-    possibleStates.forEach(graph::addVertex)
-    possibleStates.forEach { sourceState -> buttons.map(sourceState::update)
-        .filter(graph::containsVertex)
-        .forEach { targetState -> graph.addEdge(sourceState, targetState) } }
+    while(!current.contains(targetState)) {
+        val next = current.flatMap { state -> buttons.map { state.update(it) } }
+            .filter { !visited.contains(it) }
+            .toSet()
 
-    return DijkstraShortestPath(graph).getPath(initialState, targetState).length
+        visited.addAll(current)
+        current = next
+        buttonPresses++
+    }
+
+    return buttonPresses
 }
