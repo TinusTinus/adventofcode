efb7572adadc375ec222f9bf038769b2f1e1ca05
Martijn van de Rijdt <martijn.vande.rijdt@gmail.com>
2025-12-17 20:34:37 +0100

    Do not keep track of previously visited states

--- adventofcode-2025/src/main/kotlin/nl/mvdr/adventofcode/adventofcode2025/day10/State.kt
+++ adventofcode-2025/src/main/kotlin/nl/mvdr/adventofcode/adventofcode2025/day10/State.kt
@@ -11,17 +11,12 @@ interface State<S : State<S>> {
 }
 
 fun <S: State<S>> computeFewestButtonPressesToReach(targetState: S, buttons: Set<Button>): Int {
-    val visited = mutableSetOf<S>()
     var current = setOf(targetState.getInitialState())
     var buttonPresses = 0
 
     while(!current.contains(targetState)) {
-        val next = current.flatMap { state -> buttons.map { state.update(it) } }
-            .filter { !visited.contains(it) }
+        current = current.flatMap { state -> buttons.map { state.update(it) } }
             .toSet()
-
-        visited.addAll(current)
-        current = next
         buttonPresses++
     }
 
